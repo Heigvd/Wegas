420fac576cbfcff65521cdd7fc7e063e0cc73b9c
Cyril Junod <cyril.junod@gmail.com>
2012-03-22 11:36:00 +0100

    +CDI integration +CDI mocks test

---
+++ src/main/java/com/wegas/totest/MessageEvent.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wegas.totest;
+
+/**
+ *
+ * @author Cyril Junod <cyril.junod at gmail.com>
+ */
+public class MessageEvent{
+
+}
