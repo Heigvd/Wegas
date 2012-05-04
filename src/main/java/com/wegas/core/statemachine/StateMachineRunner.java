03a7ff4000580c391b8b6b021b6fe12cd92e3301
Cyril Junod <cyril.junod@gmail.com>
2012-05-04 12:24:19 +0200

    *uncompressed javascript for dev *Storing descriptors

--- src/main/java/com/wegas/core/statemachine/StateMachineRunner.java
+++ src/main/java/com/wegas/core/statemachine/StateMachineRunner.java
@@ -39,7 +39,6 @@ public class StateMachineRunner implements Serializable {
     @EJB
     private VariableDescriptorFacade variableDescriptorFacade;
     private HashSet<Class> ignorableInstances = new HashSet<>();
-
     private HashSet<StateMachineInstanceEntity> stateMachines = new HashSet<>();
 
     public StateMachineRunner() {
@@ -50,11 +49,13 @@ public class StateMachineRunner implements Serializable {
         if (ignorableInstances.contains(entity.getClass())) {
         } else {
             logger.log(Level.INFO, "StateMachineRunner, update: {0}", euEvent.getEntity());
-            Long gmId = entity.getDescriptor().getRootGameModel().getId();
-            List<VariableDescriptorEntity> stateMachineDescriptors = variableDescriptorFacade.findByClassAndGameModelId(StateMachineDescriptorEntity.class, gmId);
-            logger.log(Level.INFO, "StateMachineDescriptor(s) found: {0}", stateMachineDescriptors);
-            for (VariableDescriptorEntity stateMachineDescriptor : stateMachineDescriptors){
-               stateMachines.addAll((Collection<StateMachineInstanceEntity>)stateMachineDescriptor.getScope().getVariableInstances());
+            if (stateMachines.isEmpty()) {                                      // load stateMachines only once
+                Long gmId = entity.getDescriptor().getGameModel().getId();
+                List<VariableDescriptorEntity> stateMachineDescriptors = variableDescriptorFacade.findByClassAndGameModelId(StateMachineDescriptorEntity.class, gmId);
+                logger.log(Level.INFO, "StateMachineDescriptor(s) found: {0}", stateMachineDescriptors);
+                for (VariableDescriptorEntity stateMachineDescriptor : stateMachineDescriptors) {
+                    stateMachines.addAll((Collection<StateMachineInstanceEntity>) stateMachineDescriptor.getScope().getVariableInstances());
+                }
             }
             //NEEEEEED a player id !
         }
