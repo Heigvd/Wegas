e7c0db834f2a26614a006b08c90534980edf7033
Cyril Junod <cyril.junod@gmail.com>
2012-05-09 16:31:01 +0200

    *Improved merge function *cleaning

--- src/main/java/com/wegas/core/persistence/variable/statemachine/StateMachineDescriptorEntity.java
+++ src/main/java/com/wegas/core/persistence/variable/statemachine/StateMachineDescriptorEntity.java
@@ -1,12 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Wegas.
+ * http://www.albasim.com/wegas/
+ *
+ * School of Business and Engineering Vaud, http://www.heig-vd.ch/
+ * Media Engineering :: Information Technology Managment :: Comem
+ *
+ * Copyright (C) 2012
  */
 package com.wegas.core.persistence.variable.statemachine;
 
 import com.wegas.core.persistence.AbstractEntity;
 import com.wegas.core.persistence.variable.VariableDescriptorEntity;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import javax.persistence.*;
 import javax.xml.bind.annotation.XmlType;
@@ -25,10 +31,10 @@ import org.codehaus.jackson.annotate.JsonSubTypes;
 public class StateMachineDescriptorEntity extends VariableDescriptorEntity<StateMachineInstanceEntity> {
 
     private Long initialStateId;
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval=true)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     //@MapKey(name = "id")
     @JoinColumn(name = "statemachine_id", referencedColumnName = "variabledescriptor_id")
-    @MapKeyColumn(name="fsm_statekey")
+    @MapKeyColumn(name = "fsm_statekey")
     private Map<Long, State> states = new HashMap<>();
 
     public StateMachineDescriptorEntity() {
@@ -57,11 +63,26 @@ public class StateMachineDescriptorEntity extends VariableDescriptorEntity<State
 
     @Override
     public void merge(AbstractEntity a) {
-        //TODO: MAP initialState to State
         StateMachineDescriptorEntity smDescriptor = (StateMachineDescriptorEntity) a;
-        this.setStates((HashMap<Long, State>)smDescriptor.getStates());
+        this.mergeStates((HashMap<Long, State>) smDescriptor.getStates());
         this.initialStateId = smDescriptor.initialStateId;
         super.merge(smDescriptor);
     }
 
+    private void mergeStates(HashMap<Long, State> newStates) {
+        for (Long oldKeys : this.states.keySet()) {
+            if (newStates.get(oldKeys) == null) {
+                this.states.remove(oldKeys);
+            } else {
+                this.states.get(oldKeys).merge(newStates.get(oldKeys));
+            }
+        }
+        Iterator<Long> it = newStates.keySet().iterator();
+        while (it.hasNext()) {
+            Long newKey = it.next();
+            if (this.states.get(newKey) == null) {
+                this.states.put(newKey, newStates.get(newKey));
+            }
+        }
+    }
 }
