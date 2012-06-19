b61953ad65c018800f67342e305314290f767f06
Cyril Junod <cyril.junod@gmail.com>
2012-06-19 14:03:36 +0200

    Initial Dialogue entities
    +DialogueDescriptor
    +Transition script impact
    +initial statemachine UI config
    *StateMachine removed id's setter
    *Cleanup

---
+++ src/main/webapp/wegas-editor/js/wegas-statemachineviewer.js
@@ -0,0 +1,21 @@
+/*
+ * Wegas.
+ *
+ * http://www.albasim.com/wegas/
+ *
+ * School of Business and Engineering Vaud, http://www.heig-vd.ch/
+ * Media Engineering :: Information Technology Managment :: Comem
+ *
+ * Copyright (C) 2012
+ */
+
+YUI.add('wegas-statemachineviewer', function (Y) {
+    "use strict";
+    var StateMachineViewer;
+
+    StateMachineViewer = Y.Base.create("wegas-statemachineviewer", Y.Widget, [], {
+
+    }, {});
+
+    Y.namespace('Wegas').StateMachineViewer = StateMachineViewer;
+});
