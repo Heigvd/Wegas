9b07a4dcb987d23e3a5caf82781c967d180edcdd
Cyril Junod <cyril.junod@gmail.com>
2012-07-04 13:56:02 +0200

    Transition from embeddable to entity

--- src/main/java/com/wegas/core/persistence/variable/statemachine/StateMachineInstance.java
+++ src/main/java/com/wegas/core/persistence/variable/statemachine/StateMachineInstance.java
@@ -13,11 +13,9 @@ import com.wegas.core.persistence.AbstractEntity;
 import com.wegas.core.persistence.variable.VariableInstance;
 import com.wegas.core.persistence.variable.dialogue.DialogueInstance;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
@@ -39,9 +37,11 @@ public class StateMachineInstance extends VariableInstance implements Serializab
 
     @Column(name = "currentstate_id")
     private Long currentStateId;
-//    @ElementCollection
-    @Transient
-    private List<Transition> transitionHistory;
+
+    @ElementCollection
+    @CollectionTable(name = "transitionHistory")
+    @Column(name = "transitionId")
+    private List<Long> transitionHistory = new ArrayList<>();
 
     public StateMachineInstance() {
     }
@@ -65,11 +65,11 @@ public class StateMachineInstance extends VariableInstance implements Serializab
         this.currentStateId = currentStateId;
     }
 
-    public List<Transition> getTransitionHistory() {
+    public List<Long> getTransitionHistory() {
         return transitionHistory;
     }
 
-    public void setTransitionHistory(List<Transition> transitionHistory) {
+    public void setTransitionHistory(List<Long> transitionHistory) {
         this.transitionHistory = transitionHistory;
     }
 
