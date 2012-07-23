3963ddd1d72c1ec251e6c5d75c481253b5e867d3
Benjamin Gerber <ger.benjamin@gmail.com>
2012-07-23 13:12:50 +0200

    merge correction

--- src/main/webapp/wegas-app/js/wegas-loader.js
+++ src/main/webapp/wegas-app/js/wegas-loader.js
@@ -240,7 +240,7 @@ YUI().use(function (Y) {
                     /**Leaderway**/
                     'wegas-leaderway': {
                        path: 'wegas-leaderway/js/wegas-leaderway-hrlist.js',
-                        requires:['wegas-leaderway-folder', 'wegas-leaderway-tasklist', 'wegas-leaderway-score', 'wegas-leaderway-dialogue'],
+                        requires:['wegas-leaderway-folder', 'wegas-leaderway-tasklist', 'wegas-leaderway-score', 'wegas-leaderway-dialogue']//,
                         //ix_provides: ""
                     },
                     'wegas-leaderway-folder':{
