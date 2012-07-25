fae4e8327ec257f1ed701aa397bf2ed239099734
Francois-Xavier Aeberhard <fx@red-agent.com>
2012-07-25 21:44:39 +0200

    * Fixed new variabledescriptor form w/ selector
    Signed-off-by: Francois-Xavier Aeberhard <fx@red-agent.com>

--- src/main/webapp/wegas-app/js/statemachine-entities.js
+++ src/main/webapp/wegas-app/js/statemachine-entities.js
@@ -104,12 +104,14 @@ Y.add("statemachine-entities", function(Y){
                     },
                     id: {
                         type: "string",
+                        optional: true,                                         // The id is optional for entites that have not been persisted
                         _inputex: {
-                            _type:'hidden'
+                            _type: "hidden"
                         }
                     },
                     currentStateId: {
                         type: "string",
+                        optional: true,
                         _inputex: {
                             label: 'Initial state id'
                         }
@@ -218,7 +220,7 @@ Y.add("statemachine-entities", function(Y){
         EDITMENU: [{
             type: "EditEntityButton",
             plugins: [{
-               fn: "EditFSMAction"
+                fn: "EditFSMAction"
             }]
         },{
             type: "DeleteEntityButton"
@@ -358,6 +360,7 @@ Y.add("statemachine-entities", function(Y){
             },
             currentStateId: {
                 type: "string",
+                optional: true,
                 _inputex: {
                     label: "Trigger state",
                     disabled:true
@@ -495,4 +498,19 @@ Y.add("statemachine-entities", function(Y){
             }
         }
     });
+
+
+    /**
+     * @hack
+     */
+    Y.Wegas.persistence.VariableDescriptor.EDITFORM.availableFields.push(
+        Y.mix({
+            name: 'TriggerDescriptor',
+            label: 'a trigger'
+        }, new Y.Wegas.persistence.TriggerDescriptor().getFormCfg()),
+        Y.mix({
+            name: 'DialogueDescriptor',
+            label: 'a dialogue'
+        }, new Y.Wegas.persistence.DialogueDescriptor().getFormCfg()));
+
 });
