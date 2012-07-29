1e3342b4f19fb9edf6cc127396129eac6e8547f0
Francois-Xavier Aeberhard <fx@red-agent.com>
2012-07-30 01:41:16 +0200

    + Added tooltip widget and plugin * Refactored SelectPlayer button to use button and menu * Fixed layout height bug
    Signed-off-by: Francois-Xavier Aeberhard <fx@red-agent.com>

--- src/main/webapp/wegas-app/js/widget/widgetmenu.js
+++ src/main/webapp/wegas-app/js/widget/widgetmenu.js
@@ -24,35 +24,36 @@ YUI.add('widgetmenu', function (Y) {
         WidgetMenu.superclass.constructor.apply(this, arguments);
     };
 
-    Y.mix(WidgetMenu, {
-        NS: "wegas",
-        NAME: "WidgetMenu"
-    });
+    WidgetMenu.NS = "menus";
+    WidgetMenu.NAME = "widgetmenu";
+
 
     Y.extend(WidgetMenu, Y.Plugin.Base, {
 
         // *** Lifecycle methods *** //
         initializer: function () {
-            this.afterHostEvent("click", function () {
-                var menu = this.getMenu();
+            this.afterHostEvent( "click", function () {
+                var menu = this.getMenu();                                      // Get a menu instance
 
-                menu.attachTo(this.get("host").get("contentBox"));
-                menu.removeAll();
+                menu.attachTo( this.get( "host" ).get( "boundingBox" ) );       // Attach it to the target node
+                menu.addTarget(this);
+                menu.removeAll();                                               // Empty the node current content
                 try {
-                    menu.add(this.get("children"));
-                } catch(err) {
-                    Y.error("Error while adding subpage to tab (probably du to absence of Y.WidgetChild in config", err, WidgetMenu);
-                }
+                    menu.add( this.get("children") );                           // And place the widget found in the config
+                } catch( err ) {
+                    Y.error("Error while adding subpage to tab (probably du to absence of Y.WidgetChild in config).", err, WidgetMenu);
+                };
             }, this);
         },
 
         // *** Private methods *** //
 
         getMenu: function () {
-            if (!WidgetMenu.menu) {
-                WidgetMenu.menu = new Y.Wegas.Menu();
+            if ( !this.menu ) {
+                this.menu = new Y.Wegas.Menu();
+            //this.menu.addTarget( this );
             }
-            return WidgetMenu.menu;
+            return this.menu;
         }
     }, {
         ATTRS: {
@@ -63,76 +64,91 @@ YUI.add('widgetmenu', function (Y) {
         menu: null
     });
 
-    Y.namespace('Plugin').WidgetMenu = WidgetMenu;
+    Y.namespace( 'Plugin' ).WidgetMenu = WidgetMenu;
 
-    Y.namespace('Wegas').Menu = Y.Base.create("menu", Y.Widget,
-        [Y.WidgetPosition,  Y.WidgetPositionAlign, Y.WidgetStack, Y.WidgetParent, Y.WidgetPositionConstrain], {
+    Y.namespace( 'Wegas' ).Menu = Y.Base.create( "menu" , Y.Widget,
+        [ Y.WidgetPosition,  Y.WidgetPositionAlign, Y.WidgetStack, Y.WidgetParent, Y.WidgetPositionConstrain ], {
 
-        // *** private fields *** //
+            // *** private fields *** //
 
-        timer: null,
+            timer: null,
 
-        // *** Lifecycle methods *** //
+            // *** Lifecycle methods *** //
+            initializer: function () {
+                this.publish( "click", {
+                    emitFacade: true,
+                    bubbles: true
+                });
+            },
 
-        renderUI: function () {
-            var cb = this.get("boundingBox");
+            renderUI: function () {
+                var bb = this.get( "boundingBox" );
 
-            // cb.on("clickoutside", this.hideMenu, this);
-            cb.on("click", this.hide, this);
-            cb.on("mouseenter", this.cancelMenuTimer, this);
-            cb.on("mouseleave", this.startMenuHideTimer, this);
-        },
+                // cb.on("clickoutside", this.hideMenu, this);
+                bb.on( "click", this.hide, this);
+                bb.on( "mouseenter", this.cancelMenuTimer, this);
+                bb.on( "mouseleave", this.startMenuHideTimer, this);
+            },
 
-        // *** Public methods *** //
+            bindUI: function () {
+                this.on( "*:click", function ( e ) {                            // @hack in order for event to be bubbled up
+                    //Y.log("fix");
+                }, this);
+            },
 
-        /**
-         *
-         *  Displays the menu next to the provided node and add mouseenter and
-         *  mouseleave callbacks to the node
-         *
-         * @method attachTo
-         */
-        attachTo: function ( node ) {
-            this.cancelMenuTimer();
-            this.show();
+            // *** Public methods *** //
+            /**
+            *
+            *  Displays the menu next to the provided node and add mouseenter and
+            *  mouseleave callbacks to the node
+            *
+            * @method attachTo
+            */
 
-            // node.on("mouseenter", this.cancelMenuTimer, this);
-            // node.on("mouseleave", this.startMenuHideTimer, this);
-            this.set("align",{
-                node: node,
-                points: ["tl", "bl"]
-            });
-        },
+            attachTo: function ( node , eventTarget) {
+                this.cancelMenuTimer();
+                this.show();
 
-        // *** Private methods *** //
-        startMenuHideTimer: function () {
-            this.cancelMenuTimer();
-            this.timer = Y.later(500, this, this.hide);
-        },
-        cancelMenuTimer: function () {
-            if (this.timer) {
-                this.timer.cancel();
+                // node.on("mouseenter", this.cancelMenuTimer, this);
+                // node.on("mouseleave", this.startMenuHideTimer, this);
+                this.set( "align",{
+                    node: node,
+                    points: [ "tl", "bl" ]
+                });
+            },
+
+            // *** Private methods *** //
+            startMenuHideTimer: function () {
+                this.cancelMenuTimer();
+                this.timer = Y.later(500, this, this.hide);
+            },
+            cancelMenuTimer: function () {
+                if (this.timer) {
+                    this.timer.cancel();
+                }
             }
-        }
-    }, {
-        ATTRS: {
-            constrain: {
-                value: true
-            },
-            zIndex:{
-                value: 1
-            },
-            width: {
-                value: "12em"
-            },
-            render: {
-                value: true
-            },
-            visible: {
-                value: false
+        }, {
+            ATTRS: {
+                constrain: {
+                    value: true
+                },
+                zIndex:{
+                    value: 25
+                },
+                width: {
+                    value: "12em"
+                },
+                render: {
+                    value: true
+                },
+                visible: {
+                    value: false
+                },
+                defaultChildType: {
+                    value: "Button"
+                }
             }
-        }
-    });
+        });
 });
 
 
