f0b87fc500312470fee956a732ce65db1b1f372c
Cyril Junod <cyril.junod@gmail.com>
2012-09-13 12:03:05 +0200

    Was necessary ....

---
+++ src/main/java/com/wegas/app/jsf/controllers/LoginController.java
@@ -0,0 +1,29 @@
+/*
+ * Wegas.
+ * http://www.albasim.com/wegas/
+ *
+ * School of Business and Engineering Vaud, http://www.heig-vd.ch/
+ * Media Engineering :: Information Technology Managment :: Comem
+ *
+ * Copyright (C) 2012
+ */
+package com.wegas.app.jsf.controllers;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Francois-Xavier Aeberhard <fx@red-agent.com>
+ */
+@ManagedBean(name = "loginController")
+@SessionScoped
+public class LoginController implements Serializable {
+
+    /**
+     *
+     */
+    public LoginController() {
+    }
+}
