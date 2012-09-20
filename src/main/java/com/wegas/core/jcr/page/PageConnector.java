6d36a609675dc2b28949ba476f8ffac4d8ed48fb
Cyril Junod <cyril.junod@gmail.com>
2012-09-20 20:56:04 +0200

    First pagecontent storage
    + set page
    + get page
    + get pages

---
+++ src/main/java/com/wegas/core/jcr/page/PageConnector.java
@@ -0,0 +1,112 @@
+/*
+ * Wegas.
+ *
+ * http://www.albasim.com/wegas/
+ *
+ * School of Business and Engineering Vaud, http://www.heig-vd.ch/
+ * Media Engineering :: Information Technology Managment :: Comem
+ *
+ * Copyright (C) 2012
+ */
+package com.wegas.core.jcr.page;
+
+import com.wegas.core.jcr.JackrabbitConnector;
+import java.util.ResourceBundle;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.DependsOn;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Cyril Junod <cyril.junod at gmail.com>
+ */
+@DependsOn({"JackrabbitConnector"})
+@Startup
+@Singleton
+public class PageConnector {
+
+    static final private org.slf4j.Logger logger = LoggerFactory.getLogger(PageConnector.class);
+    final private ResourceBundle resourceBundle = ResourceBundle.getBundle("wegas");
+    final private SimpleCredentials admin = new SimpleCredentials(resourceBundle.getString("jcr.admin.username"), resourceBundle.getString("jcr.admin.password").toCharArray());
+    private static Repository repo;
+    private static Session session = null;
+
+    @PostConstruct
+    private void init() {
+        PageConnector.repo = (Repository) new JackrabbitConnector().getRepo();
+        if (PageConnector.repo == null) {
+            logger.error("Repository initialization failed ");
+        }
+    }
+
+    private Node getRootNode(String gameModelName) throws RepositoryException {
+        Node ret = null;
+        try {
+            ret = this.getSession().getRootNode().getNode(gameModelName);
+        } catch (PathNotFoundException ex) {
+            logger.info("Could not retrieve node ({}), creating it.", ex.getMessage());
+            NodeIterator ni = this.getSession().getRootNode().getNodes();
+            while (ni.hasNext()) {
+                logger.debug(((Node) ni.next()).getPath());
+            }
+            ret = this.getSession().getRootNode().addNode(gameModelName);
+
+        } finally {
+            return ret;
+        }
+    }
+
+    protected NodeIterator listChildren(String gameModelName) throws PathNotFoundException, RepositoryException {
+        NodeIterator ni = this.getRootNode(gameModelName).getNodes();
+        return ni;
+    }
+
+    protected Node getChild(String gameModelName, String path) throws RepositoryException {
+        try {
+            return this.getRootNode(gameModelName).getNode(path);
+        } catch (PathNotFoundException ex) {
+            return null;
+        }
+    }
+
+    protected Node addChild(String gameModelName, String name) throws RepositoryException {
+        try {
+            return this.getRootNode(gameModelName).addNode(name);
+        } catch (javax.jcr.ItemExistsException ex) {
+            return this.getChild(gameModelName, name);
+        }
+    }
+
+    private Session getSession() throws RepositoryException {
+        if (PageConnector.session == null || !PageConnector.session.isLive()) {
+            try {
+                PageConnector.session = PageConnector.repo.login(admin, "Pages");
+            } catch (RepositoryException ex) {
+                Session s;
+                s = PageConnector.repo.login(admin);
+                s.getWorkspace().createWorkspace("Pages");
+                s.logout();
+                PageConnector.session = PageConnector.repo.login(admin, "Pages");
+            }
+        }
+        return PageConnector.session;
+
+    }
+
+    @PreDestroy
+    protected void close() {
+        if (PageConnector.session != null && PageConnector.session.isLive()) {
+            PageConnector.session.logout();
+        }
+    }
+}
---
+++ src/main/java/com/wegas/core/rest/PageController.java
@@ -0,0 +1,89 @@
+/*
+ * Wegas.
+ *
+ * http://www.albasim.com/wegas/
+ *
+ * School of Business and Engineering Vaud, http://www.heig-vd.ch/
+ * Media Engineering :: Information Technology Managment :: Comem
+ *
+ * Copyright (C) 2012
+ */
+package com.wegas.core.rest;
+
+import com.wegas.core.ejb.GameModelFacade;
+import com.wegas.core.jcr.page.Page;
+import com.wegas.core.jcr.page.Pages;
+import com.wegas.core.persistence.game.GameModel;
+import java.util.Iterator;
+import java.util.Map;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jcr.RepositoryException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Cyril Junod <cyril.junod at gmail.com>
+ */
+@Stateless
+@Path("Page/GameModelId/{gameModelId : [1-9][0-9]*}")
+public class PageController {
+
+    static final private org.slf4j.Logger logger = LoggerFactory.getLogger(PageController.class);
+    @EJB
+    private GameModelFacade gmFacade;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONObject getPages(@PathParam("gameModelId") String gameModelId) throws RepositoryException {
+        GameModel gm = gmFacade.find(new Long(gameModelId));
+        Pages pages = new Pages(gm.getName());
+        return pages.getPages();
+    }
+
+    @GET
+    @Path("/{pageId : [1-9][0-9]*}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONObject getPage(@PathParam("gameModelId") String gameModelId, @PathParam("pageId") String pageId) throws RepositoryException {
+        GameModel gm = gmFacade.find(new Long(gameModelId));
+        Pages pages = new Pages(gm.getName());
+        Page page = pages.getPage(new Integer(pageId));
+        if (page == null) {
+            return null;
+        } else {
+            return page.getContent();
+        }
+    }
+
+    @POST
+    @Path("/{pageId : [1-9][0-9]*}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void setPage(@PathParam("gameModelId") String gameModelId, @PathParam("pageId") String pageId, JSONObject content) throws RepositoryException {
+        GameModel gm = gmFacade.find(new Long(gameModelId));
+        Pages pages = new Pages(gm.getName());
+        Page page = new Page(new Integer(pageId), content);
+        pages.store(page);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void setPages(@PathParam("gameModelId") String gameModelId, JSONObject content) throws RepositoryException, JSONException {
+        GameModel gm = gmFacade.find(new Long(gameModelId));
+        Pages pages = new Pages(gm.getName());
+        Iterator kIterator = content.keys();
+        while(kIterator.hasNext()){
+            String key = (String) kIterator.next();
+            Page page = new Page(new Integer(key), content.getJSONObject(key));
+            pages.store(page);
+        }
+    }
+}
