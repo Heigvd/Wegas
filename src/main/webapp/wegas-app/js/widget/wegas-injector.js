5b9c7cc701b4c9dd33c39523709bb3dc3a098aaf
Cyril Junod <cyril.junod@gmail.com>
2012-11-15 13:53:20 +0100

    Injector now loads gallery if needed
    + light-picture class on elements are grouped with the same
    "data-gallery" attribute
    + Injector will instanciate only one Gallery
    - Remove gallery from initial loading
    + Add gallery to crimesim

--- src/main/webapp/wegas-app/js/widget/wegas-injector.js
+++ src/main/webapp/wegas-app/js/widget/wegas-injector.js
@@ -12,6 +12,9 @@
 YUI.add("wegas-injector", function(Y) {
     "use strict";
     var MATCHER = "[data-file]",
+            Injector = function(cfg) {
+        Injector.superclass.constructor.apply(this, arguments);
+    },
             parser = function(element) {
         var nodeName = element.getDOMNode().nodeName;
         switch (nodeName) {
@@ -27,30 +30,50 @@ YUI.add("wegas-injector", function(Y) {
         }
     };
 
-    function Injector(cfg) {
-        Injector.superclass.constructor.apply(this, arguments);
-    }
-
     Y.extend(Injector, Y.Plugin.Base, {
         initializer: function() {
+            this.gallery = false;
             this.insertEvent = this.get("host").get("boundingBox").delegate("DOMNodeInserted", function(e) {
                 parser(e.currentTarget);
-            }, MATCHER);
+                this.instanciateGallery(e.currentTarget);
+            }, MATCHER, this);
             this.renderEvent = this.afterHostEvent("*:render", function(e) {
                 var list = e.currentTarget.get("boundingBox").all(MATCHER);
                 list.each(function(item) {
                     parser(item);
+                });
+                this.instanciateGallery(e.currentTarget.get("boundingBox"));
+            }, this);
+        },
+        instanciateGallery: function(element) {
+            /* Check for gallery elements and loads it*/
+            if (!this.gallery && (element.one(".light-picture") || element.one(".light-gallery"))) {
+                if (!Injector.GALLERY) {
+                    Y.use("wegas-gallery", function() {
+                        Injector.GALLERY = new Y.Wegas.WegasGallery({
+                            "lightGallery": true
+                        });
+                    });
                 }
-                );
-            });
+                Injector.GALLERY_COUNTER += 1;
+                this.gallery = true;
+            }
         },
         destructor: function() {
             this.insertEvent.detach();
             this.renderEvent.detach();
+            if (this.gallery && Injector.GALLERY) {
+                Injector.GALLERY_COUNTER -= 1;
+                if (Injector.GALLERY_COUNTER === 0) {
+                    Injector.GALLERY.destroy();
+                }
+            }
         }
     });
     Injector.NAME = "Injector";
     Injector.NS = "injector";
+    Injector.GALLERY = null;
+    Injector.GALLERY_COUNTER = 0;
     Injector.ATTRS = {
     };
 
