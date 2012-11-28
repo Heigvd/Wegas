e130ac80a72097068189f8ada719d9452fd3112e
Francois-Xavier Aeberhard <fx@red-agent.com>
2012-11-28 15:39:17 +0100

    Refactored lang selector
    Signed-off-by: Francois-Xavier Aeberhard <fx@red-agent.com>

--- wegas-app/src/main/webapp/wegas-app/js/widget/wegas-langselector.js
+++ wegas-app/src/main/webapp/wegas-app/js/widget/wegas-langselector.js
@@ -4,75 +4,75 @@ YUI.add('wegas-langselector', function (Y) {
     var CONTENTBOX = 'contentBox', LangSelector;
 
     LangSelector = Y.Base.create("wegas-langselector", Y.Widget, [Y.WidgetChild, Y.Wegas.Widget], {
-        
-        items: null,
-        handlers:new Array(),
-        
-        renderUI: function() {
-            this.items = this.get("items");
-            this.get(CONTENTBOX).insert("<select class='wegas-langselector-select'></select>");
+
+        CONTENT_TEMPLATE: "<div><select class='wegas-langselector-select'></select></div>",
+
+        handlers: null,
+
+        initializer: function () {
+            this.handlers = [];
         },
-        
-        bindUI: function(){
+
+        bindUI: function () {
             var cb = this.get(CONTENTBOX);
             this.handlers.push(cb.one('.wegas-langselector-select').on('change', function (e) {
-               var lang = e.currentTarget.get('value'),
-               url = window.location.href,
-               urlStart, urlEnd;
-               if(url.indexOf("&lang=") > -1){
-                  urlStart = url.substring(0, url.indexOf("&lang="));
-                  urlEnd = url.substring(url.indexOf("&lang=")+1);
-                    if(urlEnd.indexOf("&")>-1){
-                        urlEnd = urlEnd.substring(urlEnd.indexOf("&"));   
-                    }else{
-                        urlEnd = "";
-                    }
-                    location.replace(urlStart+urlEnd+"&lang="+lang);
-               }else{
-                    location.replace(url+"&lang="+lang);
-               }
-            },'option', this));
+                var lang = e.currentTarget.get('value'),
+                url = window.location.href;
+
+                if (url.indexOf("&lang=") > -1) {
+                    location.replace(url.replace(/&lang=[^&]*/i, "&lang=" + lang));
+                } else {
+                    location.replace(url + "&lang=" + lang);
+                }
+            }));
         },
-        
+
         syncUI: function () {
-            if(this.items == null) return;
-            var i, cb=this.get(CONTENTBOX), browserLang, pageLang, url, selected = false;
-            url = window.location.href;
-            browserLang = (navigator.language) ? navigator.language : navigator.userLanguage; 
-            if(url.indexOf("&lang=")>-1){
-                pageLang = url.substring(url.indexOf("&lang=")+6);
-                if(pageLang.indexOf("&")>-1){
+            var i, cb =  this.get(CONTENTBOX), pageLang, selected = false,
+                url = window.location.href,
+                browserLang = navigator.language || navigator.userLanguage,
+                targetNode = cb.one('.wegas-langselector-select'),
+                items = this.get("items");
+
+            if (items === null) {
+                return;
+            }
+
+            if (url.indexOf("&lang=") > -1) {
+                pageLang = url.substring(url.indexOf("&lang=") + 6);
+                if (pageLang.indexOf("&") > -1) {
                     pageLang = pageLang.substring(0, pageLang.indexOf("&"));
                 }
             }
-            for(i=0;i<this.items.length;i++){
-                if(!selected){
-                    if(pageLang && (this.items[i].indexOf(pageLang)>-1 || pageLang.indexOf(this.items[i])>-1)){
-                        cb.one('.wegas-langselector-select').insert("<option selected='selected'>"+this.items[i]+"</option>");
+            for (i = 0; i < items.length; i++) {
+                if (!selected) {
+                    if (pageLang && (items[i].indexOf(pageLang) > -1 || pageLang.indexOf(items[i]) > -1)) {
+                        targetNode.insert("<option selected='selected'>" + items[i] + "</option>");
                         selected = true;
-                    }else if((this.items[i].indexOf(browserLang)>-1 || browserLang.indexOf(this.items[i])>-1)&&url.indexOf("&lang=")<=-1){
-                        cb.one('.wegas-langselector-select').insert("<option selected='selected'>"+this.items[i]+"</option>");
+                    } else if ((this.items[i].indexOf(browserLang) > -1 || browserLang.indexOf(items[i]) > -1)
+                        && url.indexOf("&lang=") <= -1) {
+                        targetNode.insert("<option selected='selected'>" + items[i] + "</option>");
                         selected = true;
+                    } else {
+                        targetNode.insert("<option>" + items[i] + "</option>");
                     }
-                    else{
-                        cb.one('.wegas-langselector-select').insert("<option>"+this.items[i]+"</option>");
-                    }
-                }else{
-                    cb.one('.wegas-langselector-select').insert("<option>"+this.items[i]+"</option>");
+                } else {
+                    targetNode.insert("<option>" + items[i] + "</option>");
                 }
             }
         },
 
-        destroy: function(){
-            for (var i=0; i<this.handlers.length;i++) {
+        destroy: function () {
+            var i;
+            for (i = 0; i < this.handlers.length; i++) {
                 this.handlers[i].detach();
             }
         }
-        
+
     }, {
         ATTRS : {
-            items:{
-                value:null,
+            items: {
+                value: null,
                 validator: Y.Lang.isArray
             }
         }
