a35eea7a5307f454ec0bc6b7cf55cd754f17ff67
Francois-Xavier Aeberhard <fx@red-agent.com>
2012-12-14 10:18:38 +0100

    Fix loggedout.xhtml link bug
    Signed-off-by: Francois-Xavier Aeberhard <fx@red-agent.com>

--- wegas-core/src/main/java/com/wegas/core/rest/PublicGameController.java
+++ wegas-core/src/main/java/com/wegas/core/rest/PublicGameController.java
@@ -11,11 +11,13 @@ package com.wegas.core.rest;
 
 import com.wegas.core.ejb.GameFacade;
 import com.wegas.core.persistence.game.Game;
+import com.wegas.core.security.ejb.UserFacade;
 import java.util.Collection;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -24,18 +26,30 @@ import javax.ws.rs.core.MediaType;
  * @author Yannick Lagger <lagger.yannick@gmail.com>
  */
 @Stateless
-@Path("PublicGames")
+@Path("/")
 public class PublicGameController {
 
-    
     @EJB
     private GameFacade gameFacade;
+    @EJB
+    private UserFacade userFacade;
 
     @GET
-    @Path("/Games/")
+    @Path("PublicGames/Games/")
     @Produces(MediaType.APPLICATION_JSON)
     public Collection<Game> publicGame() {
         return gameFacade.getPublicGames();
     }
 
+    /**
+     *
+     * @param userId
+     * @return
+     */
+    @GET
+    @Path("RegisteredGames/{userId : [1-9][0-9]*}/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Game> index(@PathParam("userId") Long userId) {
+        return (Collection) userFacade.registeredGames(userId);
+    }
 }
