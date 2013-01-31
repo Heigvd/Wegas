5df01a8838f0dd94c9a69fdb4eb150a6c3700a15
Francois-Xavier Aeberhard <fx@red-agent.com>
2013-01-31 09:49:37 +0100

    Added base for reply controller

---
+++ wegas-core/src/main/java/com/wegas/mcq/rest/ReplyController.java
@@ -0,0 +1,51 @@
+/*
+ * Wegas
+ * http://www.albasim.com/wegas/
+ *
+ * School of Business and Engineering Vaud, http://www.heig-vd.ch/
+ * Media Engineering :: Information Technology Managment :: Comem
+ *
+ * Copyright (C) 2012
+ */
+package com.wegas.mcq.rest;
+
+import com.wegas.core.rest.AbstractRestController;
+import com.wegas.mcq.ejb.ReplyFacade;
+import com.wegas.mcq.persistence.Reply;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import org.apache.shiro.SecurityUtils;
+
+/**
+ *
+ */
+@Stateless
+@Path("GameModel/{gameModelId : [1-9][0-9]*}/VariableDescriptor/{descriptorId :(([1-9][0-9]*)/)?}Reply/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ReplyController {
+
+    /**
+     *
+     */
+    @EJB
+    private ReplyFacade replyFacade;
+
+    /**
+     *
+     * @param entity
+     * @return
+     */
+    @PUT
+    @Path("{entityId: [1-9][0-9]*}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Reply update(@PathParam("entityId") Long entityId, Reply entity) {
+
+      //  SecurityUtils.getSubject().checkPermission("Game:Edit:g" + variableInstanceFacade.findGame(entityId).getId());
+
+        return replyFacade.update(entityId, entity);
+    }
+}
