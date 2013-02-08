0c12c84fd82d06cfb2161dcc31a93688bbdad294
Cyril Junod <cyril.junod@gmail.com>
2013-02-08 18:21:52 +0100

    Some docs

--- wegas-app/src/main/webapp/wegas-app/js/plugin/wegas-injector.js
+++ wegas-app/src/main/webapp/wegas-app/js/plugin/wegas-injector.js
@@ -5,16 +5,19 @@
  * Copyright (c) 2013 School of Business and Engineering Vaud, Comem
  * Licensed under the MIT License
  */
+/**
+ * @fileoverview
+ * @author Cyril Junod
+ */
 YUI.add("wegas-injector", function(Y) {
     "use strict";
 
     var MATCHER = "[data-file]",
-    Injector = function(cfg) {
+            Injector = function(cfg) {
         Injector.superclass.constructor.apply(this, arguments);
     };
 
     Y.extend(Injector, Y.Plugin.Base, {
-
         initializer: function() {
             Y.mix(Y.Node.DOM_EVENTS, {
                 DOMNodeInserted: true,
@@ -37,25 +40,23 @@ YUI.add("wegas-injector", function(Y) {
                 this.instanciateGallery(bb);
             }, this);
         },
-
         parser: function(element) {
             switch (element.getDOMNode().nodeName) {
                 case "IMG":
                     if (!element.hasAttribute("src") || !element.getAttribute("src").match("^(https?://)")) {
                         element.setAttribute("src",
-                            Y.Wegas.FileFacade.get("source") + "read" + element.getAttribute("data-file"));
+                                Y.Wegas.FileFacade.get("source") + "read" + element.getAttribute("data-file"));
                         element.removeAttribute("data-file");
                     }
                     break;
                 default:
                     if (!element.hasAttribute("href") || !element.getAttribute("href").match("^(https?://)")) {
                         element.setAttribute("href",
-                            Y.Wegas.FileFacade.get("source")  + "read" + element.getAttribute("data-file"));
+                                Y.Wegas.FileFacade.get("source") + "read" + element.getAttribute("data-file"));
                         element.removeAttribute("data-file");
                     }
             }
         },
-
         instanciateGallery: function(element) {
             /* Check for gallery elements and loads it*/
             if (!this.gallery && (element.one(".light-picture") || element.one(".light-gallery"))) {
@@ -70,7 +71,6 @@ YUI.add("wegas-injector", function(Y) {
                 this.gallery = true;
             }
         },
-
         destructor: function() {
             this.insertEvent.detach();
             if (this.gallery && Injector.GALLERY) {
