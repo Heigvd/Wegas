7b8c85d7bd595a60f2f700ff43f91c38d34118bd
Cyril Junod <cyril.junod@gmail.com>
2013-02-15 16:58:13 +0100

    showafter, hideafter plugins
    @T7 autoStart and time attribute.

---
+++ wegas-app/src/main/webapp/wegas-app/js/plugin/wegas-visibilitytimer.js
@@ -0,0 +1,121 @@
+/*
+ * Wegas
+ * http://www.albasim.ch/wegas/
+ *
+ * Copyright (c) 2013 School of Business and Engineering Vaud, Comem
+ * Licensed under the MIT License
+ */
+
+/**
+ * @fileOverview 
+ * @author Cyril Junod <cyril.junod at gmail.com>
+ */
+YUI.add("wegas-visibilitytimer", function(Y) {
+    var HIDDENNODECSSCLASS = "timed-hidden-node",
+            visibilityPlugin;
+    visibilityPlugin = function() {
+        visibilityPlugin.superclass.constructor.apply(this, arguments);
+    };
+    Y.extend(visibilityPlugin, Y.Plugin.Base, {
+        initializer: function() {
+            this.timer = null;
+            this.initialVisible = null;
+            if (this.get("host") instanceof Y.Widget) {
+                this.initialVisible = this.get("host").get("visible");
+            } else if (this.get("host") instanceof Y.Node) {
+                this.initialVisible = this.get("host").hasClass(HIDDENNODECSSCLASS);
+            }
+            if (this.get("autoStart")) {
+                this.start();
+            }
+        },
+        reset: function() {
+            if (this.timer) {
+                this.timer.cancel();
+            }
+            if (this.initialVisible) {
+                if (this.get("host") instanceof Y.Widget) {
+                    this.get("host").show();
+                } else if (this.get("host") instanceof Y.Node) {
+                    this.get("host").removeClass(HIDDENNODECSSCLASS);
+                }
+            } else {
+                if (this.get("host") instanceof Y.Widget) {
+                    this.get("host").get("visible").hide();
+                } else if (this.get("host") instanceof Y.Node) {
+                    this.get("host").addClass(HIDDENNODECSSCLASS);
+                }
+            }
+        },
+        start: function() {
+            Y.log("Abstract function, needs to be implemented", "warn");
+        }
+    });
+    Y.mix(visibilityPlugin, {
+        ATTRS: {
+            time: {
+                value: 0,
+                type: "number",
+                _inputEx: {
+                    label: "ms"
+                }
+            },
+            autoStart: {
+                value: true,
+                type: "boolean"
+            }
+        }
+    });
+
+
+    Y.Plugin.ShowAfter = Y.Base.create("wegas-showafter", visibilityPlugin, [Y.Wegas.Plugin, Y.Wegas.Editable], {
+        /**
+         * @lends Y.Plugin.ShowAfter#
+         */
+
+        start: function() {
+            var host = this.get("host");
+            this.reset();
+            if (host instanceof Y.Widget) {
+                this.timer = Y.later(this.get("time"), host, host.show);
+            } else if (host instanceof Y.Node) {
+                this.timer = Y.later(this.get("time"), host, host.removeClass, HIDDENNODECSSCLASS);
+            }
+        }
+
+    }, {
+        /**
+         * @lends Y.Plugin.ShowAfter#
+         */
+        NAME: "ShowAfter",
+        NS: "showafter",
+        ATTRS: {
+        }
+    });
+
+    Y.Plugin.HideAfter = Y.Base.create("wegas-hideafter", visibilityPlugin, [Y.Wegas.Plugin, Y.Wegas.Editable], {
+        /**
+         * @lends Y.Plugin.HideAfter#
+         */
+
+        start: function() {
+            var host = this.get("host");
+            this.reset();
+            if (host instanceof Y.Widget) {
+                this.timer = Y.later(this.get("time"), host, host.hide);
+            } else if (host instanceof Y.Node) {
+                this.timer = Y.later(this.get("time"), host, host.addClass, HIDDENNODECSSCLASS);
+            }
+        }
+    }, {
+        /**
+         * @lends Y.Plugin.HideAfter#
+         */
+        NAME: "HideAfter",
+        NS: "hideafter",
+        ATTRS: {
+        }
+    });
+});
+
+
