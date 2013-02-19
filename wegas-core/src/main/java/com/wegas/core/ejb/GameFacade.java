a84d60613c532966d15fdb9278f096e5dc27c615
Francois-Xavier Aeberhard <fx@red-agent.com>
2013-02-19 12:12:09 +0100

    Refactored lobby.xhtml to have game models on the left, games on the right

--- wegas-core/src/main/java/com/wegas/core/ejb/GameFacade.java
+++ wegas-core/src/main/java/com/wegas/core/ejb/GameFacade.java
@@ -8,15 +8,15 @@
 package com.wegas.core.ejb;
 
 import com.wegas.core.exception.WegasException;
-import com.wegas.core.persistence.game.Game;
-import com.wegas.core.persistence.game.GameModel;
-import com.wegas.core.persistence.game.Game_;
-import com.wegas.core.persistence.game.Team;
+import com.wegas.core.persistence.game.*;
 import com.wegas.core.security.ejb.RoleFacade;
 import com.wegas.core.security.ejb.UserFacade;
 import com.wegas.core.security.persistence.Role;
+import com.wegas.core.security.persistence.User;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
@@ -70,25 +70,6 @@ public class GameFacade extends AbstractFacadeImpl<Game> {
     }
 
     /**
-     * Search for a game with token
-     *
-     * @param token
-     * @return first game found or null
-     */
-    public Game findByToken(final String token) {
-        final CriteriaBuilder cb = em.getCriteriaBuilder();
-        final CriteriaQuery cq = cb.createQuery();
-        final Root<Game> game = cq.from(Game.class);
-        cq.where(cb.equal(game.get(Game_.token), token));
-        Query q = em.createQuery(cq);
-        try {
-            return (Game) q.getSingleResult();
-        } catch (NoResultException ex) {
-            return null;
-        }
-    }
-
-    /**
      *
      * @param gameModelId
      * @param game
@@ -128,12 +109,64 @@ public class GameFacade extends AbstractFacadeImpl<Game> {
     }
 
     /**
+     * Search for a game with token
+     *
+     * @param token
+     * @return first game found or null
+     */
+    public Game findByToken(final String token) {
+        final CriteriaBuilder cb = em.getCriteriaBuilder();
+        final CriteriaQuery cq = cb.createQuery();
+        final Root<Game> game = cq.from(Game.class);
+        cq.where(cb.equal(game.get(Game_.token), token));
+        Query q = em.createQuery(cq);
+        try {
+            return (Game) q.getSingleResult();
+        } catch (NoResultException ex) {
+            return null;
+        }
+    }
+
+    public List<Game> findByGameModelId(final Long gameModelId, final String orderBy) {
+        final Query getByGameId =
+                em.createQuery("SELECT game FROM Game game WHERE game.gameModel.id = :gameModelId ORDER BY game.createdTime DESC");
+        getByGameId.setParameter("gameModelId", gameModelId);
+        //getByGameId.setParameter("orderBy", orderBy);
+        return getByGameId.getResultList();
+    }
+
+    /**
      *
      * @return
      */
-    @Override
-    public EntityManager getEntityManager() {
-        return em;
+    public List<Game> findAll(String orderBy) {
+        final Query getByGameId = em.createQuery("SELECT game FROM Game game ORDER BY game.createdTime DESC");
+        //getByGameId.setParameter("orderBy", orderBy);
+        return getByGameId.getResultList();
+    }
+
+    public List<Game> findRegisteredGames(final Long userId) {
+
+        //final Query getByGameId = em.createQuery("SELECT game FROM Game game ORDER BY game.createdTime ASC");
+
+        final User user = userFacade.find(userId);
+        final List<Game> ret = new ArrayList<>();
+        for (Player p : user.getPlayers()) {
+            ret.add(p.getGame());
+        }
+        return ret;
+    }
+
+    public List<Game> findRegisteredGames(final Long userId, final Long gameModelId) {
+
+        final List<Game> games = this.findRegisteredGames(userId);
+        for (Iterator<Game> it = games.iterator(); it.hasNext();) {
+            Game g = it.next();
+            if (!g.getGameModel().getId().equals(gameModelId)) {
+                it.remove();
+            }
+        }
+        return games;
     }
 
     /**
@@ -142,10 +175,10 @@ public class GameFacade extends AbstractFacadeImpl<Game> {
      * @param userId
      * @return Collection<Game>
      */
-    public Collection<Game> getPublicGames(final Long userId) {
+    public Collection<Game> findPublicGames(final Long userId) {
         final String PREFIX = "Game:View:g";
         final Role pRolle = roleFacade.findByName("Public");
-        final Collection<Game> registerdGame = userFacade.registeredGames(userId);
+        final Collection<Game> registerdGame = this.findRegisteredGames(userId);
         Collection<Game> games = new ArrayList<>();
 
         for (String permission : pRolle.getPermissions()) {
@@ -159,5 +192,14 @@ public class GameFacade extends AbstractFacadeImpl<Game> {
             }
         }
         return games;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
     }
 }
