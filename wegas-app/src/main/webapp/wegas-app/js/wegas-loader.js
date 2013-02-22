3b3ab8e1bb149cfca5aa1c6afe5efc3f5e001ac8
Cyril Junod <cyril.junod@gmail.com>
2013-02-22 13:51:36 +0100

    flexitest refactor
    * controller is an absolute Layout

--- wegas-app/src/main/webapp/wegas-app/js/wegas-loader.js
+++ wegas-app/src/main/webapp/wegas-app/js/wegas-loader.js
@@ -614,7 +614,7 @@ YUI().use(function(Y) {
                 },
                 'wegas-flexitests-controller': {
                     path: "wegas-flexitests/js/wegas-flexitests-controller-min.js",
-                    requires: ["wegas-widget"],
+                    requires: ["wegas-absolutelayout"],
                     ws_provides: "FlexitestsController"
                 }
             }
