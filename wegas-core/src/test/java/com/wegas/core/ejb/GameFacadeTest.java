851b81e370f42f42e7bb2f16de71bc6a31f0daa4
Francois-Xavier Aeberhard <fx@red-agent.com>
2013-02-23 17:53:00 +0100

    Fix missing gamemodel issue in testFindRegisteredGames and moved it to GameFacadeTest

--- wegas-core/src/test/java/com/wegas/core/ejb/GameFacadeTest.java
+++ wegas-core/src/test/java/com/wegas/core/ejb/GameFacadeTest.java
@@ -7,10 +7,18 @@
  */
 package com.wegas.core.ejb;
 
+import com.sun.accessibility.internal.resources.accessibility;
 import com.wegas.core.persistence.game.Game;
+import com.wegas.core.persistence.game.Player;
+import com.wegas.core.persistence.game.Team;
+import com.wegas.core.security.ejb.UserFacade;
+import com.wegas.core.security.jparealm.JpaAccount;
+import com.wegas.core.security.persistence.AbstractAccount;
+import com.wegas.core.security.persistence.User;
 import java.util.List;
 import javax.naming.NamingException;
 import junit.framework.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,13 +30,44 @@ import org.slf4j.LoggerFactory;
 public class GameFacadeTest extends AbstractEJBTest {
 
     private static final Logger logger = LoggerFactory.getLogger(GameFacadeTest.class);
+    private static GameFacade gameFacade;
 
+    @BeforeClass
+    public static void init() throws NamingException {
+        gameFacade = lookupBy(GameFacade.class);
+    }
+
+    /**
+     * Test registeredGames
+     */
     @Test
-    public void testFindRegisteredGames() throws NamingException {
-        logger.info("testFindRegisteredGames()");
+    public void testFindRegisteredGames() throws Exception {
+        final TeamFacade teamFacade = lookupBy(TeamFacade.class);
+        final PlayerFacade playerFacade = lookupBy(PlayerFacade.class);
+        final UserFacade userFacade = lookupBy(UserFacade.class);
 
-        final GameFacade gameFacade = lookupBy(GameFacade.class);
-        final List<Game> registeredGames = gameFacade.findRegisteredGames(Long.valueOf(99999));
-        Assert.assertEquals(0, registeredGames.size());
+        final Game g = new Game();
+        g.setName("game");
+        g.setGameModel(gameModel);
+        gameFacade.create(g);
+        final Team t = new Team();
+        t.setGame(g);
+        t.setName("team");
+        teamFacade.create(t);
+        final User u = new User();
+        final AbstractAccount abstractAccount = new JpaAccount();
+        abstractAccount.setEmail("a@a.com");
+        u.addAccount(abstractAccount);
+        userFacade.create(u);
+        final Player p = new Player();
+        p.setName("player");
+        p.setUser(u);
+        p.setTeam(t);
+        playerFacade.create(p);
+
+        final List<Game> registeredGames = gameFacade.findRegisteredGames(u.getId());
+        org.junit.Assert.assertEquals("game", registeredGames.get(0).getName());
+
+        gameFacade.remove(g.getId());
     }
 }
