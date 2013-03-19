01ae0be55a028da5268e15666702229cde370a36
Francois-Xavier Aeberhard <fx@red-agent.com>
2013-03-19 16:52:12 +0100

    Added page serialization test

--- wegas-app/src/main/webapp/wegas-editor/js/widget/wegas-console.js
+++ wegas-app/src/main/webapp/wegas-editor/js/widget/wegas-console.js
@@ -36,7 +36,7 @@ YUI.add('wegas-console', function (Y) {
         },
 
         executeScript: function (scriptEntity) {
-            Y.Wegas.app.dataSources.VariableDescriptor.sendRequest({
+            Y.Wegas.VariableDescriptorFacade.sendRequest({
                 request: "/Script/Run/" + Y.Wegas.app.get('currentPlayer'),
                 cfg: {
                     method: "POST",
