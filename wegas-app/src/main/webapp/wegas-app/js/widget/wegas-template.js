1c9985d15d159546fdbbb30ce08038d025da9515
Cyril Junod <cyril.junod@gmail.com>
2013-04-04 15:13:01 +0200

    Template improv
    + custom template
    +this.variable as descriptor
    +helper hashCode function
    *deprecate variable-display
    -static template getter. Now prototype.TEMPLATES[key]

--- wegas-app/src/main/webapp/wegas-app/js/widget/wegas-template.js
+++ wegas-app/src/main/webapp/wegas-app/js/widget/wegas-template.js
@@ -36,24 +36,41 @@ YUI.add("wegas-template", function(Y) {
         title: engine.compile(TEMPLATE.TITLE),
         fraction: engine.compile(TEMPLATE.FRACTION)
     };
+    /**
+     * @name Y.Wegas.Template
+     * @extends Y.Widget
+     * @borrows Y.WidgetChild, Y.Wegas.Widget, Y.Wegas.Editable
+     * @class 
+     * @constructor
+     * @description  Display  Wegas variables instance (or/and descriptor) under
+     * specifique templates : text, title, box, fraction and valuebox.
+     * It is also possible to create a custom template.
+     */
     Y.namespace("Wegas").Template = Y.Base.create("wegas-template", Y.Widget,
             [Y.WidgetChild, Y.Wegas.Widget, Y.Wegas.Editable], {
         /*@lends Y.Wegas.Template#*/
-
+        TEMPLATES: {
+            text: engine.compile(TEMPLATE.TEXT),
+            box: engine.compile(TEMPLATE.BOX),
+            valuebox: engine.compile(TEMPLATE.VALUEBOX),
+            title: engine.compile(TEMPLATE.TITLE),
+            fraction: engine.compile(TEMPLATE.FRACTION)
+        },
         initializer: function() {
-            this.data = {};
         },
         renderUI: function() {
             this.set("variable", this.get("variable"));
         },
         syncUI: function() {
-
-            if (COMPILED[this.get("template")]) {
-                this.computeData();
-                this.get("contentBox").setHTML(COMPILED[this.get("template")](this.data));
+            var template = this.get("custom"), hashCode = "" + Y.Wegas.Helper.hashCode(template),
+                    data = this.computeData();
+            if (template === "" && COMPILED[this.get("template")]) {
+                this.get("contentBox").setHTML(this.TEMPLATES[this.get("template")](data));
             } else {
-                // if a template is writen, the descriptor is given as data (this === variableDescriptor) data are unused
-                this.get("contentBox").setHTML(engine.render(this.get("template"), this.get("variable.evaluated")));
+                if (Y.Lang.isUndefined(this.TEMPLATES[hashCode])) {
+                    this.TEMPLATES[hashCode] = engine.compile(template);
+                }
+                this.get("contentBox").setHTML(this.TEMPLATES[hashCode](data));
             }
 
         },
@@ -68,7 +85,8 @@ YUI.add("wegas-template", function(Y) {
             data.max_value = undefinedToEmpty(desc.get("maxValue"));
             data.min_value = undefinedToEmpty(desc.get("minValue"));
             data.default_value = undefinedToEmpty(desc.get("defaultInstance").get("value"));
-            this.data = Y.mix(Y.clone(this.get("data")), data, false, null, 0, true);
+            data.variable = desc;
+            return Y.mix(Y.merge(this.get("data")), data, false, null, 0, true);
         },
         destructor: function() {
 
@@ -77,6 +95,17 @@ YUI.add("wegas-template", function(Y) {
     {/*@lends Y.Wegas.Template*/
         EDITORNAME: "Variable template",
         ATTRS: {
+            /**
+             * The target variable, returned either based on the variableName attribute,
+             * and if absent by evaluating the expr attribute.
+             */
+            variable: {
+                getter: Y.Wegas.Widget.VARIABLEDESCRIPTORGETTER,
+                _inputex: {
+                    _type: "variableselect",
+                    label: "variable"
+                }
+            },
             template: {
                 value: "TEXT",
                 type: "string",
@@ -90,17 +119,19 @@ YUI.add("wegas-template", function(Y) {
                         value: "valuebox"
                     }, {
                         value: "fraction"
-                    }]
-            },
-            /**
-             * The target variable, returned either based on the variableName attribute,
-             * and if absent by evaluating the expr attribute.
-             */
-            variable: {
-                getter: Y.Wegas.Widget.VARIABLEDESCRIPTORGETTER,
+                    }],
                 _inputex: {
-                    _type: "variableselect",
-                    label: "variable"
+                    label: "Predefined template"
+                }
+            },
+            custom: {
+                type: "string",
+                optional: true,
+                value: "",
+                _inputex: {
+                    label: "Custom template",
+                    description: "Takes precedence over predefined templates",
+                    _type: "html"
                 }
             },
             data: {
@@ -111,14 +142,6 @@ YUI.add("wegas-template", function(Y) {
                     required: false
                 }
             }
-        },
-        /**
-         * Get compiled template
-         * @param {String} name
-         * @returns {function} Template
-         */
-        getCompiled: function(name) {
-            return COMPILED[name];
         }
     });
 });
