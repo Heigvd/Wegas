c7962a5f43d3487cd95fb50c669f096c324e0ccd
Cyril Junod <cyril.junod@gmail.com>
2013-05-28 12:40:48 +0200

    Flexitest: save form (as player)
    *corrections

--- wegas-app/src/main/webapp/wegas-flexitests/js/wegas-flexitests-results.js
+++ wegas-app/src/main/webapp/wegas-flexitests/js/wegas-flexitests-results.js
@@ -24,19 +24,20 @@ YUI.add("wegas-flexitests-results", function(Y) {
         syncUI: function() {
             var props = this.get("variable.evaluated").getInstance().get("properties"),
                     table = this.get("contentBox").one("table"),
-                    tmp, o;
+                    tmp, o, i;
             table.empty();
-            table.append("<tr><th>id</th><th>left</th><th>center</th><th>right</th><th>response</th><th>delay</th><th>valid</th></tr>");
-            for (var i in props) {
+            table.append("<tr><th>order</th><th>question id</th><th>left</th><th>center</th><th>right</th><th>response</th><th>delay</th><th>valid</th></tr>");
+            for (i in props) {
                 o = Y.JSON.parse(props[i]);
                 tmp = ["<tr class='row-", (i % 2 === 0 ? 'even' : 'odd'), "'>",
                     "<td>", i, "</td>",
+                    "<td>", o.id, "</td>",
                     "<td>", o.left, "</td>",
                     "<td>", o.center, "</td>",
                     "<td>", o.right, "</td>",
                     "<td>", o.response, "</td>",
                     "<td>", o.delay, "</td>",
-                    "<td>", (o.valid ? "GOOD" : "WRONG"), "</td>",
+                    "<td>", o.valid , "</td>",
                     "</tr>"];
                 table.append(tmp.join(""));
             }
