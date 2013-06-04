c57ed2aec98b3eec42e4d4ab2f18c99f4d9b81c1
Yannick Lagger <yannick.lagger@heig-vd.ch>
2013-06-04 13:40:07 +0200

    add history size + now history save is manual

--- wegas-core/src/main/java/com/wegas/core/persistence/variable/primitive/NumberInstance.java
+++ wegas-core/src/main/java/com/wegas/core/persistence/variable/primitive/NumberInstance.java
@@ -28,6 +28,7 @@ public class NumberInstance extends VariableInstance {
 
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(NumberInstance.class);
+    public static final int HISTORYSIZE = 20;
     /**
      *
      */
@@ -78,7 +79,13 @@ public class NumberInstance extends VariableInstance {
         }
 
         this.val = value;
-        this.history.add(value);
+    }
+    
+    public void saveHistory(){
+        this.history.add(this.val);
+        if (this.history.size() > HISTORYSIZE) {
+            this.history.remove(0);
+        }
     }
     
     public List<Double> getHistory() {
