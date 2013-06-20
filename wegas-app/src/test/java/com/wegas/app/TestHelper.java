cc04853044a85d152191dd6c2f4ef0ec435348a0
Francois-Xavier Aeberhard <fx@red-agent.com>
2013-06-20 13:50:19 +0200

    Fix buggus url in credits

--- wegas-app/src/test/java/com/wegas/app/TestHelper.java
+++ wegas-app/src/test/java/com/wegas/app/TestHelper.java
@@ -2,7 +2,7 @@ package com.wegas.app;
 
 /*
  * Wegas
- * http://www.albasim.ch/wegas/
+ * http://wegas.albasim.ch
  *
  * Copyright (c) 2013 School of Business and Engineering Vaud, Comem
  * Licensed under the MIT License
--- wegas-core/src/main/java/com/wegas/core/rest/AbstractRestControllerI.java
+++ wegas-core/src/main/java/com/wegas/core/rest/AbstractRestControllerI.java
@@ -1,7 +1,7 @@
 /*
  * Wegas
  *
- * http://www.albasim.ch/wegas/
+ * http://wegas.albasim.ch
  *
  * Copyright (c) 2013 School of Business and Engineering Vaud, Comem
  * Licensed under the MIT License
