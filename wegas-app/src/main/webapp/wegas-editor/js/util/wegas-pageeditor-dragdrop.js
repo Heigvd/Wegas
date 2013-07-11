083f18f6d084816f1496306f9b83dd6046f0e5c5
Cyril Junod <cyril.junod@gmail.com>
2013-07-11 16:49:25 +0200

    PageEditorDD constrain/scroll

--- wegas-app/src/main/webapp/wegas-editor/js/util/wegas-pageeditor-dragdrop.js
+++ wegas-app/src/main/webapp/wegas-editor/js/util/wegas-pageeditor-dragdrop.js
@@ -14,14 +14,22 @@ YUI.add("wegas-pageeditor-dragdrop", function(Y) {
     "use strict";
     /**
      * PageEditor Plugin Extension enabling position drag and drop.
+     * @constructor
      */
     function PageEditorDD() {
         Y.onceAfter(this._initDD, this, "render");
         Y.after(this._alignDD, this, "showOverlay");
+        Y.onceAfter(this._destruct, this, "destructor");
     }
     PageEditorDD.NAME = "wegas-pageeditor-dragdrop";
     PageEditorDD.prototype = {
+        /* @lends Y.Wegas.PageEditorDD# */
+        /**
+         * @private
+         * @returns {undefined}
+         */
         _initDD: function() {
+
             if (!this.highlightOverlay) {
                 Y.error("PageEditorDD is an extension for PageEditor.");
             }
@@ -39,27 +47,33 @@ YUI.add("wegas-pageeditor-dragdrop", function(Y) {
                     this._ddNode.hide();
                 }
             }, this);
-            this._ddNode.before("mousedown", function(e) {
-                this.detach();
-                this._ddNode.show();
-            }, this);
+
             this._dd = new Y.DD.Drag({
                 node: this._ddNode
             });
-            this._dd.on("drag:start", function(e) {
+            this._dd.plug(Y.Plugin.DDConstrained, {
+                constrain: this.get("host").get("contentBox")
+            }).plug(Y.Plugin.DDNodeScroll, {
+                node: this.get("host").get("contentBox")
+            });
+            this._ddNode.before("mousedown", function(e) {
+                this.detach();
+                this._ddNode.show();
                 try {
                     this._dd.set("dragNode", this.overlayWidget.get("boundingBox"));
-                    this.overlayWidget.unplug(Y.Plugin.CSSPosition);
                     this._dd.get("dragNode").setStyles({
                         bottom: null,
                         right: null
                     });
-                } catch (e) {
+                } catch (ex) {
                 }
+            }, this);
+            this._ddNode.after("mouseup", function(e) {
+                this.bind();
             }, this);
             this._dd.on("drag:end", function(e) {
                 var bb = this._dd.get("dragNode"), widget = Y.Widget.getByNode(bb);
-                widget.plug(Y.Plugin.CSSPosition);
+                widget.plug(Y.Plugin.CSSPosition);                              //no effect if present
                 widget.CSSPosition.setAttrs({
                     "styles": {
                         "position": bb.getStyle("position"),
@@ -71,11 +85,27 @@ YUI.add("wegas-pageeditor-dragdrop", function(Y) {
                 this.bind();
             }, this);
         },
+        /**
+         * Align drag node with PageEditor's overlay
+         * @private
+         * @returns {undefined}
+         */
         _alignDD: function() {
             var bb = this.overlayWidget.get("boundingBox");
             this._ddNode.setXY(bb.getXY());
+        },
+        /**
+         * self destructor called after PageEditor's destructor
+         * @private
+         * @returns {undefined}
+         */
+        _destruct: function() {
+            Y.detach(this._alignDD, this, "showOverlay");
+            this._dd.detachAll();
+            this._ddNode.detachAll();
+            this._dd.destroy();
+            this._ddNode.destroy(true);
         }
-
 
     };
 
