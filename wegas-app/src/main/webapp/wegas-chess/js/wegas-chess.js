728a44d74dc75cbfa8ecdbb7c7b7d651cf090db0
Francois-Xavier Aeberhard <fx@red-agent.com>
2013-07-16 14:53:10 +0200

    Chess game v0.2

--- wegas-app/src/main/webapp/wegas-chess/js/wegas-chess.js
+++ wegas-app/src/main/webapp/wegas-chess/js/wegas-chess.js
@@ -15,37 +15,20 @@ YUI.add("wegas-chess", function(Y) {
     var CONTENTBOX = "contentBox", ChessBoard;
 
     ChessBoard = Y.Base.create("wegas-chessboard", Y.Widget, [Y.WidgetChild, Y.Wegas.Widget, Y.Wegas.Editable], {
-        BOUNDING_TEMPLATE: "<div><div class=\"chess-label-p2\"></div>"
+        BOUNDING_TEMPLATE: "<div>"
+                + "<div class=\"chess-label-p2\"></div>"
                 + "<div class=\"chess-status\">Loading</div>"
                 + "<div class=\"chess-label-p1\"></div></div>",
         BOARDSIZE: 8,
+        /**
+         *
+         */
         initializer: function() {
-            this.board = [];
-            for (var i = 0; i < this.BOARDSIZE; i++) {                    // Fill the board with empty arrays
-                this.board.push([]);
-            }
-            this.side = 1;
-        },
-        renderUI: function() {
-            var i, pieces = this.getPieces(),
-                    bb = this.get("boundingBox"),
-                    currentGame = Y.Wegas.Facade.Game.cache.getCurrentGame(),
-                    currentPlayers = currentGame.get("teams")[0].get("players"),
-                    cPlayerId = Y.Wegas.app.get("currentPlayer");
-
             this.side = 0;
-            if (currentPlayers[0].get("id") === cPlayerId) {
-                this.side = 1;
-            } else if (currentPlayers[1].get("id") === cPlayerId) {
-                this.side = -1;
-            }
-            for (i = 0; i < pieces.length; i += 1) {
-                this.renderPiece(pieces[i]);
-            }
-            bb.one(".chess-label-p1").setContent(currentPlayers[0].get("name"));
-            bb.one(".chess-label-p2").setContent(currentPlayers[1].get("name"));
-
         },
+        /**
+         *
+         */
         bindUI: function() {
             var cb = this.get(CONTENTBOX),
                     bb = this.get("boundingBox");
@@ -72,6 +55,14 @@ YUI.add("wegas-chess", function(Y) {
             }, ".chess-playing .chess-piece-playable", this);
 
             cb.delegate("click", function(e) {                                  // Move selection: move selected piece to target position
+
+                var colidee = this.getPieceAt(e.target.cfg),
+                        script = "move(\"" + this.currentTarget.cfg.name + "\", \"" + this.pos2string(e.target.cfg) + "\");";
+
+                if (colidee) {                                                  // If there is a clolidee
+                    script += "destroy(\"" + colidee.name + "\");";             // destroy it
+                }
+
                 Y.Wegas.Facade.VariableDescriptor.sendRequest({
                     request: "/Script/Run/" + Y.Wegas.app.get('currentPlayer'),
                     cfg: {
@@ -79,12 +70,15 @@ YUI.add("wegas-chess", function(Y) {
                         data: {
                             "@class": "Script",
                             language: "JavaScript",
-                            content: "move(\"" + this.currentTarget.cfg.name + "\", \"" + this.pos2string(e.target.cfg) + "\");"
+                            content: script
                         }
                     }
                 });
             }, ".chess-move", this);
         },
+        /**
+         *
+         */
         syncUI: function() {
             Y.log("syncUI()", "info", "Wegas.ChessBoard");
 
@@ -96,33 +90,45 @@ YUI.add("wegas-chess", function(Y) {
                     treated = [],
                     allPiecesNodes = cb.all(".chess-piece"),
                     currentTurn = Y.Wegas.Facade.VariableDescriptor.cache.find("name", "currentTurn").getInstance().get("value"),
-                    statusNode = bb.one(".chess-status");
+                    statusNode = bb.one(".chess-status"),
+                    currentGame = Y.Wegas.Facade.Game.cache.getCurrentGame(),
+                    players = currentGame.get("teams")[0].get("players"),
+                    cPlayerId = Y.Wegas.app.get("currentPlayer");
+
+            this.side = 0;                                                      // Determine which side
+            if (players[0].get("id") === cPlayerId) {
+                this.side = 1;
+            } else if (players[1].get("id") === cPlayerId) {
+                this.side = -1;
+            }
 
             cb.removeClass("chess-playing");
             if (currentTurn === 0) {
-                statusNode.setContent("Waiting for a second player to join before starting game.");
+                statusNode.setContent("Waiting for a second player to join before starting game. You will need to refresh page to start game.");
+                return;
             } else if (currentTurn !== this.side) {
                 statusNode.setContent("Waiting for opponent's move");
             } else {
                 statusNode.setContent("Your turn to play");
                 cb.addClass("chess-playing");
             }
+
+            bb.one(".chess-label-p1").setContent(players[0].get("name"));
+            bb.one(".chess-label-p2").setContent(players[1].get("name"));
+
             for (i = 0; i < pieces.length; i += 1) {
                 var cfg = pieces[i],
                         cNode = cb.one(".chess-piece-" + cfg.name + cfg.side);
 
                 if (cNode) {                                                    // case 1: Piece has been moved
                     treated.push(cNode);
-                    if (cfg.x !== cNode.cfg.x || cfg.x !== cNode.cfg.y) {
-
+                    if (cfg.x !== cNode.cfg.x || cfg.y !== cNode.cfg.y) {
                         cNode.transition(Y.mix({
                             easing: 'ease-out',
                             duration: 0.75
                         }, this.formatPos(cfg)));
 
-                        this.removePieceAt(cNode.cfg);
                         cNode.cfg = Y.mix(cfg, cNode.cfg);
-                        this.board[cNode.cfg.x][cNode.cfg.y] = cNode.cfg;
                     }
                 } else {                                                        // Case 2: Piece was added to the board (start game)
                     this.renderPiece(cfg);
@@ -132,22 +138,31 @@ YUI.add("wegas-chess", function(Y) {
             for (i = 0; i < allPiecesNodes.size(); i += 1) {                    // Case 3: Piece is not present anymore, we destroy it
                 var item = allPiecesNodes.item(i);
                 if (treated.indexOf(item) === -1) {
-                    this.removePieceAt(item.cfg);
                     item.remove(true);
                 }
             }
         },
+        destructor: function() {
+            this.updateHandler.detach();
+        },
+        /**
+         *
+         */
         getPieces: function() {
             var i, j,
                     pieces = Y.Wegas.Facade.VariableDescriptor.cache.find("name", "pieces"),
                     cPlayerPieces,
                     currentGame = Y.Wegas.Facade.Game.cache.getCurrentGame(),
-                    currentPlayers = currentGame.get("teams")[0].get("players"),
+                    players = currentGame.get("teams")[0].get("players"),
                     side = 1,
                     ret = [];
 
+            if (players.length < 2) {
+                return [];
+            }
+
             for (j = 0; j < 2; j += 1) {                                        // Only render pieces for the two first players, other are spectators
-                cPlayerPieces = pieces.getInstance(currentPlayers[j].get("id")).get("properties");
+                cPlayerPieces = pieces.getInstance(players[j].get("id")).get("properties");
 
                 for (i in cPlayerPieces) {
                     var cfg = Y.mix(this.string2pos(cPlayerPieces[i]), {
@@ -161,27 +176,29 @@ YUI.add("wegas-chess", function(Y) {
             }
             return ret;
         },
-        getPieceAt: function(cfg) {
-            if (this.board[cfg.x]) {
-                return this.board[cfg.x][cfg.y];
-            }
-            return null;
+        /**
+         *
+         */
+        getPieceAt: function(pos) {
+            return Y.Array.find(this.getPieces(), function(item) {
+                return item.x === pos.x && item.y === pos.y;
+            });
         },
-        removePieceAt: function(cfg) {
-            this.board[cfg.x][cfg.y] = null;
-        },
+        /**
+         *
+         */
         getAvailableMoves: function(cfg) {
             var i, j, moves = [], finalMoves = [],
                     genLine = function(dirs, length) {
                 for (i = 0; i < dirs.length; i += 1) {
-                    var d = dirs[i], occupant = null;
-                    for (j = 1; j < length && !occupant; j += 1) {
+                    var d = dirs[i], collidee = null;
+                    for (j = 1; j < length && !collidee; j += 1) {
                         var m = {
                             x: cfg.x + (j * d.x),
                             y: cfg.y + (j * d.y)
                         };
-                        occupant = this.getPieceAt(m);
-                        if (this.isValidMove(m)) {
+                        collidee = this.getPieceAt(m);
+                        if (this.isValidMove(m, cfg)) {
                             moves.push(m);
                         }
                     }
@@ -200,45 +217,84 @@ YUI.add("wegas-chess", function(Y) {
                     moves.push({x: cfg.x + 2, y: cfg.y - 1});
                     moves.push({x: cfg.x - 2, y: cfg.y - 1});
                     break;
+
                 case "queen":
                     var dirs = [{x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}, {x: 0, y: -1},
                         {x: 1, y: 1}, {x: -1, y: 1}, {x: 1, y: -1}, {x: -1, y: -1}];
-                    moves = moves.concat(genLine.call(this, dirs, 8));
+                    moves = genLine.call(this, dirs, 8);
                     break;
+
                 case "bishop":
                     var dirs = [{x: 1, y: 1}, {x: -1, y: 1}, {x: 1, y: -1}, {x: -1, y: -1}];
-                    moves = moves.concat(genLine.call(this, dirs, 8));
+                    moves = genLine.call(this, dirs, 8);
                     break;
+
                 case "king":
-                    var dirs = [{x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}, {x: 0, y: -1}];
-                    moves = moves.concat(genLine.call(this, dirs, 2));
+                    var dirs = [{x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}, {x: 0, y: -1},
+                        {x: 1, y: 1}, {x: 1, y: -1}, {x: -1, y: 1}, {x: -1, y: -1}];
+                    moves = genLine.call(this, dirs, 2);
                     break;
+
                 case "tower":
                     var dirs = [{x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}, {x: 0, y: -1}];
-                    moves = moves.concat(genLine.call(this, dirs, 8));
+                    moves = genLine.call(this, dirs, 8);
                     break;
-                default:                                                        // Pawns
-                    var factor = ((cfg.y === 1 && cfg.side === 1)
-                            || (cfg.y === 6 && cfg.side === -1)) ? 3 : 2;       // When it's their 1st move, pawns can move by 2
 
-                    moves = moves.concat(genLine.call(this, [{x: 0, y: cfg.side}], factor));
+                case "pawn":                                                    // Pawns
+                    var i, factor = ((cfg.y === 1 && cfg.side === 1)
+                            || (cfg.y === 6 && cfg.side === -1)) ? 3 : 2; // When it's their 1st move, pawns can move by 2,
+
+                    moves = genLine.call(this, [{x: 0, y: cfg.side}], factor);
+
+                    for (i = 0; i < 2; i += 1) {                                // Pawn eat on til up, left and right
+                        var factor = (i === 0) ? 1 : -1,
+                                m = {
+                            x: cfg.x + factor,
+                            y: cfg.y + this.side
+                        };
+                        if (this.getPieceAt(m)) {
+                            moves.push(m);
+                        }
+                        //colidee = this.getPieceAt({                           // Pawn also eat "En passant"
+                        //    x: cfg.x + factor,
+                        //    y: cfg.y
+                        //});
+                        //if (colidee && colidee.side !== this.side) {
+                        //    m.enPassant = true;
+                        //    moves.push(m);
+                        //}
+                    }
                     break;
+
+                default:
+                    Y.log("Unknow piece type", "error", "Wegas.ChessBoard");
+                    break
             }
 
             for (i = 0; i < moves.length; i += 1) {                             // Finally, remove any move that is on an enemy
                 var m = moves[i];                                               // or out of the board
-                if (this.isValidMove(m)) {
+                if (this.isValidMove(m, cfg)) {
                     finalMoves.push(m);
                 }
             }
             return finalMoves;
         },
-        isValidMove: function(cfg) {
-            var occupant = this.getPieceAt(cfg);
-            return (cfg.x >= 0 && cfg.x < 8
-                    && cfg.y >= 0 && cfg.y < 8
-                    && (!occupant || (occupant.side !== this.side && occupant.type !== "king")));
+        /**
+         *
+         */
+        isValidMove: function(pos, cfg) {
+            var collidee = this.getPieceAt(pos);
+            return (pos.x >= 0 && pos.x < 8
+                    && pos.y >= 0 && pos.y < 8
+                    && (!collidee ||
+                    (collidee.side !== this.side
+                            && collidee.type !== "king"
+                            && (cfg.type !== "pawn" || pos.x !== cfg.x)
+                            )));
         },
+        /**
+         *
+         */
         renderPiece: function(cfg) {
             var cNode, cb = this.get(CONTENTBOX);
 
@@ -251,8 +307,6 @@ YUI.add("wegas-chess", function(Y) {
             cNode.setStyles(this.formatPos(cfg));
             cNode.cfg = cfg;
             cb.appendChild(cNode);
-
-            this.board[cfg.x][cfg.y] = cfg;
         },
         unselectCurrentPiece: function() {
             var cb = this.get(CONTENTBOX);
