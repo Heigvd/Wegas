232fc933a4304d14057fdff92a925f324645cf43
Yannick Lagger <yannick.lagger@heig-vd.ch>
2013-09-30 10:11:47 +0200

    Improve fixbug player action name

--- wegas-core/src/main/java/com/wegas/core/persistence/variable/VariableDescriptor.java
+++ wegas-core/src/main/java/com/wegas/core/persistence/variable/VariableDescriptor.java
@@ -80,13 +80,14 @@ abstract public class VariableDescriptor<T extends VariableInstance> extends Nam
     /**
      *
      */
-//    @JsonView(Views.EditorI.class)
+    @JsonView(Views.EditorI.class)
+    private String label;
+    /**
+     * Title displayed in the for the player, should be removed from variable descriptor and placed
+     * in the required entities (MCQQuestionDrescriptor, TriggerDescriptor, aso)
+     */
     @Column(name = "editorLabel")
     private String title;
-    /**
-     *
-     */
-    private String label;
     /**
      *
      */
