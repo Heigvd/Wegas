3ceba44cc9d63895e052e6f5be519802d2d288d0
Anthony Geiser <antho.geiser@gmail.com>
2013-10-08 13:33:26 +0200

    Persistent tooltip

--- wegas-app/src/main/webapp/wegas-app/js/wegas-loader.js
+++ wegas-app/src/main/webapp/wegas-app/js/wegas-loader.js
@@ -929,7 +929,7 @@ YUI().use(function(Y) {
                     path: "wegas-teaching/js/wegas-teaching-main.js",
                     ws_provides: "TeachingMain",
                     requires: ["plugin", "panel", "wegas-teaching-arrow", "wegas-teaching-rectangle",
-                        "editor", "gallery-yui-tooltip", "dd-plugin", "autocomplete",
+                        "editor", "dd-plugin", "autocomplete",
                         "autocomplete-highlighters", "autocomplete-filters"]
                 }
             }
