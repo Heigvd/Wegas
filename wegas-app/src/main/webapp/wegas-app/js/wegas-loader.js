46e865925f8f4f26c5436d14f53832bc13ad32d1
Anthony Geiser <antho.geiser@gmail.com>
2013-10-08 14:00:37 +0200

    Add GUI toolbar in rich text editor (bold, italic and underline)

--- wegas-app/src/main/webapp/wegas-app/js/wegas-loader.js
+++ wegas-app/src/main/webapp/wegas-app/js/wegas-loader.js
@@ -929,7 +929,7 @@ YUI().use(function(Y) {
                     path: "wegas-teaching/js/wegas-teaching-main.js",
                     ws_provides: "TeachingMain",
                     requires: ["plugin", "panel", "wegas-teaching-arrow", "wegas-teaching-rectangle",
-                        "editor", "dd-plugin", "autocomplete",
+                        "editor", "dd-plugin", "autocomplete", "gallery-itsatoolbar",
                         "autocomplete-highlighters", "autocomplete-filters"]
                 }
             }
