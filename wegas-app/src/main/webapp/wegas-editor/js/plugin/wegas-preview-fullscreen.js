a29f243b7f79438092c591948eb36d5e71626bed
Francois-Xavier Aeberhard <fx@red-agent.com>
2013-12-23 12:39:59 +0100

    Refactor wegas-datasource.js to remove useless methods

--- wegas-app/src/main/webapp/wegas-editor/js/plugin/wegas-preview-fullscreen.js
+++ wegas-app/src/main/webapp/wegas-editor/js/plugin/wegas-preview-fullscreen.js
@@ -5,9 +5,8 @@
  * Copyright (c) 2013 School of Business and Engineering Vaud, Comem
  * Licensed under the MIT License
  */
-
 /**
- * @fileOverview 
+ * @fileOverview
  * @author Cyril Junod <cyril.junod at gmail.com>
  */
 YUI.add("wegas-preview-fullscreen", function(Y) {
@@ -16,29 +15,22 @@ YUI.add("wegas-preview-fullscreen", function(Y) {
     Y.namespace("Plugin").PreviewFullScreen = Y.Base.create("wegas-preview-fullscreen", Y.Plugin.Base, [], {
         initializer: function() {
             this.afterHostEvent("render", this.render);
-            this.handlers = [];
         },
         render: function() {
-            var el, host = this.get('host');
+            var host = this.get('host');
 
             if (host.toolbar) {
                 this.swapNode = Y.Node.create("<span><span>");
                 Y.one("body").append(this.swapNode);
-                el = host.toolbar.get('header');
-                this.fullScreenButton = new Y.ToggleButton({
+                this.fullScreenButton = host.toolbar.add(new Y.ToggleButton({
                     label: "<span class='wegas-icon wegas-icon-fullscreen'></span>Fullscreen"
-                }).render(el);
-                this.handlers.push(this.fullScreenButton.after("pressedChange", function() {
+                }));
+                this.fullScreenButton.after("pressedChange", function() {
                     this.get("host").get("boundingBox").swap(this.swapNode);
-                }, this));
+                }, this);
             }
         },
         destructor: function() {
-            var i;
-            for (i = 0; i < this.handlers.length; i += 1) {
-                this.handlers[i].detach();
-            }
-            this.handlers = [];
             if (this.fullScreenButton) {
                 this.fullScreenButton.destroy();
             }
@@ -46,5 +38,7 @@ YUI.add("wegas-preview-fullscreen", function(Y) {
                 this.swapNode.destroy(true);
             }
         }
-    }, {NS: "preview"});
+    }, {
+        NS: "preview"
+    });
 });
