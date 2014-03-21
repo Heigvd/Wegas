9bfc95971699ff11b75e952542a0627c16c509ae
Cyril Junod <cyril.junod@gmail.com>
2014-03-21 14:50:48 +0100

    EHCache added to ComboController
    *Fix #445

--- wegas-core/src/main/java/com/wegas/core/rest/util/CacheFilterFactory.java
+++ wegas-core/src/main/java/com/wegas/core/rest/util/CacheFilterFactory.java
@@ -21,25 +21,45 @@ import java.util.List;
  */
 public class CacheFilterFactory implements ResourceFilterFactory {
 
+    private static final CacheResponseFilter noCacheResponseFilter = new CacheResponseFilter(CacheResponseFilter.NO_CACHE);
+    private static final String private_cache = "private, ";
+
+    /**
+     * Filter jersey resources and add a filter to CacheMaxAge and NoCache
+     * annotated functions.
+     *
+     * @param am AbstractMethod passed during Resource filtering
+     * @return
+     */
     @Override
     public List<ResourceFilter> create(AbstractMethod am) {
         /*Test method level*/
         CacheMaxAge cma = am.getAnnotation(CacheMaxAge.class);
         NoCache nc = am.getAnnotation(NoCache.class);
         if (cma != null) {
-            return Collections.<ResourceFilter>singletonList(new CacheResponseFilter("max-age: " + cma.unit().toSeconds(cma.time())));
+            return Collections.<ResourceFilter>singletonList(new CacheResponseFilter(genCacheString(cma)));
         } else if (nc != null) {
-            return Collections.<ResourceFilter>singletonList(new CacheResponseFilter("no-cache"));
+            return Collections.<ResourceFilter>singletonList(noCacheResponseFilter);
         }
         /*Test class level*/
         cma = am.getResource().getAnnotation(CacheMaxAge.class);
         nc = am.getResource().getAnnotation(NoCache.class);
         if (cma != null) {
-            return Collections.<ResourceFilter>singletonList(new CacheResponseFilter("max-age: " + cma.unit().toSeconds(cma.time())));
+            return Collections.<ResourceFilter>singletonList(new CacheResponseFilter(genCacheString(cma)));
         } else if (nc != null) {
-            return Collections.<ResourceFilter>singletonList(new CacheResponseFilter("no-cache"));
+            return Collections.<ResourceFilter>singletonList(noCacheResponseFilter);
         }
         return Collections.emptyList();
 
+    }
+
+    /**
+     * Compute cache-control string based on annotations.
+     *
+     * @param cma
+     * @return
+     */
+    private static String genCacheString(CacheMaxAge cma) {
+        return (cma.private_cache() ? private_cache : "") + "max-age: " + cma.unit().toSeconds(cma.time());
     }
 }
---
+++ wegas-core/src/main/java/com/wegas/core/rest/util/CacheManagerHolder.java
@@ -0,0 +1,36 @@
+/*
+ * Wegas
+ * http://wegas.albasim.ch
+ *
+ * Copyright (c) 2013 School of Business and Engineering Vaud, Comem
+ * Licensed under the MIT License
+ */
+package com.wegas.core.rest.util;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import net.sf.ehcache.CacheManager;
+
+/**
+ *
+ * @author Cyril Junod <cyril.junod at gmail.com>
+ */
+@Singleton
+public class CacheManagerHolder {
+
+    private final CacheManager instance;
+
+    public CacheManagerHolder() {
+        instance = CacheManager.getInstance();
+    }
+
+    public CacheManager getInstance() {
+        return instance;
+    }
+
+    @PreDestroy
+    private void onDestroy() {
+        instance.shutdown();
+    }
+}
