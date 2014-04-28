1f202eb53ca25587c5c0f31c60d78eca88ed281e
Francois-Xavier Aeberhard <fx@red-agent.com>
2014-04-28 12:05:04 +0200

    PMG - Simplegame test

---
+++ wegas-app/src/main/webapp/wegas-pmg/scripts/wegas-pmg-server-test.js
@@ -0,0 +1,26 @@
+function testsimplepmg() {
+    var task1 = Variable.findByName(self.getGameModel(), 'task 1'),
+            task2 = Variable.findByName(self.getGameModel(), 'task2'),
+            jean = Variable.findByName(self.getGameModel(), 'Jean').getInstance(self),
+            yves = Variable.findByName(self.getGameModel(), 'Yves').getInstance(self),
+            resourceController = lookupBean("ResourceController");
+
+    nextPeriod();                                                               // Avant-projet   
+    
+    task1.getInstance(self).setProperty('bac', '750');                          // Plannification
+    task2.getInstance(self).setProperty('bac', '1500');
+
+    resourceController.addTaskPlannification(task1.getInstance(self).id, 1);
+    resourceController.addTaskPlannification(task2.getInstance(self).id, 2);
+    resourceController.addTaskPlannification(task2.getInstance(self).id, 3);
+
+    resourceController.addAssignment(yves.id, task1);                           // Execution
+    resourceController.addAssignment(yves.id, task2);
+    resourceController.addAssignment(jean.id, task2);
+
+    resourceController.addReservation(yves.id, 1);
+    resourceController.addReservation(yves.id, 2);
+    resourceController.addReservation(yves.id, 3);
+    resourceController.addReservation(jean.id, 2);
+    resourceController.addReservation(jean.id, 3);    
+}
