411d73b9af158383dd8f1dfb1bbe86f4aa5029fb
maxencelaurent <maxence.laurent@gmail.com>
2014-07-16 12:15:36 +0200

    [wegas-pdf] impact pretty printer

--- wegas-app/src/main/java/com/wegas/app/pdf/helper/ImpactPrinter.java
+++ wegas-app/src/main/java/com/wegas/app/pdf/helper/ImpactPrinter.java
@@ -21,9 +21,25 @@ import javax.faces.context.ResponseWriter;
  */
 public class ImpactPrinter {
 
+    private final String script;
 
-    private static List<String> parse(String script) {
-	List<String> instructions = new ArrayList<String>();
+    /**
+     * 
+     * @param script the script to print
+     */
+    public ImpactPrinter(String script){
+	// Replace all new line by a semicolon 
+	this.script = script.replaceAll("\r\n|\n", ";");
+    }
+    
+    /**
+     * 
+     * @param script
+     * @return 
+     */
+    private List<String> parse() {
+	List<String> instructions;
+	instructions = new ArrayList<>();
 
 	boolean inQuotes = false;
 	boolean escaped = false;
@@ -33,7 +49,7 @@ public class ImpactPrinter {
 		case '{':
 		case '}':
 		    b.append(c);
-		    if (!inQuotes){
+		    if (!inQuotes) {
 			instructions.add(b.toString());
 			b = new StringBuilder();
 		    }
@@ -56,7 +72,7 @@ public class ImpactPrinter {
 		    }
 		default:
 		    b.append(c);
-		    if (escaped){
+		    if (escaped) {
 			escaped = false;
 		    }
 		    break;
@@ -66,23 +82,34 @@ public class ImpactPrinter {
 	return instructions;
     }
 
-
-    
-    public static void process(FacesContext context, ResponseWriter writer, String script) throws IOException {
+    /**
+     * Impact Pretty Printer
+     * @param context
+     * @param writer
+     * @throws IOException 
+     */
+    public void print(FacesContext context, ResponseWriter writer) throws IOException {
 	List<String> instructions;
-	instructions = parse(script);
+	instructions = parse();
 	//instructions = script.split("(?s)[;\n](?=(?:(?:.*?(?<!\\\\)\"){2})*[^\"]*$)");
+	UIHelper.startTextArea(writer);
 	for (String instruction : instructions) {
-	    processInstruction(context, writer, instruction);
+	    printInstruction(context, writer, instruction);
 	}
+	UIHelper.endTextArea(writer);
     }
 
-    private static void processInstruction(FacesContext context, ResponseWriter writer, String instruction) throws IOException {
+    private void printInstruction(FacesContext context, ResponseWriter writer, String instruction) throws IOException {
 	Pattern pattern;
 	String str = instruction.trim();
 	//pattern = Pattern.compile("VariableDescriptorFacade.find\\(gameModel, \"*)\"\\)\\.(.*)\\(self, *(.*)\\)");
 
 	if (!str.isEmpty()) {
+	    // Unndent block (Parser create a new instruction after either a "{" or a "}", btw such a char always stands just before $)
+	    if (str.matches(".*\\}$")) {
+		UIHelper.endDiv(writer);
+	    }
+
 	    pattern = Pattern.compile("(VariableDescriptorFacade|Variable)\\.find\\(gameModel, \"(.*)\"\\)\\.(.*)\\(self, *(.*)\\)");
 
 	    Matcher matcher = pattern.matcher(str);
@@ -94,18 +121,26 @@ public class ImpactPrinter {
 		    String operator = matcher.group(3);
 		    String value = matcher.group(4);
 
-		    if ("sendMessage".equals(operator)) {
+		    /*
+		     * Detect specific operation
+		     */
+		    if ("sendMessage".equals(operator)) { // e-mail like message
 			String[] args;
-			args = value.split("(?s)[,](?=(?:(?:.*?(?<!\\\\)\"){2})*[^\"]*$)");
+			args = value.split("(?s)[,](?=(?:(?:.*?(?<!\\\\)\"){2})*[^\"]*$)"); // arg separation
 			UIHelper.printMessage(context, writer, variableAlias, args[0], args[1], args[2]);
 		    } else {
 			String print = variableAlias + " " + operator + " " + value;
-			UIHelper.printTextArea(context, writer, print, UIHelper.CSS_CLASS_PROPERTY_VALUE_TEXTAREA + " " + UIHelper.CSS_CLASS_SOURCE_CODE, true);
+			UIHelper.printTextAreaText(context, writer, print, UIHelper.CSS_CLASS_PROPERTY_VALUE_TEXTAREA + " " + UIHelper.CSS_CLASS_SOURCE_CODE, true);
 		    }
 		}
 	    } else {
-		UIHelper.printTextArea(context, writer, instruction, UIHelper.CSS_CLASS_PROPERTY_VALUE_TEXTAREA + " " + UIHelper.CSS_CLASS_SOURCE_CODE, true);
+		UIHelper.printTextAreaText(context, writer, instruction, UIHelper.CSS_CLASS_PROPERTY_VALUE_TEXTAREA + " " + UIHelper.CSS_CLASS_SOURCE_CODE, true);
 	    }
+
+	    // Indent block (Parser create a new instruction after a "{", btw such a char always stands just before $)
+	    if (str.matches(".*\\{$")) {
+		UIHelper.startDiv(writer, UIHelper.CSS_CLASS_INDENT_CODE);
+	    } 
 	}
     }
 }
