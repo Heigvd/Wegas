fd5dbb2f77f152822f17774f34b4ec5994c42061
Yannick Lagger <yannick.lagger@heig-vd.ch>
2014-08-12 17:47:30 +0200

    Fix bug plugin PageeditorFullWidthTab

--- wegas-app/src/main/webapp/wegas-editor/js/plugin/wegas-pageeditor-fullwidthtab.js
+++ wegas-app/src/main/webapp/wegas-editor/js/plugin/wegas-pageeditor-fullwidthtab.js
@@ -14,16 +14,32 @@ YUI.add("wegas-pageeditor-fullwidthtab", function(Y) {
 
     Y.Plugin.PageeditorFullWidthTab = Y.Base.create("wegas-pageeditor-fullwidthtab", Y.Plugin.Base, [Y.Wegas.Plugin, Y.Wegas.Editable], {
         initializer: function() {
-            var host = this.get("host"), layoutCenter;
-            host.on("selectedChange", function(e) {
+            this.handlers = [];
+            var host = this.get("host"), layoutCenter, tabview, item;
+            this.onceAfterHostEvent("render", function() {
+                tabview = host.get("parent");
                 layoutCenter = host.get("root").get("boundingBox").ancestor();
-                if (e.newVal === 0) {
-                    layoutCenter.setStyle("left", this.oldPosition);
-                } else {
-                    this.oldPosition = layoutCenter.getStyle("left");
-                    layoutCenter.setStyle("left", "-8px");
-                }
-            }, this);
+                this.handlers.push(tabview.after("selectionChange", function(e) {
+                    item = tabview.item(e.target.get("selection").get("index"));
+                    if (item.PageeditorFullWidthTab) {
+                        if (!tabview.oldPosition) {
+                            tabview.oldPosition = layoutCenter.getStyle("left");
+                            layoutCenter.setStyle("left", "-8px");
+                        }
+                    } else {
+                        if (tabview.oldPosition) {
+                            layoutCenter.setStyle("left", tabview.oldPosition);
+                            tabview.oldPosition = null;
+                        }
+                    }
+                }));
+            });
+        },
+        destructor: function() {
+            var i;
+            for (i = 0; i < this.handlers.length; i += 1) {
+                this.handlers[i].detach();
+            }
         }
     }, {
         NS: "PageeditorFullWidthTab"
