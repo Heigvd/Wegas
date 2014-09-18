b8be77e7fa355a4c10b553f5adce3f566c1247ea
Maxence Laurent <maxence.laurent@gmail.com>
2014-09-18 15:58:47 +0200

    [Pmg] Minor Fix in Action results and histories styles

--- wegas-app/src/main/webapp/wegas-pmg/scripts/server-scripts/wegas-pmg-server-event-listeners.js
+++ wegas-app/src/main/webapp/wegas-pmg/scripts/server-scripts/wegas-pmg-server-event-listeners.js
@@ -10,7 +10,8 @@
  * @author Maxence Laurent <maxence.laurent@gmail.com>
  */
 Event.on("replyValidate", function(e) {
-    var msg, type = e.choice.getDescriptor().getTitle();
+    var msg, type = e.choice.getDescriptor().getTitle(),
+        replies = e.question.getReplies(), i;
 
     //root = Variable.findParentList(e.question.getDescriptor());
     //root = Variable.findParentList(root);
@@ -27,9 +28,25 @@ Event.on("replyValidate", function(e) {
         }
     }
 
-    // Result:
-    msg += "<b>Result</b><br />";
-    msg += e.reply.getResult().getAnswer();                                     //Reply
+    // result(s)
+    if (replies.size() > 1) {
+        msg += "<b>Results</b><br />";
+    } else {
+        msg += "<b>Result</b><br />";
+    }
+
+    println("Replies: " + replies);
+    println("l(Replies): " + replies.size());
+
+    msg += '<div class="replies">';
+    for (i = replies.size() - 1; i >= 0; i--) {
+        println ("i: " + i);
+        msg += '<div class="replyDiv">';
+        msg += replies.get(i).getResult().getAnswer();
+        msg += "</div>";
+        println("Reply: " + replies.get(i));
+    }
+    msg += "</div>";
 
     PMGHelper.sendHistory(type, e.question.getDescriptor().getTitle(), msg);
 });
