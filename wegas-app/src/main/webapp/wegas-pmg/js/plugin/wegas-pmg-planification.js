cc4df60fa7a6bca0aac0aaba80fc82c696263551
Maxence Laurent <maxence.laurent@gmail.com>
2014-12-12 15:36:42 +0100

    PMG GANTT & Resource reservation: update UI on server response
    * reservation and planification PMG plugins

--- wegas-app/src/main/webapp/wegas-pmg/js/plugin/wegas-pmg-planification.js
+++ wegas-app/src/main/webapp/wegas-pmg/js/plugin/wegas-pmg-planification.js
@@ -35,39 +35,31 @@ YUI.add('wegas-pmg-planification', function(Y) {
             var dt = this.get("host").datatable,
                 time = dt.getColumn(e.currentTarget).time,
                 task = dt.getRecord(e.currentTarget).get("descriptor").getInstance(),
-                cell = dt.getCell(e.currentTarget);
+                cell = dt.getCell(e.currentTarget),
+                action = "POST";
 
-            // v1: based on element state
             if (cell.get("children").size() > 0) {
-                Wegas.Panel.confirmPlayerAction(Y.bind(function() {
-                    cell.setContent("");
-                    this.request(task.get("id"), time, "DELETE");
-                }, this));
-                return;
+                action = "DELETE";
             }
-
-            // v2: based on cache state (need to make sure request is finished)
-            // Y.Array.each(task.get("plannification"), function (planPeriod) {
-            //    if (planPeriod === time) {                                    // remove plannif
-            //        this.request(task.get("id"), time, "DELETE");
-            //        cell.setContent("");
-            //        return;
-            //    }
-            // });
-
             Wegas.Panel.confirmPlayerAction(Y.bind(function() {
-                cell.append('<span class="editable plannification"></span>');
-                this.request(task.get("id"), time, "POST");                         // add plannif
+                this.request(task.get("id"), time, action, cell);
             }, this));
         },
-        request: function(taskInstanceId, time, method) {
-            Wegas.Facade.Variable.sendQueuedRequest({
+        request: function(taskInstanceId, time, method, cell) {
+            Wegas.Facade.Variable.sendRequest({
                 request: "/ResourceDescriptor/Player/" + Wegas.Facade.Game.get('currentPlayerId') + "/Plan/" + taskInstanceId + "/" + time,
                 cfg: {
                     method: method,
                     updateEvent: false
                 },
                 on: {
+                    success: function() {
+                        if (method === "DELETE") {
+                            cell.setContent("");
+                        } else {
+                            cell.append('<span class="editable plannification"></span>');
+                        }
+                    },
                     failure: Y.bind(this.defaultFailureHandler, this)
                 }
             });
