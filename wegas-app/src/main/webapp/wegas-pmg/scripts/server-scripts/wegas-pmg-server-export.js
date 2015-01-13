20fab4577b8f4227143536b4e051395bcdcb21c2
Maxence Laurent <maxence.laurent@gmail.com>
2015-01-13 11:13:12 +0100

    PMG Export Team Path
    * Add message date to export; some jslint'in

--- wegas-app/src/main/webapp/wegas-pmg/scripts/server-scripts/wegas-pmg-server-export.js
+++ wegas-app/src/main/webapp/wegas-pmg/scripts/server-scripts/wegas-pmg-server-export.js
@@ -15,93 +15,94 @@
  * @author Maxence Laurent <maxence.laurent@gmail.com>
  */
 
-
+/*global Variable, gameModel, self */
 var PMGExport = (function() {
     "use strict";
 
-    function findQuestionAndResult(message){
-        var  qDesc, resultAnswer, type, i, j, k, l,
-            question, choice, result, textAnswer;
-        
+    function findQuestionAndResult(message) {
+        var i, j, k,
+            question, choice, result, textAnswer,
+            candidates;
+
         candidates = Variable.findByTitle(gameModel, message.getSubject());
-        
+
         /* Msg Body is like :
-        
-            <b>Question Title</b> QUESTION DESCRIPTION....
-            Result(s)
-            <div class="replyDiv"><p>BlahBlah1</p></div>
-            <div class="replyDiv"></p>BlahBlah2</p></div>
-        
-           We're looking for blahblah1 only !
+         
+         <b>Question Title</b> QUESTION DESCRIPTION....
+         Result(s)
+         <div class="replyDiv"><p>BlahBlah1</p></div>
+         <div class="replyDiv"></p>BlahBlah2</p></div>
+         
+         We're looking for blahblah1 only !
          */
         textAnswer = message.getBody().match(/<div class="replyDiv"><p>((?:.(?!<\/p><\/div><div class="replyDiv"))*)<\/p><\/div>/)[1];
-        
-        for (i=0 ; i<candidates.size(); i++){ // Question Iterator
+
+        for (i = 0; i < candidates.size(); i += 1) { // Question Iterator
             question = candidates.get(i);
-            for (j=0; j<question.getItems().size(); j++){ // Choice iterator
+            for (j = 0; j < question.getItems().size(); j += 1) { // Choice iterator
                 choice = question.getItems().get(j);
-                for (k=0; k<choice.getResults().size(); k++){
+                for (k = 0; k < choice.getResults().size(); k += 1) {
                     result = choice.getResults().get(k);
-                    if (result.getAnswer().match(textAnswer)){
+                    if (result.getAnswer().match(textAnswer)) {
                         // MATCH FOUND
-                        return {"question" : question.getName(), "result" : k};
+                        return {"question": question.getName(), "result": k, "date": message.getDate()};
                     }
                 }
             }
         }
     }
 
-    function processTeam(team){
+    function processTeam(team) {
         var i, path = [], messages, msg;
 
         messages = Variable.find(gameModel, "history").getInstance(team.getPlayers().get(0)).getMessages();
-        for (i=0; i<messages.size(); i++){
+        for (i = 0; i < messages.size(); i += 1) {
             msg = messages.get(i);
             path.push(findQuestionAndResult(msg));
         }
-        
+
         return path;
     }
-    
 
-    function getActiveTeams(){
-         var i,
+
+    function getActiveTeams() {
+        var i,
             games = gameModel.getGames(),
             allTeams, team, teams = [];
-        
-        if (games.size() > 0){
+
+        if (games.size() > 0) {
             allTeams = games.get(0).getTeams();
             // extract teams
-            if (allTeams.size() === 1){
+            if (allTeams.size() === 1) {
                 teams.push(allTeams.get(0)); // GameModels only contains a debug teams           
             } else { // Real Game case : do NOT consider DebugGames nor empty ones
-                for (i = 0; i< allTeams.size() ; i += 1){
+                for (i = 0; i < allTeams.size(); i += 1) {
                     team = allTeams.get(i);
                     if (team.getClass().getSimpleName() == "Team"
-                        && team.getPlayers().size() > 0){
+                        && team.getPlayers().size() > 0) {
                         teams.push(team);
                     }
                 }
             }
         }
-        
+
         return teams;
     }
 
-    function generateHistory(){
+    function generateHistory() {
         var i, teams = getActiveTeams(), team, result = {};
-        
-        for (i in teams){
+
+        for (i = 0; i < teams.length; i += 1) {
             team = teams[i];
             result[team.getName()] = processTeam(team);
         }
-        
+
         return result;
     }
 
     return {
-        generateHistory : function() {
+        generateHistory: function() {
             return generateHistory();
         }
-    }
+    };
 }());
