beb8dbf7bec705f2d7f2cb6d8f409bbe4c59ea01
Cyril Junod <cyril.junod@gmail.com>
2015-03-02 17:17:52 +0100

    build: sourcemaps path fixes
    remove "-min" from sources

--- wegas-app/gulpfile.js
+++ wegas-app/gulpfile.js
@@ -2,7 +2,6 @@
 
 var gulp       = require('gulp'),
     chug       = require('gulp-chug'),
-    watch      = require("gulp-watch"),
     uglify     = require("gulp-uglify"),
     minifycss  = require('gulp-cssmin'),
     sourcemaps = require("gulp-sourcemaps"),
@@ -17,6 +16,13 @@ gulp.task('default', ["submodule", "compress-css", "compress-js"], function() {
         .pipe(replace(/sourceMappingURL=([\.\/]*)map/g, "sourceMappingURL=/map"))
         .pipe(gulp.dest("target/Wegas"));
 });
+gulp.task("dev", ["submodule", "compress-css", "compress-js"], function(){
+    "use strict";
+    /*@Hack combo support ...*/
+    gulp.src("target/Wegas/**/*-min.js")
+        .pipe(replace(/sourceMappingURL=([\.\/]*)map/g, "sourceMappingURL=/Wegas/map"))
+        .pipe(gulp.dest("target/Wegas"));
+});
 gulp.task("submodule", function() {
     "use strict";
     return gulp.src('target/Wegas/*/gulpfile.js', {readFile: false})
@@ -29,8 +35,8 @@ gulp.task("compress-css", function() {
             "!**/*-min.css"],
         {base: "target/Wegas"})
         .pipe(sourcemaps.init())
-        .pipe(rename({suffix: "-min"}))
         .pipe(cache(minifycss()))
+        .pipe(rename({suffix: "-min"}))
         .pipe(sourcemaps.write("map"))
 
         .pipe(gulp.dest("target/Wegas"));
@@ -43,23 +49,17 @@ gulp.task("compress-js", function() {
             "!**/gulpfile.js"],
         {base: "target/Wegas"})
         .pipe(sourcemaps.init())
-        .pipe(rename({suffix: "-min"}))
         .pipe(cache(uglify()))
+        .pipe(rename({suffix: "-min"}))
         .pipe(sourcemaps.write("map",
             {
                 includeContent: false,
-                sourceRoot: "./"
+                sourceRoot: "/"
             }))
+
         .pipe(gulp.dest("target/Wegas"));
 });
 gulp.task('clear', function(done) {
     "use strict";
     return cache.clearAll(done);
-});
-gulp.task("watch", function() {
-    "use strict";
-    return watch("target/Wegas/**/*.hbs", function() {
-        return gulp.src('target/Wegas/*/gulpfile.js')
-            .pipe(chug({tasks: ["compile-template"]}));
-    });
 });
