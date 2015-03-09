debe419c510ae139c26779021f537e81a8b690b3
Jerome Freyre <jerome.freyre@gmail.com>
2015-03-09 14:57:02 +0100

    - Initial import

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('albasimchWww', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('albasimchWww')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
