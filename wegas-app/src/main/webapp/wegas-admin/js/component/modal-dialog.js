eee1bce85f4c8e35a78b75eaf66f54ef8e9cdb29
Cyril Junod <cyril.junod@gmail.com>
2015-03-11 15:58:51 +0100

    Admin: improve UI

---
+++ wegas-app/src/main/webapp/wegas-admin/js/component/modal-dialog.js
@@ -0,0 +1,25 @@
+/*
+ * Wegas
+ * http://wegas.albasim.ch
+ *
+ * Copyright (c) 2015 School of Business and Engineering Vaud, Comem
+ * Licensed under the MIT License
+ */
+
+/*global define*/
+define(["ember", "templates/components/modal-dialog"], function(Ember) {
+    "use strict";
+    var ModalDialogComponent = Ember.Component.extend({
+        actions: {
+            close: function() {
+                return this.sendAction();
+            }
+        }
+    });
+    Ember.Application.initializer({
+        name: "hh",
+        initialize: function(container, application) {
+            application.register("component:modal-dialog", ModalDialogComponent);
+        }
+    });
+});
