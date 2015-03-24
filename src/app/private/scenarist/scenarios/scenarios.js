6d1be9de91dd1634434cce3ef28cbe38fc199731
Jerome Freyre <jerome.freyre@gmail.com>
2015-03-24 10:54:35 +0100

    - Scenario management for scenarist

--- src/app/private/scenarist/scenarios/scenarios.js
+++ src/app/private/scenarist/scenarios/scenarios.js
@@ -1,27 +1,17 @@
-angular.module('private.scenarist.scenarios', [
-    'private.scenarist.scenarios.new',
-    'private.scenarist.scenarios.edit',
-    'private.scenarist.scenarios.users',
-    'private.scenarist.scenarios.download'
+angular
+.module('private.scenarist.scenarios', [
+    'private.scenarist.scenarios.directives'
 ])
 .config(function ($stateProvider) {
     $stateProvider
         .state('wegas.private.scenarist.scenarios', {
             url: '/scenarios',
             views: {
-                'scenarios-new':{
-                    controller: 'ScenariosNewCtrl as scenariosNewCtrl',
-                    templateUrl: 'app/private/scenarist/scenarios/scenarios-new/scenarios-new.tmpl.html'
-                },
-                'scenarios-list': {
-                    controller: 'ScenariosListCtrl as scenariosListCtrl',
+                'workspace@wegas.private': {
+                    controller: 'ScenaristCtrl as scenaristCtrl',
                     templateUrl: 'app/private/scenarist/scenarios/scenarios.tmpl.html'
                 }
             }
         })
     ;
-})
-.controller('ScenariosListCtrl', function ScenariosListCtrl($state) {
-    var scenariosListCtrl = this;
-    console.log("Loadings scenario list");
 });
