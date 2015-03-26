a645ad4a4b5bace8895a6b04f9ff6a813e7bfbdd
Jerome Freyre <jerome.freyre@gmail.com>
2015-03-26 15:53:27 +0100

    - Implements history management

--- src/app/private/scenarist/history/history-directives.js
+++ src/app/private/scenarist/history/history-directives.js
@@ -9,7 +9,37 @@ angular
             var ctrl = this,
             scenarios = [],
             scenario = null,
-            history = null;
+            versions = [];
+
+            ctrl.scenarioId = $stateParams.scenarioId;
+
+            ctrl.updateVersions = function () {
+                ScenariosModel.getVersionsHistory($stateParams.scenarioId).then(function(results) {
+                    if (results === false) {
+                        window.alert('Whooops.');
+                    } else {
+                        ctrl.versions = results;
+                    }
+                });
+            }
+            ScenariosModel.getScenario($stateParams.scenarioId).then(function (scenario) {
+                ctrl.scenario = scenario;
+                ctrl.updateVersions();
+            });
+
+            $scope.addVersion = function() {
+                if($stateParams.scenarioId !== "") {
+                    ScenariosModel.addVersionHistory($stateParams.scenarioId).then(function (result) {
+                        if (result === true) {
+                            ctrl.updateVersions();
+                        }
+                    });
+                }
+            };
+
+
+
+
 
 
         }
@@ -31,12 +61,42 @@ angular
 .directive('scenaristHistoryList', function(ScenariosModel){
     return {
         templateUrl: 'app/private/scenarist/history/tmpl/history-list.html',
-        controller : function($scope, $stateParams, $sce) {
-            var ctrl = this,
-            scenarios = [],
-            scenario = null,
-            history = null;
+        scope: false,
+        require: "^scenaristHistoryIndex",
+        link : function(scope, element, attrs, parentCtrl) {
 
+
+            scope.$watch(function() {
+                return parentCtrl.versions
+            } , function(n,o) {
+                scope.versions = n;
+            });
+
+        }
+    };
+})
+.directive('scenaristHistoryCard', function(ScenariosModel){
+    return {
+        templateUrl: 'app/private/scenarist/history/tmpl/history-card.html',
+        scope: false,
+        require: "^scenaristHistoryIndex",
+        link : function($scope, element, attrs, parentCtrl) {
+
+            $scope.deleteFork = function(name) {
+                ScenariosModel.deleteVersionHistory(parentCtrl.scenarioId, name).then(function (result) {
+                    if (result === true) {
+                        parentCtrl.updateVersions();
+                    }
+                });
+            };
+
+            $scope.createFork = function(name) {
+                ScenariosModel.restoreVersionHistory(parentCtrl.scenarioId, name).then(function (result) {
+                    if (result !== false) {
+                        alert('Le scenario a été dupliqué sous le nom "'+result.name+'"');
+                    }
+                });
+            };
 
         }
     };
