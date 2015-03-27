801daa75bbc4181800215f0ffd6fc8af8a6fc436
Rapou <raphael.schmutz@heig-vd.ch>
2015-03-27 17:19:48 +0100

    Common Autocomplete

---
+++ src/app/commons/directives/search-users.js
@@ -0,0 +1,55 @@
+angular.module('wegas.directives.search.users', [
+    'ngSanitize',
+    'wegas.behaviours.autocomplete',
+    'MassAutoComplete'
+])
+.directive('searchUsers', function($sce, UsersModel) {
+    return {
+        templateUrl: 'app/commons/directives/search-users.tmpl.html',
+        scope: {
+            callback: "=",
+            placeholder: "@",
+            inputStyle: "@"
+        },
+        link: function(scope, element, attrs) {
+            function suggest_obj(term) {
+                return UsersModel.autocomplete(term).then(function(matches) {
+                    var results = [];
+
+                    function highlight(str, term) {
+                        var highlight_regex = new RegExp('(' + term + ')', 'gi');
+                        return str.replace(highlight_regex,
+                            '<strong>$1</strong>');
+                    };
+
+                    for (var i = 0; i < matches.length; i++) {
+                        var user = matches[i];
+                        results.push({
+                            value: user.name + '('+user.email+')',
+                            obj: user,
+                            label:
+                            '<div class="row">' +
+                            ' <div class="col-xs-6">' +
+                            '  ' + highlight(user.name, term) +
+                            ' </div>' +
+                            ' <div class="col-xs-6 text-right">' +
+                            highlight(user.email, term) +
+                            ' </div>' +
+                            '</div>'
+
+                        });
+                    }
+                    return results;
+                });
+            }
+            function selectUser(selected) {
+              scope.callback(selected.obj);
+            };
+            scope.autocomplete_options = {
+                suggest: suggest_obj,
+                on_select: selectUser
+            };
+        }
+    };
+})
+;
--- src/app/private/scenarist/coscenarists/coscenarists-directives.js
+++ src/app/private/scenarist/coscenarists/coscenarists-directives.js
@@ -1,8 +1,7 @@
 angular
 .module('private.scenarist.coscenarists.directives', [
-    'ngSanitize',
-    'MassAutoComplete'
-    ])
+    "wegas.directives.search.users"
+])
 .directive('scenaristCoscenaristsIndex', function(ScenariosModel){
   return {
     templateUrl: 'app/private/scenarist/coscenarists/tmpl/coscenarists-index.html',
@@ -41,7 +40,6 @@ angular
     }
 };
 })
-
 .directive('scenaristCoscenaristsAdd', function(ScenariosModel, UsersModel) {
     return {
         templateUrl: 'app/private/scenarist/coscenarists/tmpl/coscenarists-add.tmpl.html',
@@ -55,6 +53,11 @@ angular
                 scope.scenario = n;
             });
 
+            scope.callbackSearchUser = function(selection) {
+                scope.selected_user = selection;
+                scope.addNewCoscenarist();
+            }
+
             scope.addNewCoscenarist = function() {
                 if (scope.selected_user.id) {
                     ScenariosModel.updatePermissions(parentCtrl.scenario.id,
@@ -63,58 +66,7 @@ angular
                     });
                 }
             };
-
-            function suggest_obj(term) {
-
-                return UsersModel.autocomplete(term).then(function(matches) {
-                    var results = [];
-
-                    function highlight(str, term) {
-                        var highlight_regex = new RegExp('(' + term + ')', 'gi');
-                        return str.replace(highlight_regex,
-                            '<strong>$1</strong>');
-                    };
-
-                    for (var i = 0; i < matches.length; i++) {
-                        var user = matches[i];
-                        results.push({
-                            value: user.name + '('+user.email+')',
-                            obj: user,
-                            label:
-                            '<div class="row">' +
-                            ' <div class="col-xs-12">' +
-                            '  ' + highlight(user.name, term) +
-                            ' </div>' +
-                            ' <div class="col-xs-12 text-right">' +
-                            highlight(user.email, term) +
-                            ' </div>' +
-                            '</div>'
-
-                        });
-                    }
-                    return results;
-
-                });
-            }
-            scope.autocomplete_options = {
-                suggest: suggest_obj,
-                on_select: function (selected) {
-                    scope.selected_user = selected.obj;
-                }
-            };
         }
-    };
-})
-.directive('autoComplete', function($timeout) {
-    return function(scope, iElement, iAttrs) {
-        iElement.autocomplete({
-            source: scope[iAttrs.uiItems],
-            select: function() {
-                $timeout(function() {
-                  iElement.trigger('input');
-              }, 0);
-            }
-        });
     };
 })
 .directive('scenaristCoscenaristsList', function(ScenariosModel) {
