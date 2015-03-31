fbef07183a12d092e4bbb5801bfc6b9ca794bd90
Rapou <raphael.schmutz@heig-vd.ch>
2015-03-31 11:58:44 +0200

    Gestion valider token et affichage teams

--- src/app/private/player/sessions/sessions-directives.js
+++ src/app/private/player/sessions/sessions-directives.js
@@ -1,23 +1,33 @@
-angular.module('private.player.sessions.directives', [
-])
-.directive('playerSessionsIndex', function(SessionsModel){
-  return {
-    templateUrl: 'app/private/player/sessions/sessions-directives.tmpl/sessions-index.tmpl.html',
-    controller : function(){
-        var ctrl = this;
-        ctrl.sessions = [];
+angular.module('private.player.sessions.directives', [])
+.directive('playerSessionsIndex', function(){
+    return {
+        templateUrl: 'app/private/player/sessions/sessions-directives.tmpl/sessions-index.tmpl.html',
+        controller : 'PlayerSessionsIndexController as playerSessionsIndexCtrl'
+    };
+}).controller("PlayerSessionsIndexController", function PlayerSessionsIndexController(SessionsModel){
+    var ctrl = this;
+    ctrl.sessions = [];
+    ctrl.searchTeam = "";
+    ctrl.sessionJoinable = false;
+    ctrl.sessionToJoin = {};
+    SessionsModel.getPlayedSessions().then(function(sessions){
+        ctrl.sessions = sessions;
+    });
+    ctrl.updateSessions = function(){
         SessionsModel.getPlayedSessions().then(function(sessions){
             ctrl.sessions = sessions;
         });
-        ctrl.updateSessions = function(){
-            SessionsModel.getPlayedSessions().then(function(sessions){
-                ctrl.sessions = sessions;
-            });
-        }
-    }
-  };
-})
+    };
 
+    ctrl.changeSessionToJoin = function(newSession){
+        ctrl.sessionJoinable = true;
+        ctrl.sessionToJoin = newSession;
+    };
+
+    ctrl.updateSearchTeam = function(newName){
+        ctrl.searchTeam = newName;
+    };
+})
 .directive('playerSessionJoin', function($state, ScenariosModel, SessionsModel) {
   return {
     templateUrl: 'app/private/player/sessions/sessions-directives.tmpl/session-join.tmpl.html',
@@ -25,8 +35,32 @@ angular.module('private.player.sessions.directives', [
     require: "^playerSessionsIndex",
     link : function(scope, element, attrs, parentCtrl){
         scope.sessionToJoin = {
-            token : ""
+            token : "",
+            joinable: false
         };
+
+        var valideToken = function(session){
+            if($("#session-token").hasClass("input--state-danger")){
+                $("#session-token").removeClass("input--state-danger");
+            }
+            $("#session-token").addClass("input--state-success").attr("disabled", "disabled");
+            scope.sessionToJoin.joinable = true;
+            parentCtrl.changeSessionToJoin(session);
+
+            scope.teamToSelect = {
+                name : ""
+            }
+
+            scope.$watch(function(){
+                return scope.teamToSelect.name
+            }, function(newName, oldName){
+                parentCtrl.updateSearchTeam(newName)
+            });
+        }
+        var invalideToken = function(){
+            $("#session-token").addClass("input--state-danger");
+        }
+
         scope.joinSession = function(){
             SessionsModel.findSessionToJoin(scope.sessionToJoin.token).then(function(session){
                 if(session){
@@ -36,10 +70,10 @@ angular.module('private.player.sessions.directives', [
                             parentCtrl.updateSessions;
                         });
                     }else{
-                        $state.go("wegas.private.player.sessions.join", {"id": session.id});
+                        valideToken(session);
                     }
                 }else{
-                    console.log("Error - No session found");
+                    invalideToken();
                 }
             });
         };
@@ -59,4 +93,18 @@ angular.module('private.player.sessions.directives', [
         });
     }
   };
-});
+})
+.directive('playerSessionTeams', function(ScenariosModel, SessionsModel) {
+  return {
+    templateUrl: 'app/private/player/sessions/sessions-directives.tmpl/session-teams.tmpl.html',
+    scope: {
+        sessionToJoin : "=",
+        searchTeam : "="
+    },
+    require: "^playerSessionsIndex",
+    link : function(scope, element, attrs, parentCtrl){
+
+    }
+  };
+})
+;
