d06fed2324521cd82a99e0f1aca877dc0f074b15
Rapou <raphael.schmutz@heig-vd.ch>
2015-04-08 16:18:53 +0200

    Route Player Session Team

--- src/app/private/player/sessions/session-team/session-team-directives.js
+++ src/app/private/player/sessions/session-team/session-team-directives.js
@@ -7,24 +7,42 @@ angular.module('private.player.session.team.directives', [])
         },
         controller: 'PlayerSessionTeamController as playerSessionTeamCtrl'
     };
-}).controller('PlayerSessionTeamController', function PlayerSessionTeamController($scope, $stateParams, SessionsModel){
+}).controller('PlayerSessionTeamController', function PlayerSessionTeamController($scope, $state, $stateParams, Auth, SessionsModel){
     /* Assure access to ctrl. */
     var ctrl = this;
-
     /* Container for datas */
-    ctrl.playedSession = {};
-
+    ctrl.team = {};
+    ctrl.sessionName = "";
     /* Initialize datas */
-    SessionsModel.getPlayedSession($stateParams.id).then(function(response){
-        if(response.data){
-            var session = response.data;
-            if(!session.properties.freeForAll){
-                ctrl.playedSession = session;
-            }else{
-                $scope.close();
-            }
+    Auth.getAuthenticatedUser().then(function(user){
+        if(user !== null){
+            SessionsModel.getPlayedSession($stateParams.id).then(function(response){
+                if(response.isErroneous){
+                    $scope.close();
+                }else{
+                    if(!response.data.properties.freeForAll){
+                        ctrl.sessionName = response.data.name;
+                        response.data.teams.forEach(function(team){
+                            team.players.forEach(function(player){
+                                if(user.id == player.userId){
+                                    ctrl.team = team;
+                                }
+                            });
+                        });
+                    }else{
+                        $scope.close();
+                    }
+                }
+            });
         }else{
             $scope.close();
         }
     });
+}).directive('playerSessionTeamPlayersList', function() {
+  return {
+    templateUrl: 'app/private/player/sessions/session-team/session-team-directives.tmpl/session-team-players-list.tmpl.html',
+    scope: {
+        players : "="
+    }
+  };
 });
