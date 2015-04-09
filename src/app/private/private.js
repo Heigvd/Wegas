9945bda53ce370698ead19b9b77dc95998214f13
Jerome Freyre <jerome.freyre@gmail.com>
2015-04-09 14:16:14 +0200

    - code cleaning private

--- src/app/private/private.js
+++ src/app/private/private.js
@@ -9,44 +9,43 @@ angular.module('private', [
     'private.logout',
     'private.directives'
 ])
-.config(function ($stateProvider) {
-    $stateProvider
-        .state('wegas.private', {
-            url: '',
-            abstract:true,
-            views: {
-                'main@': {
-                    controller: 'PrivateCtrl as privateCtrl',
-                    templateUrl: 'app/private/private.tmpl.html'
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('wegas.private', {
+                url: '',
+                abstract: true,
+                views: {
+                    'main@': {
+                        controller: 'PrivateCtrl as privateCtrl',
+                        templateUrl: 'app/private/private.tmpl.html'
+                    }
                 }
+            });
+    })
+    .controller('PrivateCtrl', function PrivateCtrl($state, Auth, $scope) {
+        var privateCtrl = this;
+        privateCtrl.loading = 0
+        $scope.$on('cfpLoadingBar:loading', function() {
+            if (privateCtrl.loading == 0) {
+                $('.view--top').addClass('view--loading');
+            }
+            privateCtrl.loading++;
+        })
+        $scope.$on('cfpLoadingBar:started', function() {
+
+        })
+        $scope.$on('cfpLoadingBar:loaded', function() {
+            privateCtrl.loading--;
+        })
+        $scope.$on('cfpLoadingBar:completed', function() {
+            if (privateCtrl.loading == 0) {
+                $('.view--top').removeClass('view--loading');
             }
         })
-    ;
-})
-.controller('PrivateCtrl', function PrivateCtrl($state, Auth, $scope) {
-    var privateCtrl = this;
-    privateCtrl.loading = 0
-    $scope.$on('cfpLoadingBar:loading', function () {
-        if (privateCtrl.loading == 0) {
-            $('.view--top').addClass('view--loading');
-        }
-        privateCtrl.loading++;
-    })
-    $scope.$on('cfpLoadingBar:started', function () {
-
-    })
-    $scope.$on('cfpLoadingBar:loaded', function () {
-        privateCtrl.loading--;
-    })
-    $scope.$on('cfpLoadingBar:completed', function () {
-        if (privateCtrl.loading == 0) {
-            $('.view--top').removeClass('view--loading');
-        }
-    })
-    Auth.getAuthenticatedUser().then(function(user){
-        if(user == null){
-            $state.go("wegas.public");
-        }
-        privateCtrl.user = user;
+        Auth.getAuthenticatedUser().then(function(user) {
+            if (user == null) {
+                $state.go("wegas.public");
+            }
+            privateCtrl.user = user;
+        });
     });
-});
