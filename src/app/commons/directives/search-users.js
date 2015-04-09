11558151251d0d193ca97a675298c812778a250c
Jerome Freyre <jerome.freyre@gmail.com>
2015-04-09 14:10:28 +0200

    - Code cleaning in directives

--- src/app/commons/directives/search-users.js
+++ src/app/commons/directives/search-users.js
@@ -3,55 +3,54 @@ angular.module('wegas.directives.search.users', [
     'wegas.behaviours.autocomplete',
     'MassAutoComplete'
 ])
-.directive('searchUsers', function($sce, UsersModel) {
-    return {
-        templateUrl: 'app/commons/directives/search-users.tmpl.html',
-        scope: {
-            callback: "=",
-            placeholder: "@",
-            inputStyle: "@",
-            restrictRoles: "="
-        },
-        link: function(scope, element, attrs) {
-            function suggest_obj(term) {
-                return UsersModel.autocomplete(term, scope.restrictRoles).then(function(matches) {
-                    var results = [];
+    .directive('searchUsers', function($sce, UsersModel) {
+        return {
+            templateUrl: 'app/commons/directives/search-users.tmpl.html',
+            scope: {
+                callback: "=",
+                placeholder: "@",
+                inputStyle: "@",
+                restrictRoles: "="
+            },
+            link: function(scope, element, attrs) {
+                function suggest_obj(term) {
+                    return UsersModel.autocomplete(term, scope.restrictRoles).then(function(matches) {
+                        var results = [];
 
-                    function highlight(str, term) {
-                        var highlight_regex = new RegExp('(' + term + ')', 'gi');
-                        return str.replace(highlight_regex,
-                            '<strong>$1</strong>');
-                    };
+                        function highlight(str, term) {
+                            var highlight_regex = new RegExp('(' + term + ')', 'gi');
+                            return str.replace(highlight_regex,
+                                '<strong>$1</strong>');
+                        };
 
-                    for (var i = 0; i < matches.length; i++) {
-                        var user = matches[i];
-                        results.push({
-                            value: user.name + '('+user.email+')',
-                            obj: user,
-                            label:
-                            '<div class="row">' +
-                            ' <div class="col-xs-6">' +
-                            '  ' + highlight(user.name, term) +
-                            ' </div>' +
-                            ' <div class="col-xs-6 text-right">' +
-                            highlight(user.email, term) +
-                            ' </div>' +
-                            '</div>'
+                        for (var i = 0; i < matches.length; i++) {
+                            var user = matches[i];
+                            results.push({
+                                value: user.name + '(' + user.email + ')',
+                                obj: user,
+                                label: '<div class="row">' +
+                                    ' <div class="col-xs-6">' +
+                                    '  ' + highlight(user.name, term) +
+                                    ' </div>' +
+                                    ' <div class="col-xs-6 text-right">' +
+                                    highlight(user.email, term) +
+                                    ' </div>' +
+                                    '</div>'
 
-                        });
-                    }
-                    return results;
-                });
+                            });
+                        }
+                        return results;
+                    });
+                }
+
+                function selectUser(selected) {
+                    scope.callback(selected.obj);
+                    scope.dirty.value = "";
+                };
+                scope.autocomplete_options = {
+                    suggest: suggest_obj,
+                    on_select: selectUser
+                };
             }
-            function selectUser(selected) {
-              scope.callback(selected.obj);
-              scope.dirty.value = "";
-            };
-            scope.autocomplete_options = {
-                suggest: suggest_obj,
-                on_select: selectUser
-            };
-        }
-    };
-})
-;
+        };
+    });
