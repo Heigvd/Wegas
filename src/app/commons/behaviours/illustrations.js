b52885e8f4ea241286827c69231c4d7e13823ba1
Jerome Freyre <jerome.freyre@gmail.com>
2015-04-09 14:09:23 +0200

    - Code cleaning

--- src/app/commons/behaviours/illustrations.js
+++ src/app/commons/behaviours/illustrations.js
@@ -1,8 +1,7 @@
-
 angular.module('wegas.behaviours.illustrations', [])
-.directive('illustrationEditable', function(){
-	return function(scope, element, attrs){
-     	$(element).addClass("card__illustration--editable");
-     	// Implement events around editor box with icons and colors palette
-	};
-});
+    .directive('illustrationEditable', function() {
+        return function(scope, element, attrs) {
+            $(element).addClass("card__illustration--editable");
+            // Implement events around editor box with icons and colors palette
+        };
+    });
