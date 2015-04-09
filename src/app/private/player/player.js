1bfbe5f89f16d0ad79d062c2b5d67d512ef48b06
Jerome Freyre <jerome.freyre@gmail.com>
2015-04-09 14:14:07 +0200

    - Code cleaning player

--- src/app/private/player/player.js
+++ src/app/private/player/player.js
@@ -1,24 +1,23 @@
 angular.module('private.player', [
     'private.player.sessions'
 ])
-.config(function ($stateProvider) {
-    $stateProvider
-        .state('wegas.private.player', {
-            url: 'player',
-            views: {
-                'workspace': {
-                    controller: 'PlayerCtrl as playerCtrl',
-                    templateUrl: 'app/private/player/sessions/sessions.tmpl.html'
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('wegas.private.player', {
+                url: 'player',
+                views: {
+                    'workspace': {
+                        controller: 'PlayerCtrl as playerCtrl',
+                        templateUrl: 'app/private/player/sessions/sessions.tmpl.html'
+                    }
                 }
+            });
+    })
+    .controller('PlayerCtrl', function PlayerCtrl($state, Auth, ViewInfos) {
+        var playerCtrl = this;
+        Auth.getAuthenticatedUser().then(function(user) {
+            if (user != null) {
+                ViewInfos.editName("Player workspace");
             }
-        })
-    ;
-})
-.controller('PlayerCtrl', function PlayerCtrl($state, Auth, ViewInfos) {
-    var playerCtrl = this;
-    Auth.getAuthenticatedUser().then(function(user){
-        if(user != null){
-            ViewInfos.editName("Player workspace");
-        }
+        });
     });
-});
