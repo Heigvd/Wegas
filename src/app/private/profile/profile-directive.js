6bfd69bab758a661224b597d85ebd66452e3bb65
Jerome Freyre <jerome.freyre@gmail.com>
2015-04-09 14:15:04 +0200

    - code cleaning profile

--- src/app/private/profile/profile-directive.js
+++ src/app/private/profile/profile-directive.js
@@ -1,69 +1,69 @@
 angular
-.module('private.profile.directives', [
+    .module('private.profile.directives', [
 
-])
-.directive('profileIndex', function(UsersModel){
-    return {
+    ])
+    .directive('profileIndex', function(UsersModel) {
+        return {
 
-        templateUrl: 'app/private/profile/tmpl/profile-index.html',
-        controller : function($scope, $stateParams, $sce, $rootScope, Auth, flash) {
-            var ctrl = this;
-            $scope.user = {};
-            $scope.originalUser = false
-            Auth.getAuthenticatedUser().then(function (user) {
-                if (user !== false) {
-                    UsersModel.getUser(user.id).then(function (fulluser) {
-                        if (fulluser !== false) {
-                            $scope.user = fulluser;
-                            if ($scope.originalUser === false) {
-                                $scope.originalUsername = fulluser.lastname + ' ' + fulluser.firstname;
+            templateUrl: 'app/private/profile/tmpl/profile-index.html',
+            controller: function($scope, $stateParams, $sce, $rootScope, Auth, flash) {
+                var ctrl = this;
+                $scope.user = {};
+                $scope.originalUser = false
+                Auth.getAuthenticatedUser().then(function(user) {
+                    if (user !== false) {
+                        UsersModel.getUser(user.id).then(function(fulluser) {
+                            if (fulluser !== false) {
+                                $scope.user = fulluser;
+                                if ($scope.originalUser === false) {
+                                    $scope.originalUsername = fulluser.lastname + ' ' + fulluser.firstname;
+                                }
+                            } else {
+                                flash('error', 'Unable to load user informations...');
                             }
-                        } else {
-                            flash('error', 'Unable to load user informations...');
-                        }
-                    });
-                } else {
-                    flash('error', 'Unable to load user informations...');
+                        });
+                    } else {
+                        flash('error', 'Unable to load user informations...');
+                    }
+                });
+
+                ctrl.updateInformations = function() {
+
+                    if ($scope.user.password == $scope.user.password2) {
+
+                        UsersModel.updateUser($scope.user).then(function(result) {
+                            if (result !== false) {
+                                $scope.user.password = '';
+                                flash('Profile updated');
+                            } else {
+
+                            }
+                        })
+
+                    } else {
+                        flash('error', 'Passwords do not matches');
+                    }
                 }
-            });
 
-            ctrl.updateInformations = function () {
+            }
+        };
+    })
+    .directive('profileContent', function() {
+        return {
+            templateUrl: 'app/private/profile/tmpl/profile-content.html',
+            scope: true,
+            require: "^profileIndex",
+            link: function($scope, element, attrs, parentCtrl) {
 
-                if ($scope.user.password == $scope.user.password2) {
-
-                    UsersModel.updateUser($scope.user).then(function (result) {
-                        if (result !== false) {
-                            $scope.user.password = '';
-                            flash('Profile updated');
-                        } else {
-
-                        }
-                    })
-
-                }  else {
-                    flash('error', 'Passwords do not matches');
+                $scope.$watch(function() {
+                    return $scope.$parent.user
+                }, function(n, o) {
+                    $scope.user = n;
+                });
+                $scope.updateInformations = function() {
+                    parentCtrl.updateInformations();
                 }
+
             }
-
-        }
-    };
-})
-.directive('profileContent', function(){
-    return {
-        templateUrl: 'app/private/profile/tmpl/profile-content.html',
-        scope: true,
-        require: "^profileIndex",
-        link : function($scope, element, attrs, parentCtrl) {
-
-            $scope.$watch(function() {
-                return $scope.$parent.user
-            } , function(n,o) {
-                $scope.user = n;
-            });
-            $scope.updateInformations = function() {
-                parentCtrl.updateInformations();
-            }
-
-        }
-    };
-})
+        };
+    })
