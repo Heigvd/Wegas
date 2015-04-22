78a66c56637b7a9c3415e4eac3012aaaf2c6511f
Jerome Freyre <jerome.freyre@gmail.com>
2015-04-22 11:11:10 +0200

    - Create base admin module

---
+++ src/app/private/admin/admin.js
@@ -0,0 +1,31 @@
+angular.module('private.admin', [
+    'private.admin.directives',
+    'private.admin.users',
+    'private.admin.groups',
+    'private.admin.games'
+])
+.config(function ($stateProvider) {
+    $stateProvider
+        .state('wegas.private.admin', {
+            url: 'admin',
+            views: {
+                'workspace': {
+                    controller: 'AdminCtrl as adminCtrl',
+                    templateUrl: 'app/private/admin/directives.tmpl/index.html'
+                }
+            }
+        })
+    ;
+})
+.controller('AdminCtrl', function AdminCtrl($state, Auth, ViewInfos) {
+    var trainerCtrl = this;
+    Auth.getAuthenticatedUser().then(function(user){
+        if(user != null){
+            if(!user.isAdmin){
+                $state.go("wegas.private.scenarist");
+            }
+            ViewInfos.editName("Admin workspace");
+        }
+    });
+})
+;
