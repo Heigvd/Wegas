412a1212ba94eb3140add696da5f3d2bb941f36a
Rapou <raphael.schmutz@heig-vd.ch>
2015-05-20 14:40:12 +0200

    * UX Lobby - Some fix

--- wegas-app/src/main/webapp/wegas-lobby/src/app/private/trainer/users/directives.js
+++ wegas-app/src/main/webapp/wegas-lobby/src/app/private/trainer/users/directives.js
@@ -9,32 +9,57 @@ angular.module('private.trainer.users.directives', [
             templateUrl: 'app/private/trainer/users/directives.tmpl/index.html',
             controller: "TrainerUsersIndexCtrl as usersIndexCtrl"
         };
-    }).controller("TrainerUsersIndexCtrl", function TrainerUsersIndexCtrl($stateParams, SessionsModel, Flash) {
-        var ctrl = this;
-        ctrl.session = {},
+    }).controller("TrainerUsersIndexCtrl", function TrainerUsersIndexCtrl($state, $stateParams, PermissionsModel, SessionsModel, Flash) {
+        var ctrl = this,
+            formatPlayer = function(){
+                ctrl.session.players = [];
+                ctrl.session.teams.forEach(function(team){
+                    team.players.forEach(function(player){
+                        ctrl.session.players.push(player);
+                    });
+                });
+            },
+            updatePermission = function(){
+                PermissionsModel.getSessionPermissions(ctrl.session).then(function(trainers){
+                    ctrl.trainers = trainers;
+                });
+            };
+
+        ctrl.session = {};
+        ctrl.trainers = [];
         ctrl.MAX_DISPLAYED_CHARS = MAX_DISPLAYED_CHARS;
         ctrl.restrictRoles = ["Trainer", "Administrator", "Scenarist"];
-
         ctrl.playersViewActived = true;
+        ctrl.kindsOfSession = ($state.$current.name == "wegas.private.trainer.users") ? "LIVE" : "BIN";
 
         ctrl.refreshSession = function () {
-            SessionsModel.refreshSession("managed", ctrl.session).then(function(response) {
+            SessionsModel.refreshSession(ctrl.kindsOfSession, ctrl.session).then(function(response) {
                 if (!response.isErroneous()) {
                     ctrl.session = response.data;
+                    if(ctrl.session.properties.freeForAll){
+                        formatPlayer();
+                        updatePermission();
+                    }
                 } else {
                     response.flash();
                 }
             });
         };
+
         ctrl.updateSession = function() {
-            SessionsModel.getSession("managed", $stateParams.id, true).then(function(response) {
-                ctrl.session = response.data || {};
-                if (response.isErroneous()) {
+            SessionsModel.getSession(ctrl.kindsOfSession, $stateParams.id).then(function(response) {
+                if (!response.isErroneous()) {
+                    ctrl.session = response.data || {};
+                    if(ctrl.session.properties.freeForAll){
+                        formatPlayer();
+                        updatePermission();
+                    }
+                }else{
                     response.flash();
                 }
             });
         };
-        ctrl.updateSession();
+
         ctrl.activePlayersView = function() {
             ctrl.playersViewActived = true;
         };
@@ -44,23 +69,25 @@ angular.module('private.trainer.users.directives', [
         };
 
         ctrl.addTrainer = function(selection) {
-            SessionsModel.addTrainerToSession($stateParams.id, selection).then(function(response) {
+            PermissionsModel.addSessionPermission(ctrl.session, ctrl.trainers, selection).then(function(response) {
                 if (!response.isErroneous()) {
                     ctrl.updateSession();
                 }else{
                     response.flash();
                 }
             });
-        }
+        };
+
         ctrl.removeTrainer = function(trainerId) {
-            SessionsModel.removeTrainerToSession($stateParams.id, trainerId).then(function(response) {
+            PermissionsModel.removeSessionPermission(ctrl.session, ctrl.trainers, trainerId).then(function(response) {
                 if (!response.isErroneous()) {
                     ctrl.updateSession();
                 }else{
                     response.flash();
                 }
             });
-        }
+        };
+
         ctrl.removePlayer = function(playerId, teamId) {
             SessionsModel.removePlayerToSession($stateParams.id, playerId, teamId).then(function(response) {
                 if (!response.isErroneous()) {
@@ -69,8 +96,9 @@ angular.module('private.trainer.users.directives', [
                     response.flash();
                 }
             });
-        }
+        };
 
+        ctrl.updateSession();
     })
     .directive('trainerSessionsUsersTrainersList', function() {
         return {
