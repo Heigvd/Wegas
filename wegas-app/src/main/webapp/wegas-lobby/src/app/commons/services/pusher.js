0c80fc82d08ca40b96790ca3cc00f78d48985dd2
Rapou <raphael.schmutz@heig-vd.ch>
2015-09-01 14:01:46 +0200

    Lobby : Pusher - Fix auth bug

--- wegas-app/src/main/webapp/wegas-lobby/src/app/commons/services/pusher.js
+++ wegas-app/src/main/webapp/wegas-lobby/src/app/commons/services/pusher.js
@@ -1,18 +1,14 @@
 angular.module('wegas.service.pusher', [])
-    .service('WegasPusher', function($http, $q,  $pusher) {
+    .service('WegasPusher', function($http, $q) {
         var service = this, pusher, channels = [];
         service.start = function(){
             var deferred = $q.defer();
             $http.get(ServiceURL + "rest/Pusher/ApplicationKey").success(function(key) {
-                if(key){
-                    var client = new Pusher(key);
-                    pusher = $pusher(client);
-                    $http.post(ServiceURL + "rest/Pusher/auth", {
-                        "socket_id": client.connection.socket_id,
-                        "channel_name": 'presence-global'
-                    }).success(function(data) {
-                        channels["presence-global"] = pusher.subscribe('presence-global');
+                if(key){                    
+                    pusher = new Pusher(key, {
+                        authEndpoint: ServiceURL + "rest/Pusher/auth"
                     });
+                    channels["presence-global"] = pusher.subscribe('presence-global');
                 }
                 deferred.resolve();
             });
