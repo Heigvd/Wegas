895dc04cc9008a26ff71320e3a48fec3f64fcd38
Maxence Laurent <maxence.laurent@gmail.com>
2015-09-04 08:29:56 +0200

    Wegas-Dashboard
    * replace 'label' by 'id' to identify dashboard columns

--- wegas-app/src/main/webapp/wegas-editor/js/widget/wegas-dashboard-teams.js
+++ wegas-app/src/main/webapp/wegas-editor/js/widget/wegas-dashboard-teams.js
@@ -13,19 +13,19 @@ YUI.add('wegas-teams-dashboard', function(Y) {
     "use strict";
     Y.Wegas.TeamsDashboard = Y.Base.create("wegas-teams-dashboard", Y.Wegas.Dashboard, [], {
         BOUNDING_TEMPLATE: "<div class='dashboard dashboard--teams' />",
-        initializer: function(){
+        initializer: function() {
             var game = Y.Wegas.Facade.Game.cache.getCurrentGame(), cardsData = [],
-                icon = game.get("properties.freeForAll") ? "user": "group",
+                icon = game.get("properties.freeForAll") ? "user" : "group",
                 context = this, teamBlocs;
-            if(game && game.get("teams").length > 0){
-                game.get("teams").forEach(function(team){
-                    if(team.get("@class") !== "DebugTeam" && team.get("players").length > 0){
+            if (game && game.get("teams").length > 0) {
+                game.get("teams").forEach(function(team) {
+                    if ((game.get("@class") === "DebugGame" || team.get("@class") !== "DebugTeam") && team.get("players").length > 0) {
                         teamBlocs = [];
-                        context.get("cardsData").forEach(function(data){
-                            if(data.id == team.get("id")){
-                                if(data.blocs){
+                        context.get("cardsData").forEach(function(data) {
+                            if (data.id == team.get("id")) {
+                                if (data.blocs) {
                                     teamBlocs = data.blocs;
-                                    data.blocs.forEach(function(bloc){
+                                    data.blocs.forEach(function(bloc) {
                                         context._addOriginalBloc(team.id, bloc);
                                     });
                                 }
@@ -33,7 +33,7 @@ YUI.add('wegas-teams-dashboard', function(Y) {
                         });
                         var data = {
                             id: team.get("id"),
-                            title : game.get("properties.freeForAll") ? team.get("players")[0].get("name") : team.get("name"),
+                            title: game.get("properties.freeForAll") ? team.get("players")[0].get("name") : team.get("name"),
                             icon: icon,
                             blocs: teamBlocs
                         };
