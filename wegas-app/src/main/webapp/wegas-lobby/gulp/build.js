ce624c6249022e9d9883beb3cdfe80b7dbeef440
Cyril Junod <cyril.junod@gmail.com>
2015-10-01 15:12:18 +0200

    wegas-lobby: build tweak
    Allow some debugging

--- wegas-app/src/main/webapp/wegas-lobby/gulp/build.js
+++ wegas-app/src/main/webapp/wegas-lobby/gulp/build.js
@@ -5,93 +5,86 @@ var paths = gulp.paths;
 var merge = require('merge-stream');
 
 var $ = require('gulp-load-plugins')({
-  pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
+    pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
 });
 
-gulp.task('partials', function () {
-  return gulp.src([
-    paths.src + '/{app,components}/**/*.html',
-    paths.tmp + '/{app,components}/**/*.html'
-  ])
-    // .pipe($.minifyHtml({
-    //   empty: true,
-    //   spare: true,
-    //   quotes: true
-    // }))
-    .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'Wegas'
-    }))
-    .pipe(gulp.dest(paths.tmp + '/partials/'));
-});
+gulp.task('html', ['inject'], function() {
 
-gulp.task('html', ['inject', 'partials'], function () {
-  var partialsInjectFile = gulp.src(paths.tmp + '/partials/templateCacheHtml.js', { read: false });
-  var partialsInjectOptions = {
-    starttag: '<!-- inject:partials -->',
-    ignorePath: paths.tmp + '/partials',
-    addRootSlash: false
-  };
+    var htmlFilter = $.filter('*.html');
+    var jsFilter = $.filter('**/*.js');
+    var cssFilter = $.filter('**/*.css');
+    var assets;
 
-  var htmlFilter = $.filter('*.html');
-  var jsFilter = $.filter('**/*.js');
-  var cssFilter = $.filter('**/*.css');
-  var assets;
-
-  var gulped = gulp.src(paths.tmp + '/serve/*.jsp')
-    .pipe($.inject(partialsInjectFile, partialsInjectOptions))
-    .pipe(assets = $.useref.assets())
+    var gulped = gulp.src(paths.tmp + '/serve/*.jsp')
+        .pipe(assets = $.useref.assets());
     // .pipe($.rev())
-    .pipe(jsFilter)
-    .pipe($.ngAnnotate());
-    gulped = gulped.pipe($.cache($.uglify({preserveComments: $.uglifySaveLicense})));
-    gulped = gulped.pipe(jsFilter.restore())
-    .pipe(cssFilter);
+    /* JS COMPRESS */
+    gulped = gulped.pipe(jsFilter)
+        .pipe($.ngAnnotate())
+        .pipe($.cache($.uglify({preserveComments: $.uglifySaveLicense})))
+        .pipe(jsFilter.restore());
+    /* CSS COMPRESS */
+    gulped = gulped
+        .pipe(cssFilter)
+        .pipe($.cache($.csso())).pipe(cssFilter.restore());
 
-    gulped = gulped.pipe($.cache($.csso()));
-
-    gulped = gulped.pipe(cssFilter.restore())
-    .pipe(assets.restore())
-    .pipe($.useref())
-//    .pipe($.revReplace())
-    .pipe(htmlFilter);
+    gulped = gulped
+        .pipe(assets.restore())
+        .pipe($.useref())
+        //    .pipe($.revReplace())
+        .pipe(htmlFilter);
 
     gulped = gulped.pipe($.minifyHtml({
-      empty: true,
-      spare: true,
-      quotes: true
+        empty: true,
+        spare: true,
+        quotes: true
     }));
 
     return gulped.pipe(htmlFilter.restore())
-    .pipe(gulp.dest(paths.dist + '/'))
-    .pipe($.size({ title: paths.dist + '/', showFiles: true }));
+        .pipe(gulp.dest(paths.dist + '/'))
+        .pipe($.size({
+            title: paths.dist + '/',
+            showFiles: true
+        }));
+});
+gulp.task('debug-build', ['inject'], function() {
+    var assets = $.useref.assets();
+    return gulp.src(paths.tmp + '/serve/*.jsp')
+        .pipe(assets)
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest(paths.dist + '/'))
+        .pipe($.size({
+            title: paths.dist + '/',
+            showFiles: true
+        }));
+});
+gulp.task('images', function() {
+    return gulp.src(paths.src + '/assets/images/**/*')
+        .pipe(gulp.dest(paths.dist + '/assets/images/'));
 });
 
-gulp.task('images', function () {
-  return gulp.src(paths.src + '/assets/images/**/*')
-    .pipe(gulp.dest(paths.dist + '/assets/images/'));
-});
-
-gulp.task('fonts', function () {
+gulp.task('fonts', function() {
     var bowerFonts = gulp.src($.mainBowerFiles())
-    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
-    .pipe($.flatten())
-    .pipe(gulp.dest(paths.dist + '/fonts/'));
+        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+        .pipe($.flatten())
+        .pipe(gulp.dest(paths.dist + '/fonts/'));
 
-  var wegasFonts = gulp.src(paths.src + '/assets/fonts/**/*')
-    .pipe(gulp.dest(paths.dist + '/assets/fonts/'));
+    var wegasFonts = gulp.src(paths.src + '/assets/fonts/**/*')
+        .pipe(gulp.dest(paths.dist + '/assets/fonts/'));
 
-  return merge(bowerFonts, wegasFonts);
+    return merge(bowerFonts, wegasFonts);
 });
 
-gulp.task('misc', function () {
-  return gulp.src(paths.src + '/**/*.ico')
-    .pipe(gulp.dest(paths.dist + '/'));
+gulp.task('misc', function() {
+    return gulp.src(paths.src + '/**/*.ico')
+        .pipe(gulp.dest(paths.dist + '/'));
 });
 
-gulp.task('clean', function (done) {
-  $.del([paths.dist + '/', paths.tmp + '/'], done);
+gulp.task('clean', function(done) {
+    $.del([paths.tmp + '/', paths.dist + '/'], done);
 });
 
-gulp.task('build', ['html','images', 'fonts', 'misc'], function(cb){
+gulp.task('build', ['html', 'images', 'fonts', 'misc'], function(cb) {
     $.del([paths.tmp], cb);
 });
--- wegas-app/src/main/webapp/wegas-lobby/gulpfile.js
+++ wegas-app/src/main/webapp/wegas-lobby/gulpfile.js
@@ -4,10 +4,10 @@ var gulp = require('gulp');
 var bower = require('gulp-bower');
 
 gulp.paths = {
-  src: 'src',
-  dist: '..',
-  tmp: '.tmp',
-  e2e: 'e2e'
+    src: 'src',
+    dist: '..',
+    tmp: '.tmp',
+    e2e: 'e2e'
 };
 
 require('require-dir')('./gulp');
@@ -17,6 +17,11 @@ gulp.task('bower', function() {
         .pipe(gulp.dest('bower_components/'));
 });
 
-gulp.task('default', ['bower'], function () {
+gulp.task('default', ['bower'], function() {
     gulp.start('build');
 });
+
+gulp.task('watch', ['debug-build'], function() {
+    gulp.watch(['src/**/*.js', 'src/**/*.scss', 'src/**/*.html'], ['debug-build']);
+});
+
