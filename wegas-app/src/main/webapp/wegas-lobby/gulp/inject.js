35fe759b0500eab76bfe41c43fb1041bcd27f1e3
Cyril Junod <cyril.junod@gmail.com>
2015-10-01 15:12:18 +0200

    wegas-lobby: build tweak
    Allow some debugging

--- wegas-app/src/main/webapp/wegas-lobby/gulp/inject.js
+++ wegas-app/src/main/webapp/wegas-lobby/gulp/inject.js
@@ -5,9 +5,30 @@ var paths = gulp.paths;
 var $ = require('gulp-load-plugins')();
 
 var wiredep = require('wiredep').stream;
-
-gulp.task('inject', ['styles'], function() {
+gulp.task('partials', function() {
     'use strict';
+    return gulp.src([
+            paths.src + '/{app,components}/**/*.html',
+            paths.tmp + '/{app,components}/**/*.html'
+        ])
+        // .pipe($.minifyHtml({
+        //   empty: true,
+        //   spare: true,
+        //   quotes: true
+        // }))
+        .pipe($.angularTemplatecache('templateCacheHtml.js', {
+            module: 'Wegas'
+        }))
+        .pipe(gulp.dest(paths.tmp + '/partials/'));
+});
+gulp.task('inject', ['styles', 'partials'], function() {
+    'use strict';
+    var partialsInjectFile = gulp.src(paths.tmp + '/partials/templateCacheHtml.js', {read: false});
+    var partialsInjectOptions = {
+        starttag: '<!-- inject:partials -->',
+        ignorePath: paths.tmp + '/partials',
+        addRootSlash: false
+    };
     var injectStyles = gulp.src([
         paths.tmp + '/serve/{app,components}/**/*.css',
         '!' + paths.tmp + '/serve/app/vendor.css'
@@ -30,9 +51,10 @@ gulp.task('inject', ['styles'], function() {
     };
 
     return gulp.src(paths.src + '/*.jsp')
-        .pipe($.cache($.inject(injectStyles, injectOptions)))
-        .pipe($.cache($.inject(injectScripts, injectOptions)))
-        .pipe($.cache(wiredep(wiredepOptions)))
+        .pipe($.inject(injectStyles, injectOptions))
+        .pipe($.inject(injectScripts, injectOptions))
+        .pipe($.inject(partialsInjectFile, partialsInjectOptions))
+        .pipe(wiredep(wiredepOptions))
         .pipe(gulp.dest(paths.tmp + '/serve'));
 
 });
