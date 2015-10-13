407f66e2cc1950154b9875b63a1040d19c4e5ca8
Cyril Junod <cyril.junod@gmail.com>
2015-10-13 10:35:01 +0200

    admin: some small changes
    fix #1069

--- wegas-app/src/main/webapp/wegas-lobby/src/app/private/player/player.js
+++ wegas-app/src/main/webapp/wegas-lobby/src/app/private/player/player.js
@@ -1,24 +1,28 @@
 angular.module('private.player', [
-    'private.player.join',
-    'private.player.team',
-    'private.player.directives'
-])
-.config(function ($stateProvider) {
-    $stateProvider
-        .state('wegas.private.player', {
-            url: 'player',
-            views: {
-                'workspace': {
-                    controller: 'PlayerCtrl as playerCtrl',
-                    templateUrl: 'app/private/player/player.tmpl.html'
+        'private.player.join',
+        'private.player.team',
+        'private.player.directives',
+        'public.login'
+    ])
+    .config(function($stateProvider) {
+        "use strict";
+        $stateProvider
+            .state('wegas.private.player', {
+                url: 'player',
+                views: {
+                    'workspace': {
+                        controller: 'PlayerCtrl as playerCtrl',
+                        templateUrl: 'app/private/player/player.tmpl.html'
+                    }
                 }
-            }
-        })
-    ;
-})
-.controller('PlayerCtrl', function PlayerCtrl($rootScope, $state, Auth, WegasTranslations, $translate) {
-    Auth.getAuthenticatedUser().then(function(user){
-        $("body").removeClass("admin scenarist trainer").addClass("player");
-        $rootScope.translationWorkspace = {workspace: WegasTranslations.workspaces['PLAYER'][$translate.use()]};
+            });
+    })
+    .controller('PlayerCtrl', function PlayerCtrl($rootScope, $state, Auth, WegasTranslations, $translate) {
+        "use strict";
+        Auth.getAuthenticatedUser().then(function(user) {
+            $("body").removeClass("admin scenarist trainer").addClass("player");
+            $rootScope.translationWorkspace = {
+                workspace: WegasTranslations.workspaces['PLAYER'][$translate.use()]
+            };
+        });
     });
-});
