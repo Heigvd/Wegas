331204492ce868f963cd35714bf884668a8e6cba
Maxence Laurent <maxence.laurent@gmail.com>
2015-10-15 12:28:07 +0200

    Fix CI according to wegas-private
    * PMG i18n located in wegas-private

--- wegas-app/src/test/java/com/wegas/unit/i18n/PmgLanguageTest.java
+++ wegas-app/src/test/java/com/wegas/unit/i18n/PmgLanguageTest.java
@@ -19,8 +19,8 @@ public class PmgLanguageTest extends AbstractClientLanguageTest {
     @Override
     protected List<String> getScripts() {
         List<String> scripts = new ArrayList<>();
-        scripts.add(SCRIPTROOT + "wegas-pmg/js/i18n/i18n-pmg-en.js");
-        scripts.add(SCRIPTROOT + "wegas-pmg/js/i18n/i18n-pmg-fr.js");
+        scripts.add(SCRIPTROOT + "wegas-private/wegas-pmg/js/i18n/i18n-pmg-en.js");
+        scripts.add(SCRIPTROOT + "wegas-private/wegas-pmg/js/i18n/i18n-pmg-fr.js");
         return scripts;
     }
 
