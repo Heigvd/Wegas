0c8f5d9d91422dcbfb9def1a2f33ea48627a84c3
Maxence Laurent <maxence.laurent@gmail.com>
2015-10-16 16:06:35 +0200

    Fix Blacklister
    * fix url path detector

--- wegas-core/src/main/java/com/wegas/core/security/util/BlacklistFilter.java
+++ wegas-core/src/main/java/com/wegas/core/security/util/BlacklistFilter.java
@@ -36,43 +36,43 @@ public class BlacklistFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        blacklist.add("wegas-private/wegas-crimesim/js-server/crimesim-server.js");
-        blacklist.add("wegas-private/wegas-crimesim/db/wegas-crimesim-gamemodel.json");
-        blacklist.add("wegas-private/wegas-flexitests/scripts/flexitests-server-script.js");
-        blacklist.add("wegas-private/wegas-flexitests/db/wegas-flexitests-gamemodel.json");
-        blacklist.add("wegas-private/wegas-leaderway/db/wegas-leaderway.json");
-        blacklist.add("wegas-private/wegas-leaderway/db/wegas-leaderway-changemanagement.json");
-        blacklist.add("wegas-private/wegas-leaderway/server-js/wegas-leaderway.js");
-        blacklist.add("wegas-private/wegas-escapegamev1/db/EscapeGameV1.4.2.json");
-        blacklist.add("wegas-private/wegas-cep/js-server/wegas-cep.js");
-        blacklist.add("wegas-private/wegas-cep/db/wegas-cep-gamemodel.json");
-        blacklist.add("wegas-private/wegas-games/wegas-book/db/wegas-book.json");
-        blacklist.add("wegas-private/wegas-games/wegas-monopoly/db/wegas-monopoly-gamemodel.json");
-        blacklist.add("wegas-private/wegas-games/wegas-chess/db/wegas-chess-gamemodel.json");
-        blacklist.add("wegas-private/wegas-games/wegas-laddergame/db/laddergame-gamemodel.json");
-        blacklist.add("wegas-private/wegas-games/wegas-simpledialogue/db/wegas-simpledialogue-gamemodel.json");
-        blacklist.add("wegas-private/wegas-games/wegas-leaderway-v1/db/wegas-leaderway.json");
-        blacklist.add("wegas-private/wegas-pmg/scripts/test-scripts/wegas-pmg-server-test-language.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/test-scripts/wegas-pmg-server-test-util.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/test-scripts/wegas-pmg-server-test-artos.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/test-scripts/wegas-pmg-server-test-simplepmg.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-util.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/wegas-dashboard.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-backward.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-export.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-simulation.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-helper.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-language.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/locales");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/locales/en.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/locales/fr.js");
-        blacklist.add("wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-event-listeners.js");
-        blacklist.add("wegas-private/wegas-pmg/js/wegas-pmg-breadcrumb.js");
-        blacklist.add("wegas-private/wegas-pmg/db/wegas-pmg-gamemodel-language.json");
-        blacklist.add("wegas-private/wegas-pmg/db/wegas-pmg-gamemodel-Artos.json");
-        blacklist.add("wegas-private/wegas-pmg/db/wegas-pmg-gamemodel-simplePmg.json");
-        blacklist.add("wegas-private/wegas-corporatelaw/scripts/server/simulation.js");
-        blacklist.add("wegas-private/wegas-corporatelaw/scripts/server/event-listeners.js");
+        blacklist.add("/wegas-private/wegas-crimesim/js-server/crimesim-server.js");
+        blacklist.add("/wegas-private/wegas-crimesim/db/wegas-crimesim-gamemodel.json");
+        blacklist.add("/wegas-private/wegas-flexitests/scripts/flexitests-server-script.js");
+        blacklist.add("/wegas-private/wegas-flexitests/db/wegas-flexitests-gamemodel.json");
+        blacklist.add("/wegas-private/wegas-leaderway/db/wegas-leaderway.json");
+        blacklist.add("/wegas-private/wegas-leaderway/db/wegas-leaderway-changemanagement.json");
+        blacklist.add("/wegas-private/wegas-leaderway/server-js/wegas-leaderway.js");
+        blacklist.add("/wegas-private/wegas-escapegamev1/db/EscapeGameV1.4.2.json");
+        blacklist.add("/wegas-private/wegas-cep/js-server/wegas-cep.js");
+        blacklist.add("/wegas-private/wegas-cep/db/wegas-cep-gamemodel.json");
+        blacklist.add("/wegas-private/wegas-games/wegas-book/db/wegas-book.json");
+        blacklist.add("/wegas-private/wegas-games/wegas-monopoly/db/wegas-monopoly-gamemodel.json");
+        blacklist.add("/wegas-private/wegas-games/wegas-chess/db/wegas-chess-gamemodel.json");
+        blacklist.add("/wegas-private/wegas-games/wegas-laddergame/db/laddergame-gamemodel.json");
+        blacklist.add("/wegas-private/wegas-games/wegas-simpledialogue/db/wegas-simpledialogue-gamemodel.json");
+        blacklist.add("/wegas-private/wegas-games/wegas-leaderway-v1/db/wegas-leaderway.json");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/test-scripts/wegas-pmg-server-test-language.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/test-scripts/wegas-pmg-server-test-util.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/test-scripts/wegas-pmg-server-test-artos.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/test-scripts/wegas-pmg-server-test-simplepmg.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-util.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/wegas-dashboard.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-backward.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-export.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-simulation.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-helper.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-language.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/locales");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/locales/en.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/locales/fr.js");
+        blacklist.add("/wegas-private/wegas-pmg/scripts/server-scripts/wegas-pmg-server-event-listeners.js");
+        blacklist.add("/wegas-private/wegas-pmg/js/wegas-pmg-breadcrumb.js");
+        blacklist.add("/wegas-private/wegas-pmg/db/wegas-pmg-gamemodel-language.json");
+        blacklist.add("/wegas-private/wegas-pmg/db/wegas-pmg-gamemodel-Artos.json");
+        blacklist.add("/wegas-private/wegas-pmg/db/wegas-pmg-gamemodel-simplePmg.json");
+        blacklist.add("/wegas-private/wegas-corporatelaw/scripts/server/simulation.js");
+        blacklist.add("/wegas-private/wegas-corporatelaw/scripts/server/event-listeners.js");
     }
 
     @Override
@@ -81,14 +81,16 @@ public class BlacklistFilter implements Filter {
             HttpServletRequest req = (HttpServletRequest) request;
             HttpServletResponse resp = (HttpServletResponse) response;
 
-            String url = req.getRequestURI();
-            if (blacklist.contains(url)) {
-                doFilter(request, response, chain);
-            } else {
+            String url = req.getRequestURI().replaceFirst("^" + req.getContextPath(), "");
+            logger.error("URL: " + url);
+
+            if (url.matches("/wegas-private/private/.*$") || blacklist.contains(url)) {
                 // Blacklist URL -> forbidden
-                logger.error("Trying to access blacklisted content !");
+                logger.error("Trying to access blacklisted content (" + url + ") ! ");
                 resp.setStatus(403);
                 resp.getOutputStream().print("<h1>forbidden</h1>This content has been blacklisted");
+            } else {
+                chain.doFilter(request, response);
             }
         }
     }
