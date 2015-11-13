8559f2d07c887f159b886e861e99b4172a5aa1f4
Maxence Laurent <maxence.laurent@gmail.com>
2015-11-13 16:44:39 +0100

    ScriptEventFacade
    * be sure registering fired event once the engine has been instantiated

--- wegas-core/src/main/java/com/wegas/core/ejb/ScriptEventFacade.java
+++ wegas-core/src/main/java/com/wegas/core/ejb/ScriptEventFacade.java
@@ -29,203 +29,203 @@ import org.apache.commons.collections.map.MultiValueMap;
 @RequestScoped
 public class ScriptEventFacade {
 
-    /**
-     *
-     */
-    private final MultiValueMap eventsFired;
-    /**
-     *
-     */
-    private final MultiValueMap registeredEvents;
-    /**
-     *
-     */
-    private Boolean eventFired;
-    /**
-     *
-     */
-    @EJB
-    private ScriptFacade scriptFacace;
-    /**
-     *
-     */
-    @Inject
-    private RequestManager requestManager;
+	/**
+	 *
+	 */
+	private final MultiValueMap eventsFired;
+	/**
+	 *
+	 */
+	private final MultiValueMap registeredEvents;
+	/**
+	 *
+	 */
+	private Boolean eventFired;
+	/**
+	 *
+	 */
+	@EJB
+	private ScriptFacade scriptFacace;
+	/**
+	 *
+	 */
+	@Inject
+	private RequestManager requestManager;
 
-    /**
-     *
-     */
-    public ScriptEventFacade() {
-        this.eventFired = false;
-        this.eventsFired = new MultiValueMap();
-        this.registeredEvents = new MultiValueMap();
-    }
+	/**
+	 *
+	 */
+	public ScriptEventFacade() {
+		this.eventFired = false;
+		this.eventsFired = new MultiValueMap();
+		this.registeredEvents = new MultiValueMap();
+	}
 
-    /**
-     *
-     */
-    public void detachAll() {
-        this.eventFired = false;
-        this.eventsFired.clear();
-        this.registeredEvents.clear();
-    }
+	/**
+	 *
+	 */
+	public void detachAll() {
+		this.eventFired = false;
+		this.eventsFired.clear();
+		this.registeredEvents.clear();
+	}
 
-    /**
-     *
-     * @return
-     */
-    public Boolean isEventFired() {
-        return eventFired;
-    }
+	/**
+	 *
+	 * @return
+	 */
+	public Boolean isEventFired() {
+		return eventFired;
+	}
 
-    /**
-     *
-     * @param player
-     * @param eventName
-     * @param param
-     * @throws com.wegas.core.exception.client.WegasScriptException
-     */
-    public void fire(Player player, String eventName, Object param) throws WegasScriptException {
-        this.eventsFired.put(eventName, param);
-        this.doFire(player, eventName, param);
-    }
+	/**
+	 *
+	 * @param player
+	 * @param eventName
+	 * @param param
+	 * @throws com.wegas.core.exception.client.WegasScriptException
+	 */
+	public void fire(Player player, String eventName, Object param) throws WegasScriptException {
+		this.doFire(player, eventName, param);
+	}
 
-    /**
-     *
-     * @param player
-     * @param eventName
-     * @throws com.wegas.core.exception.client.WegasScriptException
-     */
-    public void fire(Player player, String eventName) throws WegasScriptException {
-        this.fire(player, eventName, null);
-    }
+	/**
+	 *
+	 * @param player
+	 * @param eventName
+	 * @throws com.wegas.core.exception.client.WegasScriptException
+	 */
+	public void fire(Player player, String eventName) throws WegasScriptException {
+		this.fire(player, eventName, null);
+	}
 
-    /**
-     *
-     * @param eventName
-     * @param param
-     * @throws com.wegas.core.exception.client.WegasScriptException
-     */
-    public void fire(String eventName, Object param) throws WegasScriptException {
-        this.fire(requestManager.getPlayer(), eventName, param);
-    }
+	/**
+	 *
+	 * @param eventName
+	 * @param param
+	 * @throws com.wegas.core.exception.client.WegasScriptException
+	 */
+	public void fire(String eventName, Object param) throws WegasScriptException {
+		this.fire(requestManager.getPlayer(), eventName, param);
+	}
 
-    /**
-     *
-     * @param eventName
-     * @throws com.wegas.core.exception.client.WegasScriptException
-     */
-    public void fire(String eventName) throws WegasScriptException {
-        this.fire(eventName, null);
-    }
+	/**
+	 *
+	 * @param eventName
+	 * @throws com.wegas.core.exception.client.WegasScriptException
+	 */
+	public void fire(String eventName) throws WegasScriptException {
+		this.fire(eventName, null);
+	}
 
-    private void doFire(Player player, String eventName, Object params) throws WegasScriptException {
-        this.eventFired = true;
-        if (player == null && requestManager.getPlayer() == null) {
-            throw WegasErrorMessage.error("An event '" + eventName + "' has been fired without a player defined. A player has to be defined.");
-        }
-        if (requestManager.getCurrentEngine() == null) {
-            /* init script engine, declared eventListeners are not yet in memory */
-            scriptFacace.eval(player, new Script(""), null);
-        }
-        ScriptEngine engine = requestManager.getCurrentEngine();
+	private void doFire(Player player, String eventName, Object params) throws WegasScriptException {
+		this.eventFired = true;
+		if (player == null && requestManager.getPlayer() == null) {
+			throw WegasErrorMessage.error("An event '" + eventName + "' has been fired without a player defined. A player has to be defined.");
+		}
+		if (requestManager.getCurrentEngine() == null) {
+			/* init script engine, declared eventListeners are not yet in memory */
+			scriptFacace.eval(player, new Script(""), null);
+		}
+		ScriptEngine engine = requestManager.getCurrentEngine();
+		this.eventsFired.put(eventName, params);
 
-        if (this.registeredEvents.containsKey(eventName)) {
-            Collection callbacks = this.registeredEvents.getCollection(eventName);
-            for (Object cb : callbacks) {
-                //ScriptObjectMirror sob = (ScriptObjectMirror) ((Object[]) cb)[0];
-                Object obj = ((Object[]) cb)[0];
+		if (this.registeredEvents.containsKey(eventName)) {
+			Collection callbacks = this.registeredEvents.getCollection(eventName);
+			for (Object cb : callbacks) {
+				//ScriptObjectMirror sob = (ScriptObjectMirror) ((Object[]) cb)[0];
+				Object obj = ((Object[]) cb)[0];
 
-                Object scope = (((Object[]) cb).length == 2 ? ((Object[]) cb)[1] : new EmptyObject());
-                String fcnSource;
-                
-                if (obj instanceof ScriptObjectMirror) {
-                    // @hack openjdk 1.8.0_45
-                    fcnSource = obj.toString();
-                } else {
-                    // @hack openjdk 1.8.0_31, oracle jdk
-                    fcnSource = ((ScriptFunction) ((Object[]) cb)[0]).toSource();
-                }
+				Object scope = (((Object[]) cb).length == 2 ? ((Object[]) cb)[1] : new EmptyObject());
+				String fcnSource;
 
-                /*
+				if (obj instanceof ScriptObjectMirror) {
+					// @hack openjdk 1.8.0_45
+					fcnSource = obj.toString();
+				} else {
+					// @hack openjdk 1.8.0_31, oracle jdk
+					fcnSource = ((ScriptFunction) ((Object[]) cb)[0]).toSource();
+				}
+
+				/*
                  *       JAVA BUG
                  *       http://bugs.java.com/view_bug.do?bug_id=8050977
                  *       
                  *       workaround: re-eval function through nashorn INTERNAL (O_o) ScriptObjectMirror.toString()
-                 */
-                try {
-                    ((Invocable) engine).invokeMethod(engine.eval(fcnSource), "call", scope, params);
-                } catch (ScriptException | NoSuchMethodException ex) {
-                    throw new WegasScriptException("Event exception", ex);
-                }
-                /*
+				 */
+				try {
+					((Invocable) engine).invokeMethod(engine.eval(fcnSource), "call", scope, params);
+				} catch (ScriptException | NoSuchMethodException ex) {
+					throw new WegasScriptException("Event exception", ex);
+				}
+				/*
                  *  ONCE RESOLVED, REPLACE WITH
                  *  ((Invocable) requestManager.getCurrentEngine()).invokeMethod(((Object[]) cb)[0], "call", ((Object[]) cb)[1], params);
-                 */
-            }
-        }
-    }
+				 */
+			}
+		}
+	}
 
-    /**
-     *
-     * @param eventName
-     * @return Object[] array of corresponding parameters fired. Length
-     *         correspond to number of times eventName has been fired.
-     */
-    public Object[] getFiredParameters(String eventName) {
-        if (this.eventsFired.containsKey(eventName)) {
-            return this.eventsFired.getCollection(eventName).toArray();
-        } else {
-            return new Object[]{};
-        }
-    }
+	/**
+	 *
+	 * @param eventName
+	 * @return Object[] array of corresponding parameters fired. Length
+	 *         correspond to number of times eventName has been fired.
+	 */
+	public Object[] getFiredParameters(String eventName) {
+		if (this.eventsFired.containsKey(eventName)) {
+			return this.eventsFired.getCollection(eventName).toArray();
+		} else {
+			return new Object[]{};
+		}
+	}
 
-    /**
-     *
-     * @param eventName
-     * @return
-     */
-    public int firedCount(String eventName) {
-        return this.getFiredParameters(eventName).length;
-    }
+	/**
+	 *
+	 * @param eventName
+	 * @return
+	 */
+	public int firedCount(String eventName) {
+		return this.getFiredParameters(eventName).length;
+	}
 
-    /**
-     *
-     * @param eventName
-     * @return
-     */
-    public boolean fired(String eventName) {
-        return this.firedCount(eventName) > 0;
-    }
+	/**
+	 *
+	 * @param eventName
+	 * @return
+	 */
+	public boolean fired(String eventName) {
+		return this.firedCount(eventName) > 0;
+	}
 
-    /**
-     *
-     * @param eventName
-     * @param func
-     * @param scope
-     */
-    public void on(String eventName, Object func, Object scope) {
-        this.registeredEvents.put(eventName, new Object[]{func, scope});
-    }
+	/**
+	 *
+	 * @param eventName
+	 * @param func
+	 * @param scope
+	 */
+	public void on(String eventName, Object func, Object scope) {
+		this.registeredEvents.put(eventName, new Object[]{func, scope});
+	}
 
-    /**
-     *
-     * @param eventName
-     * @param func
-     */
-    public void on(String eventName, Object func) {
-        this.registeredEvents.put(eventName, new Object[]{func});
-    }
+	/**
+	 *
+	 * @param eventName
+	 * @param func
+	 */
+	public void on(String eventName, Object func) {
+		this.registeredEvents.put(eventName, new Object[]{func});
+	}
 
-    /**
-     *
-     */
-    public static class EmptyObject {
+	/**
+	 *
+	 */
+	public static class EmptyObject {
 
-        /**
-         *
-         */
-        public EmptyObject() {
-        }
-    }
+		/**
+		 *
+		 */
+		public EmptyObject() {
+		}
+	}
 }
