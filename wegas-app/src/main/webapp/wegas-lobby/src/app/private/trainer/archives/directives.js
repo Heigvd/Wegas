9cb1fddbf6060d0f47dad21022a4ab39867f3772
Cyril Junod <cyril.junod@gmail.com>
2015-11-16 17:51:11 +0100

    Lobby: archive sync
    *Fix #1113

--- wegas-app/src/main/webapp/wegas-lobby/src/app/private/trainer/archives/directives.js
+++ wegas-app/src/main/webapp/wegas-lobby/src/app/private/trainer/archives/directives.js
@@ -9,95 +9,99 @@ angular.module('private.trainer.archives.directives', [])
             controller: "TrainerArchivesIndexController as indexCtrl"
         };
     }).controller("TrainerArchivesIndexController", function TrainerArchivesIndexController($timeout, $translate, $rootScope, $scope, $state, SessionsModel, Flash) {
-        "use strict";
-        var ctrl = this;
-        ctrl.archives = [];
-        ctrl.search = "";
+    "use strict";
+    var ctrl = this;
+    ctrl.archives = [];
+    ctrl.search = "";
+    ctrl.loading = true;
+
+    ctrl.updateSessions = function() {
         ctrl.loading = true;
-
-        ctrl.updateSessions = function() {
-            ctrl.loading = true;
-            SessionsModel.getSessions("BIN").then(function(response) {
-                ctrl.loading = false;
-                ctrl.archives = response.data || {};
-                if (response.isErroneous()) {
-                    response.flash();
-                }
-                if (ctrl.archives.length === 0) {
-                    $scope.close();
-                }
-            });
-        };
-
-        ctrl.unarchiveSession = function(sessionToUnarchive) {
-            if (sessionToUnarchive) {
-                SessionsModel.unarchiveSession(sessionToUnarchive).then(function(response) {
-                    if (!response.isErroneous()) {
-                        $rootScope.$emit('changeLimit', true);
-                        ctrl.updateSessions();
-                    }else{
-                        response.flash();
-                    }
-                });
-            } else {
-                $translate('COMMONS-SESSIONS-NO-SESSION-FLASH-ERROR').then(function (message) {
-                    Flash.danger(message);
-                });
+        SessionsModel.getSessions("BIN").then(function(response) {
+            ctrl.loading = false;
+            ctrl.archives = response.data || {};
+            if (response.isErroneous()) {
+                response.flash();
             }
-        };
-
-        ctrl.deleteArchivedSession = function(sessionToDelete) {
-            if (sessionToDelete) {
-                SessionsModel.deleteArchivedSession(sessionToDelete).then(function(response) {
-                    if (!response.isErroneous()) {
-                        $rootScope.$emit('changeArchives', true);
-                        ctrl.updateSessions();
-                    }else{
-                        response.flash();
-                    }
-                });
-            } else {
-                $translate('COMMONS-SESSIONS-NO-SESSION-FLASH-ERROR').then(function (message) {
-                    Flash.danger(message);
-                });
-            }
-        };
-
-        ctrl.showSettings = function(session) {
-            if (session) {
+            if (ctrl.archives.length === 0) {
                 $scope.close();
-                $timeout(function() {
-                    $state.go('wegas.private.trainer.archives.settings', {id: session.id});
-                }, 1500);
-            } else {
-                $translate('COMMONS-SESSIONS-NO-SESSION-FLASH-ERROR').then(function (message) {
-                    Flash.danger(message);
-                });
-            }
-        };
-
-        ctrl.showUsers = function(session) {
-            if (session) {
-                $scope.close();
-                $timeout(function() {
-                    $state.go('wegas.private.trainer.archives.users', {id: session.id});
-                }, 1500);
-            } else {
-                $translate('COMMONS-SESSIONS-NO-SESSION-FLASH-ERROR').then(function (message) {
-                    Flash.danger(message);
-                });
-            }
-        };
-
-        /* Listen for new session */
-        $rootScope.$on('changeArchives', function(e, hasNewData) {
-            if (hasNewData) {
-                ctrl.updateSessions();
             }
         });
+    };
 
-        ctrl.updateSessions();
-    })
+    ctrl.unarchiveSession = function(sessionToUnarchive) {
+        if (sessionToUnarchive) {
+            SessionsModel.unarchiveSession(sessionToUnarchive).then(function(response) {
+                if (!response.isErroneous()) {
+                    $rootScope.$emit('changeLimit', true);
+                    ctrl.updateSessions();
+                } else {
+                    response.flash();
+                }
+            });
+        } else {
+            $translate('COMMONS-SESSIONS-NO-SESSION-FLASH-ERROR').then(function(message) {
+                Flash.danger(message);
+            });
+        }
+    };
+
+    ctrl.deleteArchivedSession = function(sessionToDelete) {
+        if (sessionToDelete) {
+            SessionsModel.deleteArchivedSession(sessionToDelete).then(function(response) {
+                if (!response.isErroneous()) {
+                    $rootScope.$emit('changeArchives', true);
+                    ctrl.updateSessions();
+                } else {
+                    response.flash();
+                }
+            });
+        } else {
+            $translate('COMMONS-SESSIONS-NO-SESSION-FLASH-ERROR').then(function(message) {
+                Flash.danger(message);
+            });
+        }
+    };
+
+    ctrl.showSettings = function(session) {
+        if (session) {
+            $scope.close();
+            $timeout(function() {
+                $state.go('wegas.private.trainer.archives.settings', {
+                    id: session.id
+                });
+            }, 1500);
+        } else {
+            $translate('COMMONS-SESSIONS-NO-SESSION-FLASH-ERROR').then(function(message) {
+                Flash.danger(message);
+            });
+        }
+    };
+
+    ctrl.showUsers = function(session) {
+        if (session) {
+            $scope.close();
+            $timeout(function() {
+                $state.go('wegas.private.trainer.archives.users', {
+                    id: session.id
+                });
+            }, 1500);
+        } else {
+            $translate('COMMONS-SESSIONS-NO-SESSION-FLASH-ERROR').then(function(message) {
+                Flash.danger(message);
+            });
+        }
+    };
+
+    /* Listen for new session */
+    $rootScope.$on('changeArchives', function(e, hasNewData) {
+        if (hasNewData) {
+            ctrl.updateSessions();
+        }
+    });
+
+    ctrl.updateSessions();
+})
     .directive('trainerSessionsArchivesList', function() {
         "use strict";
         return {
@@ -108,8 +112,8 @@ angular.module('private.trainer.archives.directives', [])
                 search: "=",
                 details: "=",
                 users: "=",
-                loading:"="
-                
+                loading: "="
+
             },
             templateUrl: 'app/private/trainer/archives/directives.tmpl/list.html'
         };
