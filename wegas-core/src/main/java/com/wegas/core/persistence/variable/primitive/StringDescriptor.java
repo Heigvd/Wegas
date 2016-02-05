853d21cd216c9001c1fcbd123b456330514cfdb5
Maxence Laurent <maxence.laurent@gmail.com>
2016-02-06 00:03:54 +0100

    StringDesc
    * sort allowedValues alphabetically

--- wegas-core/src/main/java/com/wegas/core/persistence/variable/primitive/StringDescriptor.java
+++ wegas-core/src/main/java/com/wegas/core/persistence/variable/primitive/StringDescriptor.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.OrderBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,6 +37,7 @@ public class StringDescriptor extends VariableDescriptor<StringInstance> {
     private String validationPattern;
 
     @ElementCollection
+    @OrderBy("allowedvalues")
     private List<String> allowedValues = new ArrayList<>();
 
     /**
