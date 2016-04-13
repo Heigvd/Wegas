4f9421e5e373d1b6e1a832407ed3a5873be1d100
Cyril Junod <cyril.junod@gmail.com>
2016-04-13 15:07:57 +0200

    wegas-app test with ONE container
    +weaving

--- wegas-app/src/test/java/com/wegas/unit/AbstractEJBContainerTest.java
+++ wegas-app/src/test/java/com/wegas/unit/AbstractEJBContainerTest.java
@@ -7,19 +7,12 @@
  */
 package com.wegas.unit;
 
-import com.wegas.utils.AbstractTest;
 import com.wegas.core.Helper;
 import com.wegas.core.ejb.GameModelFacade;
 import com.wegas.core.ejb.VariableDescriptorFacade;
 import com.wegas.core.rest.ScriptController;
 import com.wegas.core.security.ejb.UserFacade;
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.ejb.embeddable.EJBContainer;
-import javax.naming.NamingException;
+import com.wegas.utils.AbstractTest;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.glassfish.embeddable.GlassFishException;
@@ -27,45 +20,58 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.slf4j.LoggerFactory;
 
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.NamingException;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
  * @author Cyril Junod <cyril.junod at gmail.com>
  */
 public abstract class AbstractEJBContainerTest extends AbstractTest {
 
     private static EJBContainer container;
+
     private static GameModelFacade gmFacade;
+
     private static org.slf4j.Logger logger = LoggerFactory.getLogger(AbstractEJBContainerTest.class);
 
     @BeforeClass
     public static void setUp() throws Exception {
-        Map<String, Object> properties = new HashMap<>();                       // Init Ejb container
-        properties.put(EJBContainer.MODULES, new File[]{new File("../wegas-core/target/embed-classes")});
-        properties.put("org.glassfish.ejb.embedded.glassfish.installation.root", "../wegas-core/src/test/glassfish");
-        //properties.put(EJBContainer.APP_NAME,"class");
-        //ejbContainer.getContext().rebind("inject", this);
+        if (container == null) {
+            Map<String, Object> properties = new HashMap<>();                       // Init Ejb container
+            properties.put(EJBContainer.MODULES, new File[]{new File("../wegas-core/target/embed-classes")});
+            properties.put("org.glassfish.ejb.embedded.glassfish.installation.root", "../wegas-core/src/test/glassfish");
+            //properties.put(EJBContainer.APP_NAME,"class");
+            //ejbContainer.getContext().rebind("inject", this);
 
-        // Init shiro
-        SecurityUtils.setSecurityManager(new IniSecurityManagerFactory("classpath:shiro.ini").getInstance());
+            // Init shiro
+            SecurityUtils.setSecurityManager(new IniSecurityManagerFactory("classpath:shiro.ini").getInstance());
 
-        /* Log Levels */
-        Logger.getLogger("javax.enterprise.system.tools.deployment").setLevel(Level.SEVERE);
-        Logger.getLogger("javax.enterprise.system").setLevel(Level.SEVERE);
-        org.glassfish.ejb.LogFacade.getLogger().setLevel(Level.SEVERE);
+            /* Log Levels */
+            Logger.getLogger("javax.enterprise.system.tools.deployment").setLevel(Level.SEVERE);
+            Logger.getLogger("javax.enterprise.system").setLevel(Level.SEVERE);
+            org.glassfish.ejb.LogFacade.getLogger().setLevel(Level.SEVERE);
 
-        logger.error("CREATE CONTAINER");
-        container = EJBContainer.createEJBContainer(properties);
-        Helper.lookupBy(container.getContext(), UserFacade.class, UserFacade.class).guestLogin(); //login as guest
+            logger.error("CREATE CONTAINER");
+            container = EJBContainer.createEJBContainer(properties);
+            Helper.lookupBy(container.getContext(), UserFacade.class, UserFacade.class).guestLogin(); //login as guest
 
-        gmFacade = Helper.lookupBy(container.getContext(), GameModelFacade.class, GameModelFacade.class);
+            gmFacade = Helper.lookupBy(container.getContext(), GameModelFacade.class, GameModelFacade.class);
+        }
     }
 
     @AfterClass
-    public static void tearDown() throws GlassFishException {
-        if (container != null) {
-            logger.error("CLOSE CONTAINER");
-            container.close();
-        }
+    public static void tearDown() throws GlassFishException, NamingException {
+//        if (container != null) {
+//            logger.error("CLOSE CONTAINER");
+//            container.getContext().close();
+//            container.close();
+//            container = null;
+//        }
     }
 
     protected <T> T lookup(Class<T> className) {
