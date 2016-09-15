6df9e3e94753e00cafebcd4a7d42351750e0c893
XaviJunior <xavier.aeby@outlook.com>
2016-09-15 14:35:23 +0200

    CommonView Global
    Création d'un commonView applicable à tout les composant avec un label,
    une description si nécessaire et des message d'erreurs.

---
+++ wegas-app/src/main/webapp/wegas-react-form/src/HOC/commonView.js
@@ -0,0 +1,62 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+import styles from '../css/commonView.css';
+
+export default function comonView(Comp) {
+    function CommonComp(props) {
+        const errors = props.errorMessage && props.errorMessage.map(
+            v => (
+                <span>
+                    <i
+                        className={classNames({
+                            fa: props.errorMessage,
+                            'fa-info-circle': props.errorMessage
+                        })
+                        }aria-hidden="true"
+                    />
+                        {v}
+                </span>
+                    
+                ));
+
+        return (
+            <div className={props.view.className}>
+                <div 
+                    className={styles.label}
+                >
+                    {props.view.label}
+                </div>
+                <Comp {...props} />
+                <div
+                    className={classNames({
+                        [styles.infos]: props.view.description,
+                    })
+                    }
+                >
+                    <i
+                        className={classNames({
+                            fa: props.view.description,
+                            'fa-info-circle': props.view.description
+                        })
+                        }aria-hidden="true"
+                    />
+                    {props.view.description}
+                </div>
+                <div
+                    className={classNames(styles.errs)}
+                >
+                    {errors}
+                </div>
+            </div>
+        );
+    }
+    CommonComp.propTypes = {
+        errorMessage: PropTypes.arrayOf(PropTypes.string),
+        view: PropTypes.shape({
+            label: PropTypes.string,
+            description: PropTypes.string,
+            className: PropTypes.string
+        }).isRequired
+    };
+    return CommonComp;
+}
