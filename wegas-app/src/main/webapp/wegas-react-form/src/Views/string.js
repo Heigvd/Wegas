6a7f9d6b7b95144c39545e495a2be78d3c7012cd
XaviJunior <xavier.aeby@outlook.com>
2016-09-20 15:25:02 +0200

    retouche du string
    retouche du string

--- wegas-app/src/main/webapp/wegas-react-form/src/Views/string.js
+++ wegas-app/src/main/webapp/wegas-react-form/src/Views/string.js
@@ -1,10 +1,9 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
-import { grey500 } from 'material-ui/styles/colors';
-import TextField from 'material-ui/TextField';
 import debounce from '../HOC/callbackDebounce';
 import commonView from '../HOC/commonView';
 import styles from '../css/string.css';
+import IconButton from '../Components/iconButton.js';
 
 
 const debounceOnChange = debounce('onChange');
@@ -12,26 +11,31 @@ const debounceOnChange = debounce('onChange');
 function StringView(props) {
     if (typeof props.view.rows === 'number') {
         return (
-            <textarea rows={props.view.rows} onChange={ev => props.onChange(ev.target.value)} />
+            <textarea
+                rows={props.view.rows}
+                onChange={ev => props.onChange(ev.target.value)}
+            />
         );
     }
     const length = props.view.length || '10';
     return (
-        <input
-            className={classNames({ [styles.numb]: length === 6 })}
-            type="text"
-            defaultValue={props.value}
-            onChange={ev => props.onChange(ev.target.value)}
-        />
+        <div>
+            <input
+                className={classNames({ [styles.numb]: length === 6 })}
+                type="text"
+                defaultValue={props.value}
+                onChange={ev => props.onChange(ev.target.value)}
+            />
+        </div>
     );
 }
 
 StringView.propTypes = {
     onChange: PropTypes.func.isRequired,
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    view: {
+    view: PropTypes.shape({
         rows: PropTypes.number
-    }
+    })
 };
 
 export default commonView(debounceOnChange(StringView));
