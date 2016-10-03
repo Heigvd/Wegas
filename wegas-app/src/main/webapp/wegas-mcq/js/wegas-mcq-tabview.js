c4c89b7b6b6c2e84e53a38f26381b9c67206e6f4
Maxence Laurent <maxence.laurent@gmail.com>
2016-10-03 14:11:33 +0200

    [MCQTabView] Sync Enhancements
    * update rather than replace questions

--- wegas-app/src/main/webapp/wegas-mcq/js/wegas-mcq-tabview.js
+++ wegas-app/src/main/webapp/wegas-mcq/js/wegas-mcq-tabview.js
@@ -11,11 +11,9 @@
  */
 YUI.add('wegas-mcq-tabview', function(Y) {
     "use strict";
-
     var CONTENTBOX = 'contentBox',
         Wegas = Y.Wegas,
         MCQTabView;
-
     /**
      * @name Y.Wegas.MCQTabView
      * @extends Y.Widget
@@ -67,48 +65,129 @@ YUI.add('wegas-mcq-tabview', function(Y) {
         bindUI: function() {
             this.tabView.after("selectionChange", this.onTabSelected, this);
             this.handlers.response = this.dataSource.after("update", this.syncUI, this);
-/*
-// This code does not refresh the display when a question list is to be replaced by another list:
-            this.handlers.response = this.dataSource.after("updatedInstance", function(e) {
-                // Easy test to possibly avoid entering the loop:
-                if (e.entity.get("@class") !== "QuestionInstance") return;
-                var questions = this.get("variable.evaluated");
-                // Check for null and undefined:
-                if (questions == null) return;
-                if (!Y.Lang.isArray(questions)) {
-                    questions = [questions];
+            /*
+             // This code does not refresh the display when a question list is to be replaced by another list:
+             this.handlers.response = this.dataSource.after("updatedInstance", function(e) {
+             // Easy test to possibly avoid entering the loop:
+             if (e.entity.get("@class") !== "QuestionInstance") return;
+             var questions = this.get("variable.evaluated");
+             // Check for null and undefined:
+             if (questions == null) return;
+             if (!Y.Lang.isArray(questions)) {
+             questions = [questions];
+             }
+             for (var i=0; i<questions.length; i++) {
+             var q = questions[i];
+             if (q.getInstance().get("id") === e.entity.get("id")) {
+             this.syncUI();
+             return;
+             }
+             }
+             }, this);
+             */
+        },
+        updateTab: function(tab, question) {
+            var questionInstance = question.getInstance(),
+                choiceDescriptor,
+                label, cbxType = question.get("cbx"),
+                validatedCbx = (cbxType ? questionInstance.get('validated') : false),
+                nbReplies = questionInstance.get("replies").length,
+                highlightUnanswered = (this.get("highlightUnanswered") && (cbxType ? !validatedCbx : (nbReplies === 0)));
+
+            if (cbxType) {
+                if (validatedCbx)
+                    label = ""; // Dummy status string
+            } else if (nbReplies > 0) {
+                if (question.get("allowMultipleReplies")) {
+                    label = questionInstance.get("replies").length + "x";
+                } else { // Find the last selected replies
+                    choiceDescriptor = questionInstance.get("replies")[questionInstance.get("replies").length - 1 ].getChoiceDescriptor();
+                    label = choiceDescriptor.get("title") || "";
+                    label = (label.length >= 15) ? label.substr(0, 15) + "..." : label;
                 }
-                for (var i=0; i<questions.length; i++) {
-                    var q = questions[i];
-                    if (q.getInstance().get("id") === e.entity.get("id")) {
-                        this.syncUI();
-                        return;
+            }
+
+            if (Y.Lang.isNull(label)) {
+                label = (!question.get("allowMultipleReplies")) ? Y.Wegas.I18n.t('mcq.unanswered') : Y.Wegas.I18n.t('mcq.notDone');
+            }
+
+
+            label = '<div class="'
+                + (highlightUnanswered ? "unread" : "")
+                + '"><div class="index-label">'
+                + (question.get("title") || question.get("label") || "undefined")
+                + '</div>';
+
+            tab.set("label", label);
+        },
+        createTab: function(question) {
+            var tab = new Y.Wegas.Tab();
+            tab.loaded = false;
+            tab.cQuestion = question;
+            tab.set("content", "<div class=\"wegas-loading-div\"><div>");
+            this.updateTab(tab, question);
+            return tab;
+        },
+        updateTabs: function(questions) {
+            var question, questionInstance, oldTab, newTab, tabs, toRemove, index, queue,
+                oldIndex;
+            tabs = this.tabView._items;
+            toRemove = tabs.slice(0);
+
+
+            if (!Y.Lang.isArray(questions)) {
+                queue = [questions];
+            } else {
+                queue = questions;
+            }
+
+            index = 0;
+            while (question = queue.shift()) {
+                if (question instanceof Wegas.persistence.QuestionDescriptor) {
+                    oldTab = Y.Array.find(tabs, function(item) {
+                        return item.cQuestion.get("id") === question.get("id");
+                    });
+                    questionInstance = question.getInstance();
+
+                    if (questionInstance.get("active")) {
+                        if (oldTab) {
+                            // Simple Update 
+                            this.updateTab(oldTab, question);
+                            oldIndex = toRemove.indexOf(oldTab);
+                            if (oldIndex >= 0) {
+                                toRemove.splice(oldIndex, 1);
+                            }
+                        } else {
+                            // Just activated
+                            newTab = this.createTab(question);
+                            this.tabView.add(newTab, index);
+                        }
+                        index++;
                     }
+                } else if (question instanceof Wegas.persistence.ListDescriptor) {
+                    queue = queue.concat(question.get("items"));
                 }
-            }, this);
-*/
+            }
+
+            /*
+             * Remvoe tabs which are not to be no longer displayed
+             */
+            while (question = toRemove.shift()) {
+                question.remove();
+            }
         },
         /**
          * @function
          * @private
-         * @description Clear and re-fill the TabView with active
+         * @description * Update the TabView with active
          * choice/questions and relatives reply.
          * Display a message if there is no message.
          */
         syncUI: function() {
             var questions = this.get("variable.evaluated"),
-                selectedTab = this.tabView.get('selection'),
-                lastSelection = (selectedTab) ? selectedTab.get('index') : 0;
+                selectedTab, lastSelection;
 
-            this.isRemovingTabs = true;
-            this.tabView.destroyAll(); // Empty the tabview
-            this.isRemovingTabs = false;
-
-            if (!Y.Lang.isArray(questions)) {
-                questions = [questions];
-            }
-
-            this.addQuestions(questions);
+            this.updateTabs(questions);
 
             this.hideOverlay();
 
@@ -121,58 +200,12 @@ YUI.add('wegas-mcq-tabview', function(Y) {
                 this.tabView.selectChild(0);
             } else {
                 this.get("contentBox").removeClass("empty");
+                selectedTab = this.tabView.get('selection');
+                lastSelection = (selectedTab) ? selectedTab.get('index') : 0;
                 if (lastSelection >= this.tabView.size()) { // Can occur when questions list has changed during event
                     lastSelection = 0;
                 }
                 this.tabView.selectChild(lastSelection);
-            }
-        },
-        addQuestions: function(questions) {
-
-            var i, cReplyLabel, cQuestion, cQuestionInstance,
-                tab, choiceDescriptor;
-
-            for (i = 0; i < questions.length; i += 1) {
-                cQuestion = questions[i];
-                cQuestionInstance = cQuestion.getInstance();
-                cReplyLabel = null;
-                if (cQuestion instanceof Wegas.persistence.QuestionDescriptor
-                    && cQuestionInstance.get("active")) { // If current question is active
-
-                    var cbxType = cQuestion.get("cbx"),
-                        validatedCbx = (cbxType ? cQuestionInstance.get('validated') : false),
-                        nbReplies = cQuestionInstance.get("replies").length,
-                        highlightUnanswered = (this.get("highlightUnanswered") && (cbxType ? !validatedCbx : (nbReplies === 0)));
-
-                    if (cbxType) {
-                        if (validatedCbx)
-                            cReplyLabel = ""; // Dummy status string
-                    } else if (nbReplies > 0) {
-                        if (cQuestion.get("allowMultipleReplies")) {
-                            cReplyLabel = cQuestionInstance.get("replies").length + "x";
-                        } else { // Find the last selected replies
-                            choiceDescriptor = cQuestionInstance.get("replies")[cQuestionInstance.get("replies").length - 1 ].getChoiceDescriptor();
-                            cReplyLabel = choiceDescriptor.get("title") || "";
-                            cReplyLabel = (cReplyLabel.length >= 15) ? cReplyLabel.substr(0, 15) + "..." : cReplyLabel;
-                        }
-                    }
-                    if (Y.Lang.isNull(cReplyLabel)) {
-                        cReplyLabel = (!cQuestion.get("allowMultipleReplies")) ? Y.Wegas.I18n.t('mcq.unanswered') : Y.Wegas.I18n.t('mcq.notDone');
-                    }
-                    tab = new Y.Wegas.Tab({
-                        label: '<div class="'
-                            + (highlightUnanswered ? "unread" : "")
-                            + '"><div class="index-label">' + (cQuestion.get("title") || cQuestion.get("label") || "undefined") + '</div>'
-                            + '<div class="index-status">' + cReplyLabel
-                            + '</div></div>',
-                        content: "<div class=\"wegas-loading-div\"><div>"
-                    });
-                    tab.loaded = false;
-                    tab.cQuestion = cQuestion;
-                    this.tabView.add(tab);
-                } else if (cQuestion instanceof Wegas.persistence.ListDescriptor) {
-                    this.addQuestions(cQuestion.get("items"));
-                }
             }
         },
         /**
@@ -186,7 +219,6 @@ YUI.add('wegas-mcq-tabview', function(Y) {
             if (e.newVal && e.newVal.cQuestion
                 && !this.isRemovingTabs && !e.newVal.loaded) {
                 e.newVal.loaded = true;
-
                 widget = new Y.Wegas.MCQView({
                     variable: {
                         "name": e.newVal.cQuestion.get("name")
