00cb4abe079eaf09596126bb34bfdd0e65c121fc
Maxence Laurent <maxence.laurent@gmail.com>
2016-12-13 08:24:29 +0100

    Make Neo4j Singleton cluster-ready
    * do not rely on singleton mechanism to ensure adding one node at the time

--- wegas-core/src/main/java/com/wegas/log/neo4j/Neo4jCommunication.java
+++ wegas-core/src/main/java/com/wegas/log/neo4j/Neo4jCommunication.java
@@ -1,18 +1,23 @@
 package com.wegas.log.neo4j;
 
+import com.hazelcast.core.ILock;
+import com.wegas.core.Helper;
 import org.codehaus.jettison.json.JSONObject;
 
 import javax.ejb.Asynchronous;
 import javax.ejb.LocalBean;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import javax.ejb.Stateless;
+import org.slf4j.LoggerFactory;
 
 /**
  * Neo4j Singleton for communicating with db
  *
  * @author Cyril Junod (cyril.junod at gmail.com)
  */
-@Singleton
+//@Singleton
+@Stateless
 @LocalBean
 public class Neo4jCommunication {
 
@@ -28,12 +33,18 @@ public class Neo4jCommunication {
      */
     @Asynchronous
     public void createLinkedToYoungest(String key, String relationLabel, JSONObject target, String label) {
+        ILock lock = Helper.getHazelcastInstance().getLock("Neo4J");
+        lock.lock();
+        try {
         String query = "CREATE (p:`" + label + "` " + target.toString().replaceAll("\"([^\"]+)\"\\s*:", "$1:") + ") WITH p AS p Match (n " + key
                 + ") WHERE n <> p WITH max(n.starttime) AS max, p AS p MATCH (n "
                 + key + ") WHERE n.starttime = max AND n <> p WITH n AS n, p AS p CREATE (n)-[:`"
                 + relationLabel + "`]->(p) return p";
         String result = Neo4jUtils.queryDBString(query);
         checkError(result);
+        } finally{
+            lock.unlock();
+        }
     }
 
 
