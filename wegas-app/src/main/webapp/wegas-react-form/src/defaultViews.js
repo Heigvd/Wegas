140efc154e7b4a3eda5d945cb92868e973139e40
XaviJunior <xavier.aeby@outlook.com>
2017-01-12 10:25:46 +0100

    Revert "suppression du text area"
    This reverts commit 88277f35f71369f614e6b95510884de912aaba04.

--- wegas-app/src/main/webapp/wegas-react-form/src/defaultViews.js
+++ wegas-app/src/main/webapp/wegas-react-form/src/defaultViews.js
@@ -3,6 +3,7 @@ import StringView from './Views/string';
 import BooleanView from './Views/boolean';
 import UneditableView from './Views/uneditable';
 import HiddenView from './Views/hidden';
+import TextareaView from './Views/textarea';
 import SelectView from './Views/select';
 import ObjectView from './Views/object';
 import ArrayView from './Views/array';
@@ -25,6 +26,7 @@ Form.setDefaultWidgets({
     boolean: BooleanView,
     uneditable: UneditableView,
     hidden: HiddenView,
+    textarea: TextareaView,
     select: SelectView,
     html: HTMLView,
     hashlist: HashlistView,
