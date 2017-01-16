d4e47a2a5e34c7b90b4849550f68578144b80abb
Jarle <jarle.hulaas@heig-vd.ch>
2017-01-16 17:49:17 +0100

    Lobby: new session filtering without side-effects on input fields.

--- wegas-app/src/main/webapp/wegas-lobby/src/app/private/trainer/directives.js
+++ wegas-app/src/main/webapp/wegas-lobby/src/app/private/trainer/directives.js
@@ -8,16 +8,12 @@ angular.module('private.trainer.directives', [
             controller: "TrainerIndexController as trainerIndexCtrl"
         }
     })
-    .controller("TrainerIndexController", function TrainerIndexController($rootScope, $scope, $translate, SessionsModel, Flash, $timeout) {
+    .controller("TrainerIndexController", function TrainerIndexController($rootScope, $scope, $translate, SessionsModel, Flash, $timeout, $filter) {
         "use strict";
-        var ctrl = this,
+        var ctrl = this;
+        /*
             initMaxSessionsDisplayed = function() {
-                /*if (ctrl.sessions.length > 22) {
-                 ctrl.maxSessionsDisplayed = 20;
-                 } else {
-                 ctrl.maxSessionsDisplayed = ctrl.sessions.length;
-                 }*/
-                ctrl.maxSessionsDisplayed = ctrl.sessions.length;
+                ctrl.maxSessionsDisplayed = ctrl.rawSessions.length;
             },
             updateDisplaySessions = function() {
                 if (ctrl.maxSessionsDisplayed === null) {
@@ -30,26 +26,66 @@ angular.module('private.trainer.directives', [
                     }
                 }
             };
+        */
         $rootScope.currentRole = "TRAINER";
         ctrl.loading = true;
         ctrl.search = "";
+        ctrl.rawSessions = [];
         ctrl.sessions = [];
         ctrl.nbArchives = 0;
         ctrl.maxSessionsDisplayed = null;
 
+        /*
+        ** Filters ctrl.rawSessions according to the given search string and puts the result in ctrl.sessions.
+        ** Hypothesis: input array ctrl.rawSessions is already ordered according to the 'createdTime' attribute,
+        ** so that the output automatically follows the same ordering.
+         */
+        ctrl.filterSessions = function(search){
+            if (!search || search.length === 0){
+                ctrl.sessions = ctrl.rawSessions;
+                return;
+            }
+            var res = [],
+                len = ctrl.rawSessions.length,
+                i;
+            for (i=0; i<len; i++){
+                var session = ctrl.rawSessions[i];
+                if (session.gameModel.canView === false) continue;
+                var needle = search.toLowerCase();
+                if ((session.name && session.name.toLowerCase().indexOf(needle) >= 0) ||
+                    (session.createdByName && session.createdByName.toLowerCase().indexOf(needle) >= 0) ||
+                    (session.gameModelName && session.gameModelName.toLowerCase().indexOf(needle) >= 0) ||
+                    (session.gameModel.comments && session.gameModel.comments.toLowerCase().indexOf(needle) >= 0) ||
+                    // If searching for a number, the id has to start with the given pattern:
+                    session.id.toString().indexOf(needle) === 0 ||
+                    session.gameModelId.toString().indexOf(needle) === 0){
+                    res.push(session);
+                }
+            }
+            ctrl.sessions = res; // $filter('orderBy')(res, 'createdTime', true); // May be compound with a 'limitTo' filter
+        };
+
+        // Also called when a session is added or removed:
         ctrl.updateSessions = function(updateDisplay) {
-            var hideScrollbarDuringInitialRender = (ctrl.sessions.length === 0);
+            var hideScrollbarDuringInitialRender = (ctrl.rawSessions.length === 0);
             if (hideScrollbarDuringInitialRender) {
                 $('#trainer-sessions-list').css('overflow-y', 'hidden');
             }
-            ctrl.sessions = [];
+            ctrl.sessions = ctrl.rawSessions = [];
             ctrl.loading = true;
             SessionsModel.getSessions("LIVE").then(function(response) {
                 ctrl.loading = false;
-                ctrl.sessions = response.data || [];
+                ctrl.rawSessions = $filter('orderBy')(response.data, 'createdTime', true) || [];
+                // At this point, the search variable is not yet updated by Angular to reflect the input field:
+                ctrl.search = document.getElementById('searchField').getElementsByClassName('tool__input')[0].value;
+                ctrl.filterSessions(ctrl.search);
+                /*
+                ** Commented out this after suppressing the soft limit on the number of displayed sessions.
+                *
                 if (updateDisplay) {
                     updateDisplaySessions();
                 }
+                */
                 if (hideScrollbarDuringInitialRender) {
                     $timeout(function() {
                         $('#trainer-sessions-list').css('overflow-y', 'auto');
@@ -99,14 +135,15 @@ angular.module('private.trainer.directives', [
         $rootScope.$on('changeSessions', function(e, hasNewData) {
             if (hasNewData) {
                 SessionsModel.getSessions("LIVE").then(function(response) {
-                    ctrl.sessions = response.data || [];
+                    ctrl.updateSessions(true);
                 });
             }
         });
 
         $rootScope.$on('changeLimit', function(e, hasNewData) {
             if (e.currentScope.currentRole === "TRAINER" && hasNewData) {
-                ctrl.updateSessions(true);
+                // @Todo: ultimately remove repeat-autoload feature
+                //ctrl.updateSessions(true);
             }
         });
 
@@ -168,6 +205,7 @@ angular.module('private.trainer.directives', [
                                         scenarioId: 0
                                     };
                                     scope.$emit('collapse');
+                                    scope.search = "";
                                     parentCtrl.updateSessions(true);
                                     button.removeClass("button--disable button--spinner button--rotate");
                                 } else {
@@ -197,7 +235,8 @@ angular.module('private.trainer.directives', [
                 search: "=",
                 archive: "=",
                 editAccess: "=",
-                maximum: "="
+                maximum: "=",
+                filterSessions: "="
             }
     };
     })
