d219cb73555b1d770965e70fa4673c0ee864c09e
Cyril Junod <cyril.junod@gmail.com>
2017-04-18 16:22:59 +0200

    pull ViewSrc out

--- wegas-app/src/main/webapp/wegas-react-form/src/Script/parser.js
+++ wegas-app/src/main/webapp/wegas-react-form/src/Script/parser.js
@@ -1,50 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { parse, print, types } from 'recast';
-import classNames from 'classnames';
-import styles from '../css/string.css';
-/**
- * Toggle view between parsed and code
- */
-class ViewSrc extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { src: false };
-    }
-    render() {
-        let child;
-        if (this.state.src || this.props.error) {
-            child = [(
-                <textarea
-                    key="code"
-                    className={styles.textarea}
-                    defaultValue={this.props.value}
-                    onChange={event => this.props.onChange(event.target.value)}
-                />
-            ), (<div key="error">{this.props.error || <br />}</div>)];
-        } else {
-            child = this.props.children;
-        }
-        return (<span>
-            <i
-                className={classNames('fa fa-code', styles.icon)}
-                onClick={() => this.setState({
-                    src: !this.state.src
-                })}
-            />
-            <div>
-                {child}
-            </div>
-        </span>
-        );
-    }
-}
-ViewSrc.propTypes = {
-    value: PropTypes.string,
-    onChange: PropTypes.func,
-    children: PropTypes.element.isRequired,
-    error: PropTypes.string
-};
+import ViewSrc from './Views/ViewSrc';
 /**
  * HOC Parse code into AST and reverse onChange
  * @param {React.Component} Comp Component to augment
