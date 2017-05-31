f54db5588169e19de92f3078b33e077b29687641
Cyril Junod <cyril.junod@gmail.com>
2017-05-31 12:04:31 +0200

    Form: form
    form editing
    Used in Flexitests (mostly?)

--- wegas-app/src/main/webapp/wegas-app/js/widget/wegas-form.js
+++ wegas-app/src/main/webapp/wegas-app/js/widget/wegas-form.js
@@ -9,10 +9,9 @@
  * @fileoverview
  * @author Francois-Xavier Aeberhard <fx@red-agent.com>
  */
-YUI.add('wegas-form', function (Y) {
-    "use strict";
-
-    var FORM = "form", inputEx = Y.inputEx, Wegas = Y.Wegas, Form;
+YUI.add('wegas-form', function(Y) {
+    'use strict';
+    var FORM = 'form', inputEx = Y.inputEx, Wegas = Y.Wegas, Form;
 
     /**
      * @name Y.Wegas.Form
@@ -23,120 +22,145 @@ YUI.add('wegas-form', function (Y) {
      * @augments Y.Wegas.Widget
      * @constructor
      */
-    Form = Y.Base.create("wegas-form", Y.Widget, [Y.WidgetChild, Wegas.Widget, Wegas.Editable], {
-        /**
+    Form = Y.Base.create(
+        'wegas-form',
+        Y.Widget,
+        [Y.WidgetChild, Wegas.Widget, Wegas.Editable],
+        {
+            /**
          * @lends Y.Wegas.Form#
          */
 
-        // ** Lifecycle Methods ** //
-        /**
+            // ** Lifecycle Methods ** //
+            /**
          * @function
          * @private
          * @description plug a toolbar and publich "submit" event.
          */
-        initializer: function () {
-            this.plug(Y.Plugin.WidgetToolbar);
-            this.publish("submit", {
-                emitFacade: true
-            });
-            this.publish("updated", {
-                emitFacade: false
-            });
-        },
-        /**
+            initializer: function() {
+                this.plug(Y.Plugin.WidgetToolbar);
+                this.publish('submit', {
+                    emitFacade: true
+                });
+                this.publish('updated', {
+                    emitFacade: false
+                });
+            },
+            /**
          * @function
          * @private
          * @description
          */
-        renderUI: function () {
-            Y.Array.each(this.get("buttons"), this.addButton, this);
-            this.get("contentBox").on("key", this.save, "down:83+ctrl", this);  // ctrl-s shortcut
-        },
-        /**
+            renderUI: function() {
+                Y.Array.each(this.get('buttons'), this.addButton, this);
+                this.get('contentBox').on(
+                    'key',
+                    this.save,
+                    'down:83+ctrl',
+                    this
+                ); // ctrl-s shortcut
+            },
+            /**
          * @function
          * @private
          * @description call function "renderToolbar".
          */
-        syncUI: function () {
-            this.set("cfg", this.get("cfg"));
-        },
-        /**
+            syncUI: function() {
+                this.set('cfg', this.get('cfg'));
+            },
+            /**
          * @function
          * @private
          * @returns {undefined}
          */
-        destructor: function () {
-            this.set(FORM, null);
-        },
-        addButton: function (b) {
-            switch (b.action) {
-                case "submit":
-                    b.on = {
-                        click: Y.bind(this.save, this)
-                    };
-                    break;
-                default:
-                    b.on = {
-                        click: Y.bind(function (action) {
-                            this.fire(action);
-                        }, this, b.action)
-                    };
-                    break;
-            }
-            this.toolbar.add(new Wegas.Button(b));
-        },
-        /**
+            destructor: function() {
+                this.set(FORM, null);
+            },
+            addButton: function(b) {
+                switch (b.action) {
+                    case 'submit':
+                        b.on = {
+                            click: Y.bind(this.save, this)
+                        };
+                        break;
+                    default:
+                        b.on = {
+                            click: Y.bind(
+                                function(action) {
+                                    this.fire(action);
+                                },
+                                this,
+                                b.action
+                            )
+                        };
+                        break;
+                }
+                this.toolbar.add(new Wegas.Button(b));
+            },
+            /**
          * @function
          * @private
          * @description set the given form to null
          */
-        destroyForm: function () {
-            this.set(FORM, null);
-        },
-        setCfg: function (val) {
-            var cfg = Y.clone(val);                                             // Duplicate so val will be untouched while serializing
-            Y.mix(cfg, {
-                parentEl: this.get("contentBox"),
-                type: "group"
-            });                                                                 // Set up the form parentEl attribute, so it knows where to render
+            destroyForm: function() {
+                this.set(FORM, null);
+            },
+            setCfg: function(val) {
+                var cfg = Y.clone(val); // Duplicate so val will be untouched while serializing
+                Y.mix(cfg, {
+                    parentEl: this.get('contentBox'),
+                    type: 'group'
+                }); // Set up the form parentEl attribute, so it knows where to render
 
-            inputEx.use(val, Y.bind(function (cfg) {                           // Load form dependencies
-                if (this.get("destroyed")) {
+                inputEx.use(
+                    val,
+                    Y.bind(
+                        function(cfg) {
+                            // Load form dependencies
+                            if (this.get('destroyed')) {
+                                return;
+                            }
+                            var form = inputEx(cfg); // Initialize and render form
+                            form.setValue(this.get('values'), false); // Sync form with "values" ATTR
+                            form.removeClassFromState(); // Remove required state
+                            this.set(FORM, form);
+                            this.fire('formUpdate');
+                            form.on(
+                                'updated',
+                                function(e) {
+                                    this.fire('updated', e);
+                                },
+                                this
+                            );
+                        },
+                        this,
+                        cfg
+                    )
+                );
+            },
+            save: function(e) {
+                e.halt(true);
+
+                var form = this.get(FORM), val = form.getValue();
+
+                if (!form.validate()) {
+                    this.showMessage('error', 'Some fields are not valid.');
                     return;
                 }
-                var form = inputEx(cfg);                                      // Initialize and render form
-                form.setValue(this.get("values"), false);                       // Sync form with "values" ATTR
-                form.removeClassFromState();                                    // Remove required state
-                this.set(FORM, form);
-                this.fire("formUpdate");
-                form.on("updated", function (e) {
-                    this.fire("updated", e);
-                }, this);
-            }, this, cfg));
-        },
-        save: function (e) {
-            e.halt(true);
-
-            var form = this.get(FORM),
-                val = form.getValue();
-
-            if (!form.validate()) {
-                this.showMessage("error", "Some fields are not valid.");
-                return;
+                if (val.valueselector) {
+                    val = val.valueselector;
+                }
+                this.fire('submit', {
+                    value: val
+                });
+            },
+            validate: function() {
+                return this.get('form').validate();
             }
-            if (val.valueselector) {
-                val = val.valueselector;
-            }
-            this.fire("submit", {
-                value: val
-            });
         },
-        validate: function () {
-            return this.get("form").validate();
-        }
-    }, {
+        {
             /** @lends Y.Wegas.Form */
-            EDITORNAME: "Form",
+            EDITORNAME: 'Form',
             /**
              * <p><strong>Attributes</strong></p>
              * <ul>
@@ -153,9 +177,9 @@ YUI.add('wegas-form', function (Y) {
                  * Values of fields of the form
                  */
                 values: {
-                    "transient": true,
+                    transient: true,
                     value: {},
-                    setter: function (val) {
+                    setter: function(val) {
                         if (this.get(FORM)) {
                             this.get(FORM).setValue(val, false);
                         }
@@ -166,9 +190,10 @@ YUI.add('wegas-form', function (Y) {
                  * The form to manage
                  */
                 form: {
-                    "transient": true,
-                    setter: function (val) {
-                        if (this.get(FORM)) {                                       // If there is alread a form instantiated, destroy it
+                    transient: true,
+                    setter: function(val) {
+                        if (this.get(FORM)) {
+                            // If there is alread a form instantiated, destroy it
                             this.get(FORM).destroy();
                         }
                         return val;
@@ -179,7 +204,7 @@ YUI.add('wegas-form', function (Y) {
                  */
                 cfg: {
                     validator: Y.Lang.isObject,
-                    type: "object",
+                    type: 'object',
                     properties: {
                         fields: {
                             type: 'array',
@@ -190,51 +215,102 @@ YUI.add('wegas-form', function (Y) {
                                 properties: {
                                     type: {
                                         type: 'string',
-                                        required: true
+                                        required: true,
+                                        view: {
+                                            type: 'select',
+                                            label: 'Type',
+                                            choices: [
+                                                'number',
+                                                'string',
+                                                'integer',
+                                                'now',
+                                                'select',
+                                                'text'
+                                            ]
+                                        }
+                                    },
+                                    required: {
+                                        type: 'boolean',
+                                        value: false,
+                                        view: { label: 'Required' }
+                                    },
+                                    choices: {
+                                        type: 'array',
+                                        items: {
+                                            type: 'object',
+                                            properties: {
+                                                value: {
+                                                    type: 'string',
+                                                    view: { label: 'Value' }
+                                                },
+                                                label: {
+                                                    type: 'string',
+                                                    view: { label: 'Label' }
+                                                }
+                                            }
+                                        },
+                                        visible: function(val, formVal, path) {
+                                            var curr = formVal;
+                                            for (
+                                                var i = 0;
+                                                i < path.length - 1;
+                                                i++
+                                            ) {
+                                                curr = curr[path[i]];
+                                            }
+                                            return curr.type === 'select';
+                                        },
+                                        view: {
+                                            label: 'Choices'
+                                        }
                                     },
                                     name: {
                                         type: 'string',
-                                        required: true
+                                        required: true,
+                                        view: { label: 'Name' }
                                     },
                                     label: {
-                                        type: 'string'
+                                        type: 'string',
+                                        view: { label: 'Label' }
                                     }
                                 }
                             }
                         }
                     },
-                    setter: function (val) {
+                    setter: function(val) {
                         this.setCfg(val);
                         return val;
                     },
                     index: 8,
                     view: {
-                        label: "Fields",
+                        label: 'Fields',
                         fields: inputEx.Group.groupOptions
                     }
                 },
                 buttons: {
-                    type: "array",
-                    valueFn: function () {
-                        return [{
-                            type: "Button",
-                            action: "submit",
-                            label: "<span class=\"wegas-icon wegas-icon-save\" ></span>Save"
-                        }];
+                    type: 'array',
+                    valueFn: function() {
+                        return [
+                            {
+                                type: 'Button',
+                                action: 'submit',
+                                label: '<span class="wegas-icon wegas-icon-save" ></span>Save'
+                            }
+                        ];
                     },
                     view: {
-                        type: "hidden"
+                        type: 'hidden'
                     }
                 }
             }
-        });
+        }
+    );
     Wegas.Form = Form;
 
     /* Add relevant plugin*/
     Wegas.Form.ATTRS.plugins = Y.clone(Wegas.Widget.ATTRS.plugins);
     Wegas.Form.ATTRS.plugins.items.view.choices.push({
-        label: "Save to",
-        value: "SaveObjectAction"
+        label: 'Save to',
+        value: 'SaveObjectAction'
     });
-
 });
