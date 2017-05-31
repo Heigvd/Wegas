793319198128298088f9e1bb13459b31af080f9d
Jarle <jarle.hulaas@heig-vd.ch>
2017-05-31 14:00:07 +0200

    Redefine constants in form-styles

---
+++ wegas-app/src/main/webapp/wegas-react-form/src/Views/form-styles.ts
@@ -0,0 +1,26 @@
+import { css } from 'glamor';
+
+export namespace FormStyles {
+
+    export const defaultEditorWidth: string = '30em';
+    export const textInputWidth: string = '30em';
+    export const textareaWidth: string = '30em';
+
+    export const labelColor: string = '#6A95B6';
+    export const labelFontSize: string = '15px';
+
+    export const unselectable = css({
+        userSelect: 'none',
+        cursor: 'default'
+    });
+
+    export const labelStyle = css(
+        FormStyles.unselectable,
+        {
+            fontSize: FormStyles.labelFontSize,
+            color: FormStyles.labelColor,
+        }
+    )
+}
+
+export default FormStyles;
