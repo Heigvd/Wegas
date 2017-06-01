fc14c88cc46f4fa5a6f6d062f6530b9058a4d17c
Cyril Junod <cyril.junod@gmail.com>
2017-06-01 16:29:33 +0200

    pluginelement fix update

--- wegas-app/src/main/webapp/wegas-react-form/src/Views/Interacting/pluginelement.tsx
+++ wegas-app/src/main/webapp/wegas-react-form/src/Views/Interacting/pluginelement.tsx
@@ -25,27 +25,28 @@ interface IValue {
     fn: string;
     cfg: { [key: string]: any };
 }
-const AsyncForm = asyncReactor(({ value, onChange }:
-    { value: IValue, onChange: (value: IValue) => void }) => {
-    const Y = getY();
-    return new Promise((resolve) => {
-        let schema;
-        if (value && value.fn) {
-            Y.Wegas.use({ type: value.fn }, () => {
-                // load required modules
-                const targetPlg = Y.Plugin[value.fn] as Y.Plugin_Base;
-                const w: Y.Plugin_Host = new Y.Wegas.Text(); // Use this hack to retrieve a plugin config
-                w.plug(targetPlg);
-                const cfg = (w as any)[targetPlg.NS].getFormCfg();
-                cfg.name = targetPlg.NAME;
-                schema = updateCfg(cfg);
-                resolve(<Form schema={schema} value={value} onChange={onChange} />);
-            });
-        }
-    });
-}, () => <i>Loading ...</i>);
+const Y = getY();
 function PluginElement({ value, onChange, view }:
     { value: { fn: string }, onChange: (value: object) => void, view: { choices: {}[] } }) {
+    const AsyncForm = asyncReactor(({ value, onChange }:
+        { value: IValue, onChange: (value: IValue) => void }) => {
+
+        return new Promise((resolve) => {
+            let schema;
+            if (value && value.fn) {
+                Y.Wegas.use({ type: value.fn }, () => {
+                    // load required modules
+                    const targetPlg = Y.Plugin[value.fn] as Y.Plugin_Base;
+                    const w: Y.Plugin_Host = new Y.Wegas.Text(); // Use this hack to retrieve a plugin config
+                    w.plug(targetPlg);
+                    const cfg = (w as any)[targetPlg.NS].getFormCfg();
+                    cfg.name = targetPlg.NAME;
+                    schema = updateCfg(cfg);
+                    resolve(<Form schema={schema} value={value} onChange={onChange} />);
+                });
+            }
+        });
+    }, () => <i>Loading ...</i>);
     if (value && value.fn) {
         return <AsyncForm value={value} onChange={onChange} />;
     }
