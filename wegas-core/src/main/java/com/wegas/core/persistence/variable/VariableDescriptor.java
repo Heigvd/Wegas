754d8dae6753d0c0f9cca7adc066cc9ab9c57b58
Maxence Laurent <maxence.laurent@gmail.com>
2017-06-15 09:23:06 +0200

    Rollback Stupidity
    * put @GeneratedValue back

--- wegas-core/src/main/java/com/wegas/core/persistence/variable/VariableDescriptor.java
+++ wegas-core/src/main/java/com/wegas/core/persistence/variable/VariableDescriptor.java
@@ -175,6 +175,7 @@ abstract public class VariableDescriptor<T extends VariableInstance> extends Nam
      */
     @Id
     @Column(name = "variabledescriptor_id")
+    @GeneratedValue
     @JsonView(Views.IndexI.class)
     private Long id;
 
