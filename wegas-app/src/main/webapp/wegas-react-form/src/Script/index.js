94f3248679be16195f5097c611f29ee6b51a6e8f
Cyril Junod <cyril.junod@gmail.com>
2017-06-19 17:13:33 +0200

    Make script decorated with commonView.
    Mostly to show error messages.

--- wegas-app/src/main/webapp/wegas-react-form/src/Script/index.js
+++ wegas-app/src/main/webapp/wegas-react-form/src/Script/index.js
@@ -15,9 +15,11 @@ import commonView from '../HOC/commonView';
 const VariableStatement = commonView(
     scriptObject(parsed(singleStatement(Variable)))
 );
-const MultiVariableMethod = scriptObject(parsed(multipleStatement(Impact)));
-const MultiVariableCondition = scriptObject(
-    parsed(condition(multipleStatement(Condition)))
+const MultiVariableMethod = commonView(
+    scriptObject(parsed(multipleStatement(Impact)))
+);
+const MultiVariableCondition = commonView(
+    scriptObject(parsed(condition(multipleStatement(Condition))))
 );
 // Using Script edition outside form.
 function scriptRenderer(Component) {
