97d9a0bfc2942ff84d42a06c88ff5790a1e81f9f
Cyril Junod <cyril.junod@gmail.com>
2017-06-19 12:16:21 +0200

    Restore modal on whole page

--- wegas-app/src/main/webapp/wegas-react-form/src/Components/Cover.tsx
+++ wegas-app/src/main/webapp/wegas-react-form/src/Components/Cover.tsx
@@ -1,41 +1,31 @@
 import React from 'react';
 import { css } from 'glamor';
 
-const absolute = css({ position: 'absolute', zIndex: 1 })
+const absolute = css({ position: 'absolute', zIndex: 1 });
 
 const coverStyle = css({
     position: 'fixed',
-    backgroundColor: 'rgba(0,0,0,0.3)'
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
 });
 
 interface Props {
     children: React.ReactChild;
     zIndex?: number;
-    onClick?: () => void
+    onClick?: () => void;
 }
 
 export function Cover(props: Props) {
-    let top=0,
-        left=0;
-    const forms = document.getElementsByClassName('wegas-react-form-content');
-    if (forms.length !== 0) {
-        const rect = forms[0].getBoundingClientRect();
-        top = rect.top;
-        left = rect.left - 5; // Hack to suppress left margin
-    }
     // NB: The cover will not be dynamically readjusted if the frame is resized when the cover is displayed.
-    const coverFinalStyle = css(
-        coverStyle, {
-        top: top,
-        left: left,
-        right: 0,
-        bottom: 0
-    });
     return (
-        <div {...css({ position: 'relative', zIndex: props.zIndex }) }>
-            <div {...coverFinalStyle} onClick={props.onClick} />
+        <div {...css({ position: 'relative', zIndex: props.zIndex })}>
+            <div {...coverStyle} onClick={props.onClick} />
             <div {...absolute}>
                 {props.children}
             </div>
-        </div>);
+        </div>
+    );
 }
