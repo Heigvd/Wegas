e540344c8f6025736f59b07585cdb3cb9f4f9de2
Cyril Junod <cyril.junod@gmail.com>
2017-07-05 16:05:55 +0200

    Update react-ace
    Corrects typings

--- wegas-app/src/main/webapp/wegas-react-form/src/Script/Views/JSEditor.tsx
+++ wegas-app/src/main/webapp/wegas-react-form/src/Script/Views/JSEditor.tsx
@@ -4,12 +4,8 @@ import 'brace/mode/javascript';
 import 'brace/theme/kuroir';
 
 export default function JSEditor(props: AceEditorProps) {
-    // Tell Typescript what is AceEditor. Error in the declaration:
-    // 'undefined' state is not valid with new react types.
-    // As of react-ace v5.0.1
-    const Editor: React.ComponentClass<AceEditorProps> = AceEditor as any;
     return (
-        <Editor
+        <AceEditor
             width="100%"
             {...props}
             mode="javascript"
