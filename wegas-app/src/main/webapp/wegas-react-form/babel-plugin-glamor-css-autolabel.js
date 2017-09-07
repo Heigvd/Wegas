e46a34973f84184a7abea8ac42530f6ae99085c5
Cyril Junod <cyril.junod@gmail.com>
2017-09-07 10:19:59 +0200

    Babel plugin: name glamor css
    Dev purpose. Set label if unset

---
+++ wegas-app/src/main/webapp/wegas-react-form/babel-plugin-glamor-css-autolabel.js
@@ -0,0 +1,49 @@
+function hasLabel(path) {
+    return path.node.properties.some(a => {
+        if (a.type !== 'ObjectProperty') {
+            return false;
+        }
+        return a.key.name === 'label';
+    });
+}
+module.exports = function transform(babel) {
+    const { types: t } = babel;
+    let isGCSS = false;
+    return {
+        visitor: {
+            ImportDeclaration(path) {
+                if (path.node.source.value === 'glamor') {
+                    const impCSS = path.node.specifiers.find(
+                        s => s.imported.name === 'css'
+                    );
+                    if (impCSS) {
+                        isGCSS = impCSS.local.name;
+                    }
+                }
+            },
+            CallExpression(path) {
+                if (isGCSS && path.node.callee.name === isGCSS) {
+                    let name = '';
+                    const filename = this.file.opts.sourceFileName.replace(
+                        /\//g,
+                        '£'
+                    );
+                    if (path.parent.id) {
+                        name = `££${path.parent.id.name}£££`;
+                    }
+                    path.get('arguments').forEach(path => {
+                        if (path.isObjectExpression() && !hasLabel(path)) {
+                            path.node.properties = [
+                                t.ObjectProperty(
+                                    t.Identifier('label'),
+                                    t.StringLiteral(`${filename}${name}`)
+                                ),
+                                ...path.node.properties,
+                            ];
+                        }
+                    });
+                }
+            },
+        },
+    };
+};
