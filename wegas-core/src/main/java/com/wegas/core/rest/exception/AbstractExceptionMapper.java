75421ad39979216e6e8b9b7a06c8b6c14a0c842a
Maxence Laurent <maxence.laurent@gmail.com>
2017-10-16 11:49:15 +0200

    Update Postgresql JDBC to match 9.6
    * JDBC driver 42.1.4

--- wegas-core/src/main/java/com/wegas/core/rest/exception/AbstractExceptionMapper.java
+++ wegas-core/src/main/java/com/wegas/core/rest/exception/AbstractExceptionMapper.java
@@ -10,7 +10,6 @@ package com.wegas.core.rest.exception;
 import com.wegas.core.exception.client.WegasConflictException;
 import com.wegas.core.exception.client.WegasErrorMessage;
 import com.wegas.core.exception.client.WegasUniqueConstraintException;
-import java.util.Set;
 import javax.ejb.EJBException;
 import javax.enterprise.event.ObserverException;
 import javax.persistence.OptimisticLockException;
