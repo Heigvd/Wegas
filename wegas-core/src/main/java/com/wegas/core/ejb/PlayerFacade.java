acde10ef81a94afb775bba8d54b2dfc87a9082c9
Maxence Laurent <maxence.laurent@gmail.com>
2017-11-06 13:15:11 +0100

    Fix import

--- wegas-core/src/main/java/com/wegas/core/ejb/PlayerFacade.java
+++ wegas-core/src/main/java/com/wegas/core/ejb/PlayerFacade.java
@@ -33,6 +33,7 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
