f9743fe65968a48813b37ee81eabb0575bbfb18c
maxencelaurent <maxence.laurent@gmail.com>
2018-03-13 14:24:53 +0100

    Update ListDescriptor.java
    fix isAuthorized

--- wegas-core/src/main/java/com/wegas/core/persistence/variable/ListDescriptor.java
+++ wegas-core/src/main/java/com/wegas/core/persistence/variable/ListDescriptor.java
@@ -131,7 +131,7 @@ public class ListDescriptor extends VariableDescriptor<VariableInstance> impleme
      * @param child
      */
     private boolean isAuthorized(VariableDescriptor child) {
-        return this.isAuthorized(child.getClass().getSimpleName());
+        return this.isAuthorized(child.getJSONClassName());
     }
 
     /**
