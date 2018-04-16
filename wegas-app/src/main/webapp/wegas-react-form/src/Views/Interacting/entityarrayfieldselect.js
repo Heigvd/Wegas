178fbab6083dd59d5191cf58ba5b48594fe9e0e5
Cyril Junod <cyril.junod@gmail.com>
2018-04-16 11:52:29 +0200

    EntityArrayFieldSelect, out of sync fix.
    fix # 1373

--- wegas-app/src/main/webapp/wegas-react-form/src/Views/Interacting/entityarrayfieldselect.js
+++ wegas-app/src/main/webapp/wegas-react-form/src/Views/Interacting/entityarrayfieldselect.js
@@ -4,7 +4,7 @@ import Select from '../select';
 import { getY } from '../../index';
 
 function optionNameToString(result, name) {
-    const separator = (name) ? name.separator || ',' : ',';
+    const separator = name ? name.separator || ',' : ',';
     if (!name || !name.values || name.values.length <= 0) {
         return ['undefined'];
     }
@@ -12,23 +12,25 @@ function optionNameToString(result, name) {
 }
 function EntityArrayFieldSelect(props) {
     const Y = getY();
-    const {field, returnAttr, scope, entity, name, ...restView} = props.view;
-    const computedEntity = entity ?
-        Y.Wegas.Facade.Variable.cache.find('name', entity) :
-        Y.Plugin.EditEntityAction.currentEntity;
+    const { field, returnAttr, scope, entity, name, ...restView } = props.view;
+    const computedEntity = entity
+        ? Y.Wegas.Facade.Variable.cache.find('name', entity)
+        : Y.Plugin.EditEntityAction.currentEntity;
     let results;
     if (scope !== 'instance') {
         results = computedEntity.get(field);
     } else {
         results = computedEntity.getInstance().get(field);
     }
+    if (results == null) {
+        return null;
+    }
     const choices = results.map(r => ({
-            value: (r && r.get ? r.get(returnAttr || 'name') : r),
-            label: (r && r.get ? r.getEditorLabel() || optionNameToString(r, name) : r)
-        }));
-    return (
-        <Select {...props} view={{...restView, choices}} />
-        );
+        value: r && r.get ? r.get(returnAttr || 'name') : r,
+        label:
+            r && r.get ? r.getEditorLabel() || optionNameToString(r, name) : r,
+    }));
+    return <Select {...props} view={{ ...restView, choices }} />;
 }
 
 EntityArrayFieldSelect.propTypes = {
@@ -39,8 +41,8 @@ EntityArrayFieldSelect.propTypes = {
         entity: PropTypes.string,
         name: PropTypes.shape({
             values: PropTypes.arrayOf(PropTypes.string),
-            separator: PropTypes.string
-        })
-    }).isRequired
+            separator: PropTypes.string,
+        }),
+    }).isRequired,
 };
 export default EntityArrayFieldSelect;
