a4b7f0634df3dd95577d295e039c52ff08916077
Maxence Laurent <maxence.laurent@gmail.com>
2018-05-28 15:50:10 +0200

    Default value for TranslatableContent ATTRS
    * fixes #1383

--- wegas-app/src/main/webapp/wegas-app/js/persistence/wegas-entity.js
+++ wegas-app/src/main/webapp/wegas-app/js/persistence/wegas-entity.js
@@ -123,12 +123,12 @@ YUI.add('wegas-entity', function(Y) {
                     }
                     return value;
                 },
-                /*valueFn: function() {
-                 return {
-                 "@class": "TranslatableContent",
-                 translations: {}
-                 };
-                 },*/
+                valueFn: function() {
+                    return {
+                        "@class": "TranslatableContent",
+                        translations: {}
+                    };
+                },
                 properties: TRANSLATION_CONTENT_ATTR_DEF
             };
         return TRANSLATION_VIEW;
@@ -403,7 +403,7 @@ YUI.add('wegas-entity', function(Y) {
             activeLanguages: {
                 "transient": true,
                 getter: function(val) {
-                    return Y.Array.filter(this.get("languages"), function(l){
+                    return Y.Array.filter(this.get("languages"), function(l) {
                         return l.get("active");
                     });
                 }
