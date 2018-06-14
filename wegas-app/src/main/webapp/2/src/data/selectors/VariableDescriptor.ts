3fec5241e7d3f121e6f0cd9cc0ecdf9791db5940
Cyril Junod <cyril.junod@gmail.com>
2018-06-14 16:28:19 +0200

    Variable find by key

--- wegas-app/src/main/webapp/2/src/data/selectors/VariableDescriptor.ts
+++ wegas-app/src/main/webapp/2/src/data/selectors/VariableDescriptor.ts
@@ -7,7 +7,9 @@ import { store } from '../store';
  * @param {number} [id]  variableDescriptor id
  * @returns {(Readonly<IVariableDescriptor> | undefined)}
  */
-export function select(id?: number): Readonly<IVariableDescriptor> | undefined;
+export function select<T extends IVariableDescriptor = IVariableDescriptor>(
+  id?: number,
+): Readonly<T> | undefined;
 /**
  * Find a list of variableDescriptor for a list of ids
  *
@@ -27,4 +29,14 @@ export function select(id: number | number[] | undefined) {
     return id.map(i => state.variableDescriptors[i]);
   }
   return state.variableDescriptors[id];
+}
+
+export function find<T extends IVariableDescriptor>(key: keyof T, value: any) {
+  const state = store.getState();
+  for (const vd in state.variableDescriptors) {
+    const s = state.variableDescriptors[vd] as T;
+    if (s && s[key] === value) {
+      return s;
+    }
+  }
 }
