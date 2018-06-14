02692bdd0f1ada4609a7042eb384e1bb37e7c169
Cyril Junod <cyril.junod@gmail.com>
2018-06-14 16:29:24 +0200

    Define methods on VariableDescriptor

---
+++ wegas-app/src/main/webapp/2/src/data/methods/VariableDescriptor.ts
@@ -0,0 +1,17 @@
+import { TranslatableContent } from '../i18n';
+import { VariableDescriptor, GameModel } from '../selectors';
+
+export function editorLabel(vd: IVariableDescriptor) {
+  const label = TranslatableContent.toString(vd.label);
+  if (vd.editorTag && label) {
+    return `${vd.editorTag} - ${label}`;
+  }
+  return vd.editorTag || label;
+}
+
+export function getParent(vd: IVariableDescriptor) {
+  if (vd.parentDescriptorType === 'VariableDescriptor') {
+    return VariableDescriptor.select(vd.parentDescriptorId);
+  }
+  return GameModel.select(vd.parentDescriptorId);
+}
--- wegas-app/src/main/webapp/2/src/data/selectors/GameModel.ts
+++ wegas-app/src/main/webapp/2/src/data/selectors/GameModel.ts
@@ -2,11 +2,15 @@ import { store } from '../store';
 
 /**
  * Get the current GameModel
- *
- * @export
- * @returns {Readonly<IGameModel>}
  */
 export function selectCurrent() {
   const state = store.getState();
   return state.gameModels[state.global.currentGameModelId];
+}
+/**
+ * Get the gameModel with id
+ * @param id gameModel's id
+ */
+export function select(id: number) {
+  return store.getState().gameModels[id];
 }
