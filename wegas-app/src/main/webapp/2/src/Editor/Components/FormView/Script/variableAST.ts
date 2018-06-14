288ee0db297d728ed9f800abf7c7e15744fc1568
Cyril Junod <cyril.junod@gmail.com>
2018-06-14 16:38:29 +0200

    ast Variable helper

---
+++ wegas-app/src/main/webapp/2/src/Editor/Components/FormView/Script/variableAST.ts
@@ -0,0 +1,64 @@
+import {
+  CallExpression,
+  callExpression,
+  Expression,
+  Identifier,
+  identifier,
+  isCallExpression,
+  isIdentifier,
+  isMemberExpression,
+  isStringLiteral,
+  MemberExpression,
+  memberExpression,
+  StringLiteral,
+  stringLiteral,
+} from '@babel/types';
+
+interface VariableAST extends CallExpression {
+  callee: {
+    object: {
+      type: 'Identifier';
+      name: 'Variable';
+    } & Identifier;
+    property: {
+      type: 'Identifier';
+      name: 'find';
+    } & Identifier;
+    computed: false;
+  } & MemberExpression;
+  arguments: [{ name: 'gameModel' } & Identifier, StringLiteral];
+}
+interface VariableCallAST extends CallExpression {
+  callee: {
+    object: VariableAST;
+    property: Identifier;
+  } & MemberExpression;
+}
+export function isVariable(node: Expression): node is VariableAST {
+  return (
+    isCallExpression(node) &&
+    isMemberExpression(node.callee, { computed: false }) &&
+    isIdentifier(node.callee.object, { name: 'Variable' }) &&
+    isIdentifier(node.callee.property, { name: 'find' }) &&
+    isIdentifier(node.arguments[0], { name: 'gameModel' }) &&
+    isStringLiteral(node.arguments[1])
+  );
+}
+export function isVariableCall(node: Expression): node is VariableCallAST {
+  return (
+    isCallExpression(node) &&
+    isMemberExpression(node.callee, { computed: false }) &&
+    isVariable(node.callee.object) &&
+    isIdentifier(node.callee.property)
+  );
+}
+export function variableName(node: VariableAST) {
+  return node.arguments[1].value;
+}
+
+export function createVariableAST(name: string) {
+  return callExpression(
+    memberExpression(identifier('Variable'), identifier('find')),
+    [identifier('gameModel'), stringLiteral(name)],
+  ) as VariableAST;
+}
