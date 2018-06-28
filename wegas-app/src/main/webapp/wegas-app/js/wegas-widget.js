ddfdd2270c0c699794c9aadebc14351757a70844
Cyril Junod <cyril.junod@gmail.com>
2018-06-28 14:10:38 +0200

    Fix Event logger plugin
    Also an advanced feature

--- wegas-app/src/main/webapp/wegas-app/js/wegas-widget.js
+++ wegas-app/src/main/webapp/wegas-app/js/wegas-widget.js
@@ -557,9 +557,9 @@ YUI.add('wegas-widget', function(Y) {
                                     label: 'Lock',
                                     value: {fn: 'Lockable'}
                                 }, {
-                                    type: BUTTON,
                                     label: "Event Logger",
-                                    data: "EventLogger"
+                                    value: {fn: "EventLogger"},
+                                    className: "wegas-advanced-feature"
                                 }
                             ]
                         }
