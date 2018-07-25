6d8d8ac0b9ab7e8f94ac974cafa8181dd84caefc
Cyril Junod <cyril.junod@gmail.com>
2018-07-25 10:18:28 +0200

    Component import check for it beeing mounted
    Avoid setting state on unmouted component

--- wegas-app/src/main/webapp/2/src/Components/index.tsx
+++ wegas-app/src/main/webapp/2/src/Components/index.tsx
@@ -7,14 +7,19 @@ export function importComponent(type: string) {
   class AutoLoadedComponent extends React.Component {
     static displayName = `Loaded(${type})`;
     state: { Comp: React.ComponentType<any> } = { Comp: Loader };
+    mounted: boolean = true;
     componentDidMount() {
       import(`./AutoImport/${type}`)
         .then(exp => {
-          this.setState({ Comp: exp.default });
+          this.mounted && this.setState({ Comp: exp.default });
         })
         .catch(() => {
-          this.setState({ Comp: () => <span>Error loading "{type}"</span> });
+          this.mounted &&
+            this.setState({ Comp: () => <span>Error loading "{type}"</span> });
         });
+    }
+    componentWillUnmount() {
+      this.mounted = false;
     }
     render() {
       return <this.state.Comp {...this.props} />;
