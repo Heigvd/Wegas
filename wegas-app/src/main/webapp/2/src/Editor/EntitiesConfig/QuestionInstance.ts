ca921ef955ee1c80f8d521bd14af0fdafbb250c9
Cyril Junod <cyril.junod@gmail.com>
2018-07-31 15:22:29 +0200

    Fix missing instance from questions

--- wegas-app/src/main/webapp/2/src/Editor/EntitiesConfig/QuestionInstance.ts
+++ wegas-app/src/main/webapp/2/src/Editor/EntitiesConfig/QuestionInstance.ts
@@ -2,6 +2,11 @@ import { ConfigurationSchema } from '../editionConfig';
 import { config as variableInstanceConfig } from './VariableInstance';
 export const config: ConfigurationSchema<IQuestionInstance> = {
   ...variableInstanceConfig,
+  '@class': {
+    type: 'string',
+    value: 'QuestionInstance',
+    view: { type: 'hidden' },
+  },
   active: {
     type: 'boolean',
     view: { label: 'Active' },
