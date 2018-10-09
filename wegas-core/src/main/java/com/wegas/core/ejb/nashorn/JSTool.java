cd0fa23526fa9d205e742c783f181c16cc3a38ef
Cyril Junod <cyril.junod@gmail.com>
2018-10-09 12:27:47 +0200

    Add commented JDK9 version
    rename JSParseError -> JSValidationError

--- wegas-core/src/main/java/com/wegas/core/ejb/nashorn/JSTool.java
+++ wegas-core/src/main/java/com/wegas/core/ejb/nashorn/JSTool.java
@@ -123,10 +123,10 @@ public class JSTool {
             final Expression function = callNode.getFunction();
             if (function instanceof IdentNode) {
                 if ("Function".equals(((IdentNode) function).getName())) {
-                    throw new JSParseError("Function is Evil");
+                    throw new JSValidationError("Function is Evil");
                 }
                 if ("eval".equals(((IdentNode) function).getName())) {
-                    throw new JSParseError("Eval is Evil");
+                    throw new JSValidationError("Eval is Evil");
                 }
             }
             return super.enterCallNode(callNode);
@@ -150,6 +150,7 @@ public class JSTool {
 
         /**
          * Sortable operation which can take place on a string.
+         *
          * @deprecated since JDK9. Operations on StringBuilder can take place immediately.
          */
         @Deprecated
@@ -183,9 +184,159 @@ public class JSTool {
         }
     }
 
-    public static class JSParseError extends WegasRuntimeException {
-        public JSParseError(String message) {
+    public static class JSValidationError extends WegasRuntimeException {
+        public JSValidationError(String message) {
             super(message);
         }
     }
 }
+
+// !!! This is what jdk9+ version could be
+
+
+//import com.wegas.core.exception.client.WegasRuntimeException;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import jdk.nashorn.api.tree.CompilationUnitTree;
+//import jdk.nashorn.api.tree.DoWhileLoopTree;
+//import jdk.nashorn.api.tree.ForInLoopTree;
+//import jdk.nashorn.api.tree.ForLoopTree;
+//import jdk.nashorn.api.tree.FunctionCallTree;
+//import jdk.nashorn.api.tree.FunctionDeclarationTree;
+//import jdk.nashorn.api.tree.FunctionExpressionTree;
+//import jdk.nashorn.api.tree.IdentifierTree;
+//import jdk.nashorn.api.tree.Parser;
+//import jdk.nashorn.api.tree.SimpleTreeVisitorES5_1;
+//import jdk.nashorn.api.tree.StatementTree;
+//import jdk.nashorn.api.tree.WhileLoopTree;
+//
+//public class JSTool {
+//    /**
+//     * Convert code in String form to it's AST from. Nashorn's AST
+//     *
+//     * @param code source
+//     * @return AST
+//     */
+//    public static CompilationUnitTree parse(String code) {
+//
+//        return Parser.create().parse("internal", code, null);
+//    }
+//
+//    /**
+//     * Inject a specific sanitizer code into some chosen place of given code.
+//     *
+//     * @param code      code to sanitize
+//     * @param injection code to inject inline. It should end with a semicolon.
+//     * @return Sanitized code
+//     */
+//    public static String sanitize(String code, String injection) {
+//        final CompilationUnitTree node = parse(code);
+//        final Visitor visitor = new Visitor(code, injection);
+//        node.accept(visitor, null);
+//        // inject at start to avoid rewrite from the code.
+//        return injection + visitor.getResult();
+//    }
+//
+//    private static class Visitor extends SimpleTreeVisitorES5_1<Void, Void> {
+//        private long off = 0;
+//        private final StringBuilder res;
+//        private final String toInject;
+//        private final int injectionLength;
+//        private final static Set<String> forbiddenCalls = Set.of(new String[]{"Function", "eval"});
+//
+//        public String getResult() {
+//            return res.toString();
+//        }
+//
+//        public Visitor(String code, String injection) {
+//            super();
+//            this.res = new StringBuilder(code);
+//            this.toInject = injection;
+//            this.injectionLength = injection.length();
+//        }
+//
+//        @Override
+//        public Void visitFunctionDeclaration(FunctionDeclarationTree node, Void r) {
+//            long idx = node.getBody().getStartPosition() + off + 1;
+//            res.insert(Math.toIntExact(idx), toInject);
+//            off += this.injectionLength;
+//            return null;
+//        }
+//
+//        @Override
+//        public Void visitFunctionExpression(FunctionExpressionTree node, Void r) {
+//            long idx = node.getBody().getStartPosition() + off + 1;
+//            res.insert(Math.toIntExact(idx), toInject);
+//            off += this.injectionLength;
+//            return null;
+//        }
+//
+//        @Override
+//        public Void visitWhileLoop(WhileLoopTree node, Void r) {
+//            node.getCondition().accept(this, r);
+//            blockWrap(node.getStatement());
+//            return null;
+//        }
+//
+//        @Override
+//        public Void visitDoWhileLoop(DoWhileLoopTree node, Void r) {
+//            blockWrap(node.getStatement());
+//            node.getCondition().accept(this, r);
+//            return null;
+//        }
+//
+//        @Override
+//        public Void visitForLoop(ForLoopTree node, Void r) {
+//            node.getInitializer().accept(this, r);
+//            node.getCondition().accept(this, r);
+//            node.getUpdate().accept(this, r);
+//            blockWrap(node.getStatement());
+//            return null;
+//        }
+//
+//        @Override
+//        public Void visitForInLoop(ForInLoopTree node, Void r) {
+//            node.getVariable().accept(this, r);
+//            node.getExpression().accept(this, r);
+//            blockWrap(node.getStatement());
+//            return null;
+//        }
+//
+//        @Override
+//        public Void visitFunctionCall(FunctionCallTree node, Void r) {
+//            var fn = node.getFunctionSelect();
+//            if (fn instanceof IdentifierTree) {
+//                String name = ((IdentifierTree) fn).getName();
+//                if (forbiddenCalls.contains(name)) {
+//                    throw new JSValidationError(name + " is Evil");
+//                }
+//            }
+//            return super.visitFunctionCall(node, r);
+//        }
+//
+//        /**
+//         * Wraps everything (BlockStatement included -- blockchain) into a
+//         * BlockStatement starting with the injection text
+//         *
+//         * @param block the BlockStatement to wrap. The parser gives us a BlockStatement
+//         *              even for a single Statement in the source
+//         */
+//        private void blockWrap(StatementTree block) {
+//            long idx = block.getStartPosition() + off;
+//            res.insert(Math.toIntExact(idx), "{").insert(Math.toIntExact(idx) + 1, toInject);
+//            off += this.injectionLength + 1;
+//            block.accept(this, null);
+//            res.insert(Math.toIntExact(block.getEndPosition() + off), "}");
+//            off += 1;
+//        }
+//    }
+//
+//     public static class JSValidationError extends WegasRuntimeException {
+//         public JSValidationError(String message) {
+//             super(message);
+//         }
+//     }
+//}
+
