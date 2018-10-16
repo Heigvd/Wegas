f34eb44005492406d35054bee4ac693da1a1cc0b
Cyril Junod <cyril.junod@gmail.com>
2018-10-16 14:53:35 +0200

    form +jseditor view

--- wegas-app/src/main/webapp/wegas-react-form/src/Script/Views/asyncJSEditor.tsx
+++ wegas-app/src/main/webapp/wegas-react-form/src/Script/Views/asyncJSEditor.tsx
@@ -1,9 +1,7 @@
 import loadAsync from '../../HOC/loadAsyncComp';
-import React from 'react';
-import { AceEditorProps } from 'react-ace';
 
 export default loadAsync(() =>
     import(/* webpackChunkName: "ace-js" */ './JSEditor').then(
         ({ JSEditor }) => JSEditor
     )
-) as React.ComponentClass<AceEditorProps>;
+);
