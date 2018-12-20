7965a2cc1530ef9bfcb479d2805dab47a73f38c1
Cyril Junod <cyril.junod@gmail.com>
2018-12-20 13:53:49 +0100

    xapi: post statements
    instead of logging them

--- wegas-core/src/main/java/com/wegas/log/xapi/Xapi.java
+++ wegas-core/src/main/java/com/wegas/log/xapi/Xapi.java
@@ -1,5 +1,7 @@
 package com.wegas.log.xapi;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -9,14 +11,18 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import com.wegas.core.Helper;
 import com.wegas.core.ejb.RequestManager;
+import com.wegas.core.persistence.game.DebugTeam;
 import com.wegas.core.persistence.game.Game;
+import com.wegas.core.persistence.game.Player;
 import com.wegas.core.security.ejb.UserFacade;
 import com.wegas.core.security.persistence.User;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import gov.adlnet.xapi.client.StatementClient;
 import gov.adlnet.xapi.model.*;
 
 @Stateless
@@ -33,7 +39,7 @@ public class Xapi implements XapiI {
     @Override
     public Statement userStatement(final String verb, final IStatementObject object) {
 
-        final Agent agent = this.account(manager.getCurrentUser());
+        final Agent agent = this.agent(manager.getCurrentUser());
         final Verb v = new Verb(verb);
         final Statement stmt = new Statement(agent, v, object);
 
@@ -68,13 +74,38 @@ public class Xapi implements XapiI {
 
     @Override
     public void post(Statement stmt) {
+        if (!isValid()) {
+            logger.warn("Failed to persist an xapi statement, invalid context\n{}", serialize(stmt));
+            return;
+        }
         stmt.setContext(this.genContext());
-        logger.info("[post]{}", this.serialize(stmt));
+        try {
+            this.getClient().postStatement(stmt);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+
+        }
     }
 
     @Override
     public void post(List<Statement> stmts) {
-        stmts.forEach(stmt -> post(stmt));
+        if (!isValid()) {
+            for (Statement s : stmts) {
+                logger.warn("Failed to persist an xapi statement, invalid context\n{}", serialize(s));
+            }
+            return;
+        }
+        Context ctx = this.genContext();
+        stmts.forEach(stmt -> stmt.setContext(ctx));
+        try {
+            this.getClient().postStatements(new ArrayList<>(stmts));
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private StatementClient getClient() throws MalformedURLException {
+        return new StatementClient(Helper.getWegasProperty("xapi.host"), Helper.getWegasProperty("xapi.auth"));
     }
 
     private Context genContext() {
@@ -83,11 +114,13 @@ public class Xapi implements XapiI {
 
         final Context context = new Context();
         final List<User> instructorsUser = userFacade.findEditors("g" + game.getId());
-        final ArrayList<Agent> instructorsAgent = new ArrayList<>();
-        for (User u : instructorsUser) {
-            instructorsAgent.add(this.account(u));
+        if (instructorsUser.size() > 0) {
+            final ArrayList<Agent> instructorsAgent = new ArrayList<>();
+            for (User u : instructorsUser) {
+                instructorsAgent.add(this.agent(u));
+            }
+            context.setInstructor(new Group(instructorsAgent));
         }
-        context.setInstructor(new Group(instructorsAgent));
 
         ContextActivities ctx = new ContextActivities();
         ctx.setCategory(new ArrayList<Activity>() {
@@ -106,7 +139,25 @@ public class Xapi implements XapiI {
         return context;
     }
 
-    private Agent account(User user) {
+    /**
+     * Transform a user into an Agent
+     */
+    private Agent agent(User user) {
         return new Agent(null, new Account(String.valueOf(user.getId()), "https://wegas.albasim.ch"));
+    }
+
+    /**
+     * Check if there is a current player, not Debug, there is a LogID and xapi is
+     * configured
+     */
+    private Boolean isValid() {
+        final Player player = manager.getPlayer();
+        if (player == null || player.getTeam() instanceof DebugTeam || player.getTeam() instanceof DebugTeam
+                || Helper.isNullOrEmpty(player.getGameModel().getProperties().getLogID())
+                || Helper.isNullOrEmpty(Helper.getWegasProperty("xapi.auth"))
+                || Helper.isNullOrEmpty(Helper.getWegasProperty("xapi.host"))) {
+            return false;
+        }
+        return true;
     }
 }
