3a822f2eb96293d848217202281abd50e3669c92
Cyril Junod <cyril.junod@gmail.com>
2019-05-02 16:50:23 +0200

    Schema + index

--- wegas-core/src/main/java/com/wegas/editor/JSONSchema/JSONExtendedSchema.java
+++ wegas-core/src/main/java/com/wegas/editor/JSONSchema/JSONExtendedSchema.java
@@ -1,5 +1,6 @@
 package com.wegas.editor.JSONSchema;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.wegas.editor.View.BaseView;
 
 /**
@@ -7,6 +8,9 @@ import com.wegas.editor.View.BaseView;
  */
 abstract public class JSONExtendedSchema implements JSONSchema, WithView {
     private BaseView view;
+
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private int index;
 
     @Override
     public BaseView getView() {
@@ -16,5 +20,19 @@ abstract public class JSONExtendedSchema implements JSONSchema, WithView {
     @Override
     public void setView(BaseView view) {
         this.view = view;
+    }
+
+    /**
+     * @return the index
+     */
+    public int getIndex() {
+        return index;
+    }
+
+    /**
+     * @param index the index to set
+     */
+    public void setIndex(int index) {
+        this.index = index;
     }
 }
