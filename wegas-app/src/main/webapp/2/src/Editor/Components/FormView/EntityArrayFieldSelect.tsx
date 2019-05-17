87a0ca831887551655450efc73be5774ce15b2ba
Cyril Junod <cyril.junod@gmail.com>
2019-05-17 15:00:33 +0200

    Type EntityArrayFieldSelect

--- wegas-app/src/main/webapp/2/src/Editor/Components/FormView/EntityArrayFieldSelect.tsx
+++ wegas-app/src/main/webapp/2/src/Editor/Components/FormView/EntityArrayFieldSelect.tsx
@@ -1,69 +1,70 @@
 import * as React from 'react';
 import Select from './Select';
-import {WidgetProps} from 'jsoninput/typings/types';
-import {LabeledView} from './labeled';
-import {CommonView} from './commonView';
-import * as VariableDescriptor from '../../../data/selectors/VariableDescriptor'
-import {getInstance} from '../../../data/methods/VariableDescriptor';
+import { WidgetProps } from 'jsoninput/typings/types';
+import { LabeledView } from './labeled';
+import { CommonView } from './commonView';
+import * as VariableDescriptor from '../../../data/selectors/VariableDescriptor';
+import { getInstance } from '../../../data/methods/VariableDescriptor';
 
 interface IName {
-    values: string[],
-    separator: string
+  values: string[];
+  separator: string;
 }
 
 interface IEntityArrayFieldSelectProps extends WidgetProps.BaseProps {
-    view: {
-        returnAttr: string,
-        scope: 'instance' | string
-        field: string,
-        entity: string,
-        name: IName
-    } & CommonView & LabeledView;
+  view: {
+    returnAttr: string;
+    scope: 'instance' | string;
+    field: string;
+    entity: string;
+    name: IName;
+  } & CommonView &
+    LabeledView;
 }
-
-
 
 function optionNameToString(result: any, name: IName) {
-    const separator = name ? name.separator || ',' : ',';
-    if (!name || !name.values || name.values.length <= 0) {
-        return 'undefined';
-    }
-    return name.values.map(v => result[v]).join(separator);
+  const separator = name ? name.separator || ',' : ',';
+  if (!name || !name.values || name.values.length <= 0) {
+    return 'undefined';
+  }
+  return name.values.map(v => result[v]).join(separator);
 }
 
+function EntityArrayFieldSelect(props: IEntityArrayFieldSelectProps) {
+  const { field, returnAttr, scope, entity, name, ...restView } = props.view;
 
-function EntityArrayFieldSelect(
-    props: IEntityArrayFieldSelectProps
-) {
-    const {field, returnAttr, scope, entity, name, ...restView} = props.view;
+  const computedEntity = VariableDescriptor.first('name', entity);
+  if (!computedEntity) {
+    return null;
+  }
 
-    const computedEntity = VariableDescriptor.first("name", entity);
-    if (!computedEntity) {
-        return null;
-    }
+  const results: unknown =
+    scope !== 'instance'
+      ? (computedEntity as Record<string, unknown>)[field]
+      : ((getInstance(computedEntity) as Record<string, unknown>)[
+          field
+        ] as unknown);
 
-    const results: unknown = scope !== 'instance' ?
-        computedEntity[field] as unknown  :
-        getInstance(computedEntity)[field] as unknown;
+  if (results == null) {
+    return null;
+  }
 
-    if (results == null) {
-        return null;
-    }
-
-    let aResults: Object[];
-    if (typeof results === 'object' && results !== null) {
-        aResults = Object.values(results);
-    } else if (Array.isArray(results)) {
-        aResults = results;
-    } else {
-        return null;
-    }
-
-    const choices = aResults.map(r => ({
-        value: r[returnAttr || 'name'],
-        label: optionNameToString(r, name)
-    }));
-    return <Select {...props} view={{...restView, choices}} />;
+  let aResults: unknown;
+  if (typeof results === 'object' && results !== null) {
+    aResults = Object.values(results);
+  } else if (Array.isArray(results)) {
+    aResults = results;
+  } else {
+    return null;
+  }
+  if (!Array.isArray(aResults)) {
+    return null;
+  }
+  const choices = aResults.map(r => ({
+    value: r[returnAttr || 'name'],
+    label: optionNameToString(r, name),
+  }));
+  return <Select {...props} view={{ ...restView, choices }} />;
 }
 
 export default EntityArrayFieldSelect;
