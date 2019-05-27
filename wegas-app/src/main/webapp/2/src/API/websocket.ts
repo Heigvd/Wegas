5fa2d5ff891340d58e2e32bcd18df8edae946af1
IamFonky <pierre-benjamin.monaco@heig-vd.ch>
2019-05-27 16:25:20 +0200

    Update wegas-app/src/main/webapp/2/src/API/websocket.ts
    Co-Authored-By: Cyril Junod <cyril.junod@gmail.com>

--- wegas-app/src/main/webapp/2/src/API/websocket.ts
+++ wegas-app/src/main/webapp/2/src/API/websocket.ts
@@ -170,7 +170,7 @@ class WebSocketListener {
   public bindCallback(
     eventId: WebSocketEvent,
     callback: (
-      data: any, //eslint-disable-line @typescript-eslint/no-explicit-any
+      data: unknown,
     ) => void,
   ) {
     if (this.events[eventId]) {
