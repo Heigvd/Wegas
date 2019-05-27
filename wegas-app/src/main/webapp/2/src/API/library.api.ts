2fd98ab887779b078a9103db4a6cff7697c11f30
P-B Monaco <imfonky@gmail.com>
2019-05-27 15:54:10 +0200

    Modifying LibraryAPI to match FileAPI

--- wegas-app/src/main/webapp/2/src/API/library.api.ts
+++ wegas-app/src/main/webapp/2/src/API/library.api.ts
@@ -4,67 +4,68 @@ import { GameModel } from '../data/selectors';
 export type LibType = 'CSS' | 'ClientScript' | 'ServerScript';
 export type NewLibErrors = 'NOTNEW' | 'UNKNOWN';
 
-const LIBRARY_BASE = (libType: LibType) =>
-  `GameModel/${GameModel.selectCurrent().id!}/Library/${libType}`;
+const LIBRARY_BASE = (libType: LibType, gameModelId?: number) =>
+  `GameModel/${
+    gameModelId ? gameModelId : GameModel.selectCurrent().id!
+  }/Library/${libType}`;
 
-export const LibraryApi = {
-  /**
-   * get all libraries
-   * @param gameModelId gameModels'id
-   * @param libType library types (CSS, ClientScript, ServerScript)
-   */
-  getAllLibraries(libType: LibType): Promise<ILibraries> {
-    return rest(LIBRARY_BASE(libType)).then((res: Response) => {
-      return res.json();
-    });
-  },
-  getLibrary(libType: LibType, name: string): Promise<ILibrary> {
-    return rest(LIBRARY_BASE(libType) + '/' + name).then(
-      async (res: Response) => {
+export const LibraryApiFactory = (gameModelId?: number) => {
+  return {
+    getAllLibraries(libType: LibType): Promise<ILibraries> {
+      return rest(LIBRARY_BASE(libType, gameModelId)).then((res: Response) => {
         return res.json();
-      },
-    );
-  },
-  addLibrary(
-    libType: LibType,
-    name: string,
-    library?: ILibrary,
-  ): Promise<ILibrary> {
-    return rest(LIBRARY_BASE(libType) + '/' + name, {
-      method: 'POST',
-      body: JSON.stringify({
-        '@class': 'GameModelContent',
-        ...library,
-      }),
-    })
-      .then((res: Response) => {
-        return res.json();
-      })
-      .catch((e: Error) => {
-        if (e.message === 'Bad Request') {
-          throw 'NOTNEW';
-        } else {
-          throw 'UNKNOWN';
-        }
       });
-  },
-  saveLibrary(libType: LibType, name: string, library: ILibrary) {
-    return rest(
-      LIBRARY_BASE(libType) + '/' + name,
-      {
-        method: 'PUT',
+    },
+    getLibrary(libType: LibType, name: string): Promise<ILibrary> {
+      return rest(LIBRARY_BASE(libType, gameModelId) + '/' + name).then(
+        async (res: Response) => {
+          return res.json();
+        },
+      );
+    },
+    addLibrary(
+      libType: LibType,
+      name: string,
+      library?: ILibrary,
+    ): Promise<ILibrary> {
+      return rest(LIBRARY_BASE(libType, gameModelId) + '/' + name, {
+        method: 'POST',
         body: JSON.stringify({
           '@class': 'GameModelContent',
           ...library,
         }),
-      },
-      'Editor',
-      'application/json',
-    );
-  },
-  deleteLibrary(libType: LibType, name: string) {
-    return rest(LIBRARY_BASE(libType) + '/' + name, {
-      method: 'DELETE',
-    });
-  },
+      })
+        .then((res: Response) => {
+          return res.json();
+        })
+        .catch((e: Error) => {
+          if (e.message === 'Bad Request') {
+            throw Error('NOTNEW');
+          } else {
+            throw Error('UNKNOWN');
+          }
+        });
+    },
+    saveLibrary(libType: LibType, name: string, library: ILibrary) {
+      return rest(
+        LIBRARY_BASE(libType, gameModelId) + '/' + name,
+        {
+          method: 'PUT',
+          body: JSON.stringify({
+            '@class': 'GameModelContent',
+            ...library,
+          }),
+        },
+        'Editor',
+        'application/json',
+      );
+    },
+    deleteLibrary(libType: LibType, name: string) {
+      return rest(LIBRARY_BASE(libType, gameModelId) + '/' + name, {
+        method: 'DELETE',
+      });
+    },
+  };
 };
+
+export const LibraryAPI = LibraryApiFactory();
