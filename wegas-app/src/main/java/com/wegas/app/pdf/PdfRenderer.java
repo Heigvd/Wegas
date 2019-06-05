9f3a27434781b639584ce9d02dec9cb488273005
maxencelaurent <maxence.laurent@gmail.com>
2019-06-05 11:59:29 +0200

    POST utf-8 chars to PdfRenderer
    * use DejaVu as such glyphs are missing from Helvetica

--- wegas-app/src/main/java/com/wegas/app/pdf/PdfRenderer.java
+++ wegas-app/src/main/java/com/wegas/app/pdf/PdfRenderer.java
@@ -272,7 +272,7 @@ public class PdfRenderer implements Filter {
                 + "</title>"
                 + "<link rel=\"stylesheet\" type=\"text/css\" href=\"wegas-app/css/wegas-pdf-print.css\" media=\"all\" />"
                 + "<link rel=\"stylesheet\" type=\"text/css\" href=\"wegas-app/css/wegas-pdf-print-page.css\" media=\"print\" />"
-                + "</head><body class='wegas-pdf-content' style=\"font-family:Helvetica, Arial; font-size:12px\">"
+                + "</head><body class='wegas-pdf-content' style=\"font-family:DejaVu Sans, Helvetica, Arial; font-size:12px\">"
                 + body
                 + "</body></html>";
     }
