9e6ae0569f46e86b17fd866d569a7c86bf6a2880
Cyril Junod <cyril.junod@gmail.com>
2019-08-15 17:08:31 +0200

    hashmap editing: improved focus handling

--- wegas-app/src/main/webapp/wegas-react-form/src/Views/hashlist.tsx
+++ wegas-app/src/main/webapp/wegas-react-form/src/Views/hashlist.tsx
@@ -1,11 +1,11 @@
-import React, {CSSProperties} from 'react';
+import React, { CSSProperties } from 'react';
 import TextField from './string';
 import commonView from '../HOC/commonView';
 import ObjectView from './object';
 import IconButton from '../Components/IconButton';
-import {WidgetProps} from 'jsoninput/typings/types';
-import {AddStatementButton} from '../Script/Views/Button';
-import {css} from 'glamor';
+import { WidgetProps } from 'jsoninput/typings/types';
+import { AddStatementButton } from '../Script/Views/Button';
+import { css } from 'glamor';
 
 const KEY_DEFAULT_VALUE = '';
 const halfWidth: CSSProperties = {
@@ -20,113 +20,107 @@ const flex = css({
 const bottom = css({
     alignSelf: 'flex-end',
 });
-type HashListProps = WidgetProps.ObjectProps<{keyLabel?: string}> & {
+type HashListProps = WidgetProps.ObjectProps<{ keyLabel?: string }> & {
     id: string;
     view: {
         readOnly: boolean;
     };
 };
-class HashlistView extends React.Component<
-    HashListProps,
-    {newInputValue: string}
-    > {
-    child: {[key: string]: HTMLElement};
-    constructor(props: HashListProps) {
-        super(props);
-        this.state = {
-            newInputValue: '',
-        };
-        this.child = {};
-        this.addChild = this.addChild.bind(this);
+function Row({
+    children,
+    editKey,
+    keyLabel = 'Key',
+    readOnly,
+    removeKey,
+    alterKey,
+}: {
+    editKey: string;
+    children: React.ReactChild;
+    keyLabel?: string;
+    readOnly: boolean;
+    alterKey: WidgetProps.ObjectProps['alterKey'];
+    removeKey: WidgetProps.ObjectProps['removeKey'];
+}) {
+    function remove() {
+        removeKey(editKey);
     }
-    addChild() {
-        this.props.addKey(KEY_DEFAULT_VALUE, '');
 
-        setTimeout(() => {
-            const input = this.child[KEY_DEFAULT_VALUE].querySelector('input');
-            if (input !== null) {
-                input.focus();
-            }
-        }, 20);
+    function onKeyChange(value: string) {
+        try {
+            alterKey(editKey, value);
+        } catch {
+            // duplicate key
+        }
     }
-    render() {
-        const {id, removeKey, alterKey, children, ...restProps} = this.props;
-        const newChildren = React.Children.map(children, child => {
-            const c = child as React.ReactElement<{editKey: string; schema : {view: {label: string}}}>;
-            function remove() {
-                removeKey(c.props.editKey);
-            }
-
-            function onKeyChange(value: string) {
-                try {
-                    alterKey(c.props.editKey, value);
-                } catch (_) {
-                    // duplicate key
-                }
-            }
-            const readOnly = restProps.view.readOnly;
-
-            return (
-                <div {...flex} key={c.props.editKey}>
-                    <div
-                        style={{position: 'relative', flex: 1}}
-                        ref={node => {
-                            if (node !== null) {
-                                this.child[c.props.editKey] = node;
-                            }
-                        }}
-                    >
-                        <TextField
-                            id={id}
-                            value={c.props.editKey}
-                            blurOnly
-                            onChange={onKeyChange}
-                            view={{
-                                label: this.props.view.keyLabel || 'Key',
-                                style: halfWidth,
-                                readOnly: readOnly
-                            }}
-                        />
-                        <div style={halfWidth}>{child}</div>
-                    </div>
-                    {!readOnly ?
-                        <IconButton
-                            icon="fa fa-trash"
-                            tooltip="Remove property"
-                            className={String(bottom)}
-                            onClick={remove}
-                        />
-                        : null}
-                </div>
-            );
-        });
-        /*
-        <TextField
-            id={id}
-            key="newkeyinput"
-            value={this.state.newInputValue}
-            onChange={this.onAdderChange}
-            view={{
-                label: this.props.view.keyLabel || 'Key',
-                style: halfWidth
-            }}
-        />
-        */
-        return (
-            <ObjectView {...restProps as any}>
-                {newChildren}
-                <br />
-                {!this.props.view.readOnly ?
-                    <AddStatementButton
-                        key="newkeyadd"
-                        icon="fa fa-plus-circle"
-                        tooltip="Add property value"
-                        label="Property"
-                        onClick={this.addChild}
-                    />
-                    : null}
-            </ObjectView>
-        );
-    }
+    return (
+        <div {...flex}>
+            <div style={{ position: 'relative', flex: 1 }}>
+                <TextField
+                    id={editKey}
+                    value={editKey}
+                    blurOnly
+                    onChange={onKeyChange}
+                    view={{
+                        label: keyLabel,
+                        style: halfWidth,
+                        readOnly: readOnly,
+                    }}
+                />
+                <div style={halfWidth}>{children}</div>
+            </div>
+            {!readOnly ? (
+                <IconButton
+                    icon="fa fa-trash"
+                    tooltip="Remove property"
+                    className={String(bottom)}
+                    onClick={remove}
+                />
+            ) : null}
+        </div>
+    );
 }
+function HashlistView({
+    id,
+    removeKey,
+    alterKey,
+    children,
+    addKey,
+    ...restProps
+}: HashListProps) {
+    function addChild() {
+        addKey(KEY_DEFAULT_VALUE, '');
+    }
+
+    const newChildren = React.Children.map(children, child => {
+        const key = child.props.editKey;
+
+        return (
+            <Row
+                key={child.key}
+                editKey={key}
+                readOnly={restProps.view.readOnly}
+                alterKey={alterKey}
+                removeKey={removeKey}
+            >
+                {child}
+            </Row>
+        );
+    });
+    return (
+        <ObjectView {...restProps}>
+            {newChildren}
+            <br />
+            {!restProps.view.readOnly ? (
+                <AddStatementButton
+                    key="newkeyadd"
+                    icon="fa fa-plus-circle"
+                    tooltip="Add property value"
+                    label="Property"
+                    onClick={addChild}
+                />
+            ) : null}
+        </ObjectView>
+    );
+}
+
 export default commonView(HashlistView);
