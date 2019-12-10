8568ab5b232750f2cfbf1532a2397b3c52c06390
maxencelaurent <maxence.laurent@gmail.com>
2019-12-10 13:36:07 +0100

    Remove useless burndown description

--- wegas-core/src/main/java/com/wegas/resourceManagement/persistence/BurndownDescriptor.java
+++ wegas-core/src/main/java/com/wegas/resourceManagement/persistence/BurndownDescriptor.java
@@ -7,17 +7,8 @@
  */
 package com.wegas.resourceManagement.persistence;
 
-import ch.albasim.wegas.annotations.View;
-import ch.albasim.wegas.annotations.WegasEntityProperty;
-import com.fasterxml.jackson.annotation.JsonView;
 import com.wegas.core.persistence.variable.VariableDescriptor;
-import com.wegas.core.rest.util.Views;
-import com.wegas.editor.ValueGenerators.EmptyString;
-import com.wegas.editor.View.HtmlView;
-import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Lob;
 
 /**
  *
@@ -27,28 +18,4 @@ import javax.persistence.Lob;
 public class BurndownDescriptor extends VariableDescriptor<BurndownInstance> {
 
     private static final long serialVersionUID = 1L;
-    /**
-     *
-     */
-    @Lob
-    @Basic(fetch = FetchType.EAGER) // CARE, lazy fetch on Basics has some trouble.
-    @JsonView(Views.ExtendedI.class)
-    @WegasEntityProperty(searchable = true,
-            optional = false, nullable= false, proposal = EmptyString.class,
-            view = @View(label = "description", value = HtmlView.class))
-    private String description;
-
-    /**
-     * @return the description
-     */
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * @param description the description to set
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
