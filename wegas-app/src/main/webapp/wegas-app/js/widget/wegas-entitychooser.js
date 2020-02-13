9378ea132663bbb79216e1b24ddc5076f37b9475
maxencelaurent <maxence.laurent@gmail.com>
2020-02-13 14:14:40 +0100

    [EntityChooser] fix getItems

--- wegas-app/src/main/webapp/wegas-app/js/widget/wegas-entitychooser.js
+++ wegas-app/src/main/webapp/wegas-app/js/widget/wegas-entitychooser.js
@@ -19,166 +19,8 @@ YUI.add("wegas-entitychooser", function(Y) {
             CHOOSEN: "chooser-choosen"
         };
 
-    EntityChooser = Y.Base.create("wegas-entitychooser",
-        Y.Widget,
-        [Y.WidgetChild,
-            Y.Wegas.Widget,
-            Y.Wegas.Editable],
-        {
-            CONTENT_TEMPLATE: "<div><ul class='chooser-entities'></ul><div class='chooser-widget'></div></div>",
-            initializer: function() {
-                this._handlers = [];
-                /**
-                 * hold a ref to the currently selected name
-                 */
-                this.currentTarget = null;
-            },
-            hideAllOverlay: function() {
-                Y.Wegas.Widget.prototype.hideAllOverlay.call(this);
-                this._currentWidget && this._currentWidget.hideAllOverlay();
-            },
-            syncUI: function() {
-                var items = (this.get("variable.evaluated") ?
-                    (this.get("flatten") ?
-                        this.get("variable.evaluated").flatten() :
-                        this.get("variable.evaluated").get("items")) :
-                    []),
-                    i, tmp,
-                    entityBox = this.get(CONTENTBOX).one(".chooser-entities"),
-                    length = items.length,
-                    filter = [];
-
-                entityBox.empty();
-                if (this.get("classFilter")) {
-                    if (!Y.Lang.isArray(this.get("classFilter"))) {
-                        filter.push(this.get("classFilter"));
-                    } else {
-                        filter = filter.concat(this.get("classFilter"));
-                    }
-                }
-
-                for (i = 0; i < length; i += 1) {
-                    if (filter.length === 0 || Y.Array.find(filter, function(item) {
-                        return item === items[i].get("@class");
-                    })) {
-                        if ((!items[i].getInstance().getAttrs().hasOwnProperty("active") ||
-                            items[i].getInstance().get("active")) &&
-                            (!items[i].getInstance().getAttrs().hasOwnProperty("enabled") ||
-                                items[i].getInstance().get("enabled"))) {
-                            entityBox.append("<li class='chooser-entity' data-name='" + items[i].get("name") + "'>" +
-                                (I18n.t(items[i].get("label"))) + "</li>");
-                        }
-                    }
-                }
-                if (this.currentTarget) {
-                    tmp = entityBox.all("[data-name='" + this.currentTarget + "']");
-                    tmp.addClass(CLASSES.CHOOSEN);
-                    if (!tmp.size()) { //current target exists but is not rendered anymore
-                        Y.later(200, this, function() {
-                            this.widget.destroy();
-                            this.widget = null;
-                        });
-                        this.currentTarget = null;
-                    }
-                }
-            },
-            bindUI: function() {
-                this.get(CONTENTBOX).delegate("click", function(e) {
-                    var targetName = e.target.getData("name");
-                    if (this.currentTarget === targetName) { // I'm the choosen one
-                        return;
-                    }
-                    this.genWidget(targetName);
-                    this.get(CONTENTBOX).all("." + CLASSES.CHOOSEN).removeClass(CLASSES.CHOOSEN);
-                    e.target.addClass(CLASSES.CHOOSEN);
-                    this.currentTarget = targetName;
-                }, ".chooser-entities .chooser-entity", this);
-                this._handlers.push(Y.Wegas.Facade.Variable.after("update", this.syncUI, this));
-            },
-            genWidget: function(name) {
-                var cfg = this.get("widget"),
-                    ctx = this;
-                Y.Wegas.Editable.use(cfg, function(Y) {
-                    if (ctx.widget) {
-                        ctx.widget.set(ctx.get("widgetAttr"), {
-                            name: name
-                        });
-                    } else {
-                        cfg[ctx.get("widgetAttr")] = {
-                            name: name
-                        };
-                        Y.Wegas.use(cfg, Y.bind(function() {
-                            this.widget = Y.Wegas.Widget.create(cfg);
-                            this.widget.render(this.get(CONTENTBOX).one(".chooser-widget"));
-                            this.widget.on(["*:message", "*:showOverlay", "*:hideOverlay"], this.fire, this); // Event on the loaded
-                        }, ctx));
-                    }
-                });
-            },
-            destructor: function() {
-                this.widget && this.widget.destroy();
-                Y.Array.each(this._handlers, function(handle) {
-                    handle.detach();
-                });
-            }
-
-        },
-        {
-            ATTRS: {
-                variable: {
-                    type: 'object',
-                    getter: Y.Wegas.Widget.VARIABLEDESCRIPTORGETTER,
-                    view: {
-                        type: "variableselect",
-                        label: "Folder",
-                        classFilter: ["ListDescriptor"]
-                    }
-                },
-                widget: {
-                    type: "object",
-                    value: {
-                        type: "HistoryDialog"
-                    },
-                    properties: {
-                        type: {
-                            type: "string",
-                            view: {label: "Type"}
-                        }
-                    },
-                    getter: function(v) {
-                        return Y.JSON.parse(Y.JSON.stringify(v));
-                    }
-                },
-                widgetAttr: {
-                    value: "dialogueVariable",
-                    type: "string",
-                    view: {label: "Widget Attribute"}
-                },
-                flatten: {
-                    type: "boolean",
-                    value: "true",
-                    view: {label: "Flatten"}
-                },
-                classFilter: {
-                    type: "array",
-                    value: [],
-                    required: true,
-                    view: {label: "ClassFilter"},
-                    items: {
-                        type: "string",
-                        view: {
-                            type: "select",
-                            choices: Y.Wegas.persistence.AVAILABLE_TYPES
-                        }
-                    }
-                }
-            }
-        });
-    Y.Wegas.EntityChooser = EntityChooser;
-
-    EntityChooser2 = Y.Base.create("wegas-entitychooser2",
-        Y.Widget,
-        [Y.WidgetParent, Y.WidgetChild, Y.Wegas.Widget, Y.Wegas.Editable], {
+    EntityChooser = Y.Base.create("wegas-entitychooser", Y.Widget,
+        [Y.WidgetChild, Y.Wegas.Widget, Y.Wegas.Editable], {
         CONTENT_TEMPLATE: "<div><ul class='chooser-entities'></ul><div class='chooser-widget'></div></div>",
         initializer: function() {
             this._handlers = [];
@@ -192,8 +34,171 @@ YUI.add("wegas-entitychooser", function(Y) {
             this._currentWidget && this._currentWidget.hideAllOverlay();
         },
         syncUI: function() {
-            var items = (this.get("variable.evaluated") ? (this.get("flatten") ? this.get("variable.evaluated")
-                .flatten() : this.get("variable.evaluated").get("items")) : []),
+            var items = (this.get("variable.evaluated") ?
+                (this.get("flatten") ?
+                    this.get("variable.evaluated").flatten() :
+                    this.get("variable.evaluated").get("items")) :
+                []),
+                i, tmp,
+                entityBox = this.get(CONTENTBOX).one(".chooser-entities"),
+                length = items.length,
+                filter = [];
+
+            entityBox.empty();
+            if (this.get("classFilter")) {
+                if (!Y.Lang.isArray(this.get("classFilter"))) {
+                    filter.push(this.get("classFilter"));
+                } else {
+                    filter = filter.concat(this.get("classFilter"));
+                }
+            }
+
+            for (i = 0; i < length; i += 1) {
+                if (filter.length === 0 || Y.Array.find(filter, function(item) {
+                    return item === items[i].get("@class");
+                })) {
+                    if ((!items[i].getInstance().getAttrs().hasOwnProperty("active") ||
+                        items[i].getInstance().get("active")) &&
+                        (!items[i].getInstance().getAttrs().hasOwnProperty("enabled") ||
+                            items[i].getInstance().get("enabled"))) {
+                        entityBox.append("<li class='chooser-entity' data-name='" + items[i].get("name") + "'>" +
+                            (I18n.t(items[i].get("label"))) + "</li>");
+                    }
+                }
+            }
+            if (this.currentTarget) {
+                tmp = entityBox.all("[data-name='" + this.currentTarget + "']");
+                tmp.addClass(CLASSES.CHOOSEN);
+                if (!tmp.size()) { //current target exists but is not rendered anymore
+                    Y.later(200, this, function() {
+                        this.widget.destroy();
+                        this.widget = null;
+                    });
+                    this.currentTarget = null;
+                }
+            }
+        },
+        bindUI: function() {
+            this.get(CONTENTBOX).delegate("click", function(e) {
+                var targetName = e.target.getData("name");
+                if (this.currentTarget === targetName) { // I'm the choosen one
+                    return;
+                }
+                this.genWidget(targetName);
+                this.get(CONTENTBOX).all("." + CLASSES.CHOOSEN).removeClass(CLASSES.CHOOSEN);
+                e.target.addClass(CLASSES.CHOOSEN);
+                this.currentTarget = targetName;
+            }, ".chooser-entities .chooser-entity", this);
+            this._handlers.push(Y.Wegas.Facade.Variable.after("update", this.syncUI, this));
+        },
+        genWidget: function(name) {
+            var cfg = this.get("widget"),
+                ctx = this;
+            Y.Wegas.Editable.use(cfg, function(Y) {
+                if (ctx.widget) {
+                    ctx.widget.set(ctx.get("widgetAttr"), {
+                        name: name
+                    });
+                } else {
+                    cfg[ctx.get("widgetAttr")] = {
+                        name: name
+                    };
+                    Y.Wegas.use(cfg, Y.bind(function() {
+                        this.widget = Y.Wegas.Widget.create(cfg);
+                        this.widget.render(this.get(CONTENTBOX).one(".chooser-widget"));
+                        this.widget.on(["*:message", "*:showOverlay", "*:hideOverlay"], this.fire, this); // Event on the loaded
+                    }, ctx));
+                }
+            });
+        },
+        destructor: function() {
+            this.widget && this.widget.destroy();
+            Y.Array.each(this._handlers, function(handle) {
+                handle.detach();
+            });
+        }
+
+    }, {
+        ATTRS: {
+            variable: {
+                type: 'object',
+                getter: Y.Wegas.Widget.VARIABLEDESCRIPTORGETTER,
+                view: {
+                    type: "variableselect",
+                    label: "Folder",
+                    classFilter: ["ListDescriptor"]
+                }
+            },
+            widget: {
+                type: "object",
+                value: {
+                    type: "HistoryDialog"
+                },
+                properties: {
+                    type: {
+                        type: "string",
+                        view: {label: "Type"}
+                    }
+                },
+                getter: function(v) {
+                    return Y.JSON.parse(Y.JSON.stringify(v));
+                }
+            },
+            widgetAttr: {
+                value: "dialogueVariable",
+                type: "string",
+                view: {label: "Widget Attribute"}
+            },
+            flatten: {
+                type: "boolean",
+                value: "true",
+                view: {label: "Flatten"}
+            },
+            classFilter: {
+                type: "array",
+                value: [],
+                required: true,
+                view: {label: "ClassFilter"},
+                items: {
+                    type: "string",
+                    view: {
+                        type: "select",
+                        choices: Y.Wegas.persistence.AVAILABLE_TYPES
+                    }
+                }
+            }
+        }
+    });
+    Y.Wegas.EntityChooser = EntityChooser;
+
+    EntityChooser2 = Y.Base.create("wegas-entitychooser2", Y.Widget,
+        [Y.WidgetParent, Y.WidgetChild, Y.Wegas.Widget, Y.Wegas.Editable], {
+        CONTENT_TEMPLATE: "<div><ul class='chooser-entities'></ul><div class='chooser-widget'></div></div>",
+        initializer: function() {
+            this._handlers = [];
+            /**
+             * hold a ref to the currently selected name
+             */
+            this.currentTarget = null;
+        },
+        hideAllOverlay: function() {
+            Y.Wegas.Widget.prototype.hideAllOverlay.call(this);
+            this._currentWidget && this._currentWidget.hideAllOverlay();
+        },
+        getItems: function() {
+            var theVar = this.get("variable.evaluated");
+            if (theVar) {
+                if (!Y.Lang.isArray(theVar)) {
+                    return this.get("flatten") ? theVar.flatten() : theVar.get("items");
+                } else {
+                    return theVar;
+                }
+            } else {
+                return [];
+            }
+        },
+        syncUI: function() {
+            var items = this.getItems(),
                 i, tmp, li,
                 entityBox = this.get(CONTENTBOX).one(".chooser-entities"),
                 length = items.length, label, getLabel,
@@ -282,7 +287,7 @@ YUI.add("wegas-entitychooser", function(Y) {
             this._handlers.push(Y.Wegas.Facade.Variable.after("update", this.syncUI, this));
         },
         genWidget: function(type, name) {
-            var widgetConfig = this.get("widgets")[type],
+            var widgetConfig = JSON.parse(JSON.stringify(this.get("widgets")[type])),
                 ctx = this, cfg;
             cfg = widgetConfig.widget;
 
