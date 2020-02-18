089da847bdad2e0c72ce28072266c1e683bb00b0
P-B Monaco <imfonky@gmail.com>
2020-02-18 14:57:35 +0100

    Node modules upgrade corrections and eslint rule removing

--- wegas-app/src/main/webapp/wegas-react-form/src/Script/Views/asyncJSEditor.tsx
+++ wegas-app/src/main/webapp/wegas-react-form/src/Script/Views/asyncJSEditor.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
+import { IAceEditorProps } from 'react-ace';
 import { SimpleLoader } from '../../Components/Loader';
-import { AceEditorProps } from 'react-ace';
-const AsyncJSEditor = React.lazy(() =>
-    import(/* webpackChunkName: "ace-js" */ './JSEditor').then(file => ({
-        default: file.JSEditor,
-    }))
-);
-export default function JSEditor(props: AceEditorProps) {
+
+const AsyncJSEditor = React.lazy(() => import(
+    /* webpackChunkName: "ace-js" */ './JSEditor').then(file => ({
+    default: file.JSEditor,
+})));
+
+export default function JSEditor(props: IAceEditorProps) {
     return (
         <React.Suspense fallback={<SimpleLoader />}>
             <AsyncJSEditor {...props} />
