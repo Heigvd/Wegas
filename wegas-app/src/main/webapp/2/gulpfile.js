7e089d8956f927f27b73c99fc6bb6b815048a995
P-B Monaco <imfonky@gmail.com>
2020-02-19 18:15:09 +0100

    Removing eslint error in gulpfiles

--- wegas-app/src/main/webapp/2/gulpfile.js
+++ wegas-app/src/main/webapp/2/gulpfile.js
@@ -1,13 +1,9 @@
-// eslint-disable-next-line
+/* global require */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const gulp = require('gulp');
 const webpack = require('webpack');
-
 const config = require('./webpack.config');
 
-// gulp.task('default', gulp.series('build', ()=>{
-//     // eslint-disable-next-line
-//     console.log("Wegas 2 built");
-// }));
 gulp.task('default', callback => {
     webpack({ ...config, mode: 'production' }, (err, stats) => {
         if (err) return callback(err);
