3cfdb8bf90c4ee150b375cc582c75a57914daae7
maxencelaurent <maxence.laurent@gmail.com>
2020-04-29 17:45:11 +0200

    [Core] Remove everything related to facebook login
    * it's not working and it's not planned to provide such a way to authenticate

--- wegas-core/src/main/java/com/wegas/core/security/facebook/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Wegas
- * http://wegas.albasim.ch
- *
- * Copyright (c) 2013-2018 School of Business and Engineering Vaud, Comem, MEI
- * Licensed under the MIT License
- */
-package com.wegas.core.security.facebook;
