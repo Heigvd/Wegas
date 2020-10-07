c39c82e84b5fd11b5b56cfcb043f98c2287d0945
P-B Monaco <imfonky@gmail.com>
2020-10-07 15:48:19 +0200

    Input type string component

---
+++ wegas-app/src/main/webapp/2/src/Components/PageComponents/Inputs/String.component.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import {
+  registerComponent,
+  pageComponentFactory,
+} from '../tools/componentFactory';
+import { schemaProps } from '../tools/schemaProps';
+import { store } from '../../../data/store';
+import { Actions } from '../../../data';
+import { useComponentScript } from '../../Hooks/useComponentScript';
+import { WegasComponentProps } from '../tools/EditableComponent';
+import { IScript, IStringDescriptor } from 'wegas-ts-api';
+import { createFindVariableScript } from '../../../Helper/wegasEntites';
+import { SimpleInput } from '../../Inputs/SimpleInput';
+import { useScript } from '../../Hooks/useScript';
+import { useTranslate } from '../../../Editor/Components/FormView/translatable';
+
+interface PlayerStringInput extends WegasComponentProps {
+  /**
+   * script - the script that returns the variable to display and modify
+   */
+  script?: IScript;
+  /**
+   * placeholder - the grey text inside the box when nothing is written
+   */
+  placeholder?: IScript;
+}
+
+function PlayerStringInput(props: PlayerStringInput) {
+  const placeholder = useScript<string>(props.placeholder);
+  const { content, instance, notFound } = useComponentScript<IStringDescriptor>(
+    props.script,
+  );
+
+  const disabled = useScript<boolean>(props.disableIf);
+  const readOnly = useScript<boolean>(props.readOnlyIf);
+  const value = useTranslate(instance?.trValue);
+
+  return notFound ? (
+    <pre>Not found: {content}</pre>
+  ) : (
+    <SimpleInput
+      value={value}
+      onChange={v => {
+        store.dispatch(
+          Actions.VariableInstanceActions.runScript(
+            `${content}.setValue(self, ${v});`,
+          ),
+        );
+      }}
+      disabled={disabled}
+      readOnly={readOnly}
+      placeholder={placeholder}
+      className={props.className}
+      style={props.style}
+    />
+  );
+}
+
+registerComponent(
+  pageComponentFactory({
+    component: PlayerStringInput,
+    componentType: 'Input',
+    name: 'String',
+    icon: 'paragraph',
+    schema: {
+      script: schemaProps.scriptVariable('Variable', true, [
+        'SStringDescriptor',
+      ]),
+      placeholder: schemaProps.scriptString('Placeholder', false),
+    },
+    allowedVariables: ['StringDescriptor'],
+    getComputedPropsFromVariable: v => ({
+      script: createFindVariableScript(v),
+    }),
+  }),
+);
