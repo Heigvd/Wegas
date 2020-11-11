9706ddaa54592874f5f53a5dd28b4717da3d6fbb
Maxence Laurent <maxence.laurent@gmail.com>
2020-11-11 09:58:23 +0100

    Fix events drop down list

--- wegas-app/src/main/webapp/wegas-react-form/src/Views/Interacting/eventselect.js
+++ wegas-app/src/main/webapp/wegas-react-form/src/Views/Interacting/eventselect.js
@@ -3,23 +3,22 @@ import React from 'react';
 import Select from '../select';
 import { getY } from '../../index';
 
-const choices = currentValue => () =>
-    new Promise(resolve => {
-        const gmId = getY().Wegas.Facade.GameModel.cache.get('currentGameModelId');
-        getY().Wegas.Facade.GameModel.cache.sendRequest({
-            request: `/${gmId}/FindAllFiredEvents`,
-            on: {
-                success: e => {
-                    const events = e.response.entities;
-                    if (events.indexOf(currentValue) < 0) {
-                        resolve(events.concat(currentValue).sort());
-                    } else {
-                        resolve(events.sort());
-                    }
+const choices = currentValue => () => new Promise(resolve => {
+    const gmId = getY().Wegas.Facade.GameModel.cache.get('currentGameModelId');
+    getY().Wegas.Facade.GameModel.cache.sendRequest({
+        request: `/${gmId}/FindAllFiredEvents`,
+        on: {
+            success: e => {
+                const events = e.response.entities;
+                if (currentValue && events.indexOf(currentValue) < 0) {
+                    resolve(events.concat(currentValue).sort());
+                } else {
+                    resolve(events.sort());
                 }
             }
-        });
+        }
     });
+});
 
 function EventSelect(props) {
     return <Select {...props} view={{ ...props.view, choices: choices(props.value) }} />;
