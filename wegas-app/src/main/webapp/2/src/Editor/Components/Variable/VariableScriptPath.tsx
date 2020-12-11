fa6dc5c47428bd8e70ec8fff4b837d731146ea5b
P-B Monaco <imfonky@gmail.com>
2020-12-11 08:17:59 +0100

    Path on variable select and debugging wyswyg script editor

---
+++ wegas-app/src/main/webapp/2/src/Editor/Components/Variable/VariableScriptPath.tsx
@@ -0,0 +1,37 @@
+import { css } from 'emotion';
+import * as React from 'react';
+import { safeClientScriptEval } from '../../../Components/Hooks/useScript';
+import { editorLabel } from '../../../data/methods/VariableDescriptorMethods';
+import { VariableDescriptor } from '../../../data/selectors';
+
+function getVariablePath(variable: IVariableDescriptor): string {
+  let path = '';
+  const parentVariable = VariableDescriptor.select(variable.parentId);
+  if (parentVariable != null) {
+    path += getVariablePath(parentVariable);
+  }
+  return `${path}\\${editorLabel(variable, true)}`;
+}
+
+const variableScriptPathStyle = css({
+  fontSize: '0.8em',
+  lineHeight: '0.8em',
+  padding: '5px',
+  color: 'black',
+  borderRadius: '2px',
+  boxShadow: `0 0 5px grey`,
+});
+
+interface VariableScriptPathProps {
+  script: string;
+}
+
+export function VariableScriptPath({ script }: VariableScriptPathProps) {
+  return (
+    <div className={variableScriptPathStyle}>
+      {getVariablePath(
+        safeClientScriptEval<SVariableDescriptor>(script).getEntity(),
+      )}
+    </div>
+  );
+}
