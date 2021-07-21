04f26762c6f68fa41f72098a63cec1dfa6cf5991
Audrey Huguenin <audreyhugueninbenjamin@gmail.com>
2021-07-21 11:18:25 +0200

    forgotten error fixed

--- wegas-app/src/main/webapp/2/src/Editor/Components/Header.tsx
+++ wegas-app/src/main/webapp/2/src/Editor/Components/Header.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { GameModel, Global } from '../../data/selectors';
-import { css, cx, keyframes } from 'emotion';
+import { css, cx } from 'emotion';
 import { useStore, store } from '../../data/Stores/store';
 import { Actions } from '../../data';
 import { FontAwesome, IconComp } from './Views/FontAwesome';
