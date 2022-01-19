bb664d61c8e8931b0ca3fb7adfd49acf6a4edc89
Maxence Laurent <maxence.laurent@gmail.com>
2022-01-19 18:31:16 +0100

    [React-Editor] Improve Client Console
    * add autorun toggler and fix overflow
    Signed-off-by:Maxence Laurent <maxence.laurent@gmail.com>

--- wegas-app/src/main/node/wegas-react/src/Editor/Components/PlayLocal.tsx
+++ wegas-app/src/main/node/wegas-react/src/Editor/Components/PlayLocal.tsx
@@ -2,13 +2,17 @@ import { css, cx } from '@emotion/css';
 import * as React from 'react';
 import { useDebounce } from '../../Components/Hooks/useDebounce';
 import { useUnsafeScript } from '../../Components/Hooks/useScript';
+import { Toggler } from '../../Components/Inputs/Boolean/Toggler';
+import { Button } from '../../Components/Inputs/Buttons/Button';
 import { themeVar } from '../../Components/Theme/ThemeVars';
-import { defaultPadding } from '../../css/classes';
+import { Toolbar } from '../../Components/Toolbar';
+import { defaultPadding, flex } from '../../css/classes';
 import { shallowIs } from '../../Helper/shallowIs';
 import { WegasScriptEditor } from './ScriptEditors/WegasScriptEditor';
 
 const container = css({ width: '100%' });
 const editor = css({ width: '100%', height: '400px' });
+const togglerStyle = css({ padding: '0 15px 0 15px' });
 
 class ErrorBoundary extends React.Component<Record<string, unknown>> {
   readonly state: { error?: Error } = { error: undefined };
@@ -37,23 +41,60 @@ class ErrorBoundary extends React.Component<Record<string, unknown>> {
   }
 }
 
+const overlayStyle = css({ overflow: 'auto' });
+
 const Eval = React.memo(function Eval({ script }: { script: string }) {
   const val = useUnsafeScript(script);
-  return <pre>{JSON.stringify(val, null, 2)}</pre>;
+  return <pre className={overlayStyle}>{JSON.stringify(val, null, 2)}</pre>;
 });
 Eval.displayName = 'Eval';
 
 export default function PlayLocal() {
   const [script, setScript] = React.useState('');
+
+  const [tmpScript, setTmpScript] = React.useState('');
+
   const debouncedScript = useDebounce(script, 300);
+
+  const [autorun, setAutorun] = React.useState(true);
+
+  const onChangeCb = React.useCallback(
+    (newScript: string) => {
+      setTmpScript(newScript);
+      if (autorun) {
+        setScript(newScript);
+      }
+    },
+    [autorun],
+  );
+
+  const onRunCb = React.useCallback(() => {
+    setScript(tmpScript);
+  }, [tmpScript]);
+
   return (
-    <div className={container}>
-      <div className={editor}>
-        <WegasScriptEditor value={script} onChange={e => setScript(e)} />
-      </div>
-      <ErrorBoundary script={debouncedScript}>
-        <Eval script={debouncedScript} />
-      </ErrorBoundary>
-    </div>
+    <Toolbar>
+      <Toolbar.Header className={defaultPadding}>
+        <div className={flex}>
+          <Toggler
+            className={togglerStyle}
+            value={autorun}
+            onChange={setAutorun}
+            label="autorun"
+          />
+          {autorun ? null : <Button onClick={onRunCb} label="Run script" />}
+        </div>
+      </Toolbar.Header>
+      <Toolbar.Content>
+        <div className={container}>
+          <div className={editor}>
+            <WegasScriptEditor value={script} onChange={onChangeCb} />
+          </div>
+          <ErrorBoundary script={debouncedScript}>
+            <Eval script={debouncedScript} />
+          </ErrorBoundary>
+        </div>
+      </Toolbar.Content>
+    </Toolbar>
   );
 }
