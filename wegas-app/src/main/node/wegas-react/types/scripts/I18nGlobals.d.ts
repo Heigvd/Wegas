e759149ac017b7a10668dbe58dce784cc8b1737b
Maxence Laurent <maxence.laurent@gmail.com>
2022-03-09 11:36:52 +0100

    [React] fix I18nGlobals.d.ts
    * translate accepts ITranslatableContent too
    Signed-off-by:Maxence Laurent <maxence.laurent@gmail.com>

--- wegas-app/src/main/node/wegas-react/types/scripts/I18nGlobals.d.ts
+++ wegas-app/src/main/node/wegas-react/types/scripts/I18nGlobals.d.ts
@@ -3,7 +3,7 @@ type SVariableDescriptor = import('wegas-ts-api').SVariableDescriptor;
 type ITranslation = import('wegas-ts-api').ITranslation;
 
 interface GlobalI18nClass {
-  translate: (translatable: STranslatableContent) => string;
+  translate: (translatable: STranslatableContent | ITranslatableContent) => string;
   toString: (entity: SVariableDescriptor) => string;
   createTranslatableContent: (string: string) => ITranslatableContent;
   createTranslation: (string: string) => ITranslation;
