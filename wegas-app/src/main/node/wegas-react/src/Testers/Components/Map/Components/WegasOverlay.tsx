263dc9e02e78799c9e82b93b1088a956681e2763
P-B Monaco <imfonky@gmail.com>
2022-04-21 15:46:40 +0200

    Small improvements

--- wegas-app/src/main/node/wegas-react/src/Testers/Components/Map/Components/WegasOverlay.tsx
+++ wegas-app/src/main/node/wegas-react/src/Testers/Components/Map/Components/WegasOverlay.tsx
@@ -1,5 +1,6 @@
 //Other libs
 import { Overlay } from 'ol';
+import { FeatureLike } from 'ol/Feature';
 // React
 import * as React from 'react';
 import { mapCTX } from './WegasMap';
@@ -8,51 +9,89 @@ export interface WegasOverlayComponentProps {
   inputRef: React.LegacyRef<HTMLElement>;
 }
 
-interface WegasOverlayProps {
+export interface WegasOverlayProps {
+  /**
+   * The component that shows in the overlay
+   */
   OverlayComponent: React.FunctionComponent<WegasOverlayComponentProps>;
-  position?: [number, number];
+  /**
+   * The position of the overlay
+   * if undefined, the overlay will not be show at first render
+   */
+  initialPosition?: [number, number];
+  /**
+   * Set the position of the overlay when a click occures on the map
+   */
+  positionOnClick?: boolean;
+  /**
+   * A function that returns if the overlay can't be placed on feature
+   * if true, the overlay cannot be placed on any feature
+   */
+  featuresFilter?:
+    | { filter: (feature: FeatureLike) => boolean; allowClick: boolean }
+    | true;
 }
 
 export function WegasOverlay({
   OverlayComponent,
-  position,
+  initialPosition: position,
+  positionOnClick,
+  featuresFilter,
 }: WegasOverlayProps) {
   const { map } = React.useContext(mapCTX);
   const overlayComponentRef = React.useRef<HTMLElement>(null);
 
   React.useEffect(() => {
     if (overlayComponentRef.current) {
-      let overlay: Overlay;
+      const overlay = new Overlay({
+        element: overlayComponentRef.current,
+        position,
+      });
 
-      if (position == null && map != null) {
-        overlay = new Overlay({
-          element: overlayComponentRef.current,
-        });
-
+      if (positionOnClick && map) {
         map.on('click', function (evt) {
           if (overlayComponentRef.current) {
-            // set overlay position
+            // get click position
             const coordinate = evt.coordinate;
-            overlay.setPosition(coordinate);
 
-            // center overlay
-            const elementSize =
-              overlayComponentRef.current.getBoundingClientRect();
-            overlay.setOffset([
-              -elementSize.width / 2,
-              -elementSize.height / 2,
-            ]);
+            // Check if the overlay can be placed here
+            let allowClick = true;
+            if (featuresFilter != null) {
+              let clickedFeatures = map.getFeaturesAtPixel(
+                map.getPixelFromCoordinate(evt.coordinate),
+              );
 
-            // close overlay on click
-            overlayComponentRef.current.onclick = function () {
-              overlay.setPosition(undefined);
-            };
+              // If featureFilter is an object, filter the features that blocks the click
+              if (featuresFilter === true) {
+                // If featureFilter is not an object then it is set to true and every features block the click
+                allowClick = clickedFeatures.length === 0;
+              } else {
+                clickedFeatures = clickedFeatures.filter(featuresFilter.filter);
+                if (featuresFilter.allowClick) {
+                  allowClick = clickedFeatures.length > 0;
+                } else {
+                  allowClick = clickedFeatures.length === 0;
+                }
+              }
+            }
+
+            // avoid click on layers
+            if (allowClick) {
+              // set overlay position
+              overlay.setPosition(coordinate);
+              // center overlay
+              const elementSize =
+                overlayComponentRef.current.getBoundingClientRect();
+              overlay.setOffset([
+                -elementSize.width / 2,
+                -elementSize.height / 2,
+              ]);
+              // close overlay on click
+              overlayComponentRef.current.onclick = function () {
+                overlay.setPosition(undefined);
+              };
+            }
           }
-        });
-      } else {
-        overlay = new Overlay({
-          element: overlayComponentRef.current,
-          position,
         });
       }
 
@@ -61,7 +100,7 @@ export function WegasOverlay({
         map?.removeOverlay(overlay);
       };
     }
-  }, [map, position]);
+  }, [featuresFilter, map, position, positionOnClick]);
 
   return (
     <div style={{ display: 'none' }}>
--- wegas-app/src/main/node/wegas-react/src/Testers/Components/Map/MapTester.tsx
+++ wegas-app/src/main/node/wegas-react/src/Testers/Components/Map/MapTester.tsx
@@ -1,12 +1,14 @@
 ////////////////////////////////////////////////////////////////
 // Open layers
 // layer
+import { cx } from '@emotion/css';
 import LayerTile from 'ol/layer/Tile';
 // source
 import SourceOSM from 'ol/source/OSM';
 /////////////////////////////////////////////////////////////////
 // React
 import * as React from 'react';
+import { expandBoth, flex, flexColumn } from '../../../css/classes';
 import { WegasLayer } from './Components/WegasLayer';
 import { WegasMap } from './Components/WegasMap';
 import {
@@ -28,42 +30,102 @@ function overlayFactory(
       <div
         ref={inputRef as React.LegacyRef<HTMLDivElement>}
         style={{
-          width: '100px',
-          height: '100px',
+          width: '50px',
+          height: '50px',
           backgroundColor,
           borderRadius: '50%',
           textAlign: 'center',
-          lineHeight: '100px',
+          lineHeight: '50px',
         }}
       >
-        blablab
+        Overlay
       </div>
     );
   };
 }
 
+// type OverlayMode =
+//   | 'Everywhere'
+//   | 'Not on buildings'
+//   | 'Not on any feature'
+//   | 'Only on a feature';
+
+// const featureFilterFactory: Record<
+//   OverlayMode,
+//   WegasOverlayProps['featuresFilter']
+// > = {
+//   Everywhere: undefined,
+//   'Not on buildings': {
+//     filter: feature => feature.getProperties().tags.building === 'yes',
+//     allowClick: false,
+//   },
+//   'Not on any feature': true,
+//   'Only on a feature': { filter: () => false, allowClick: true },
+// };
+
 export default function MapTester() {
+  // const [selectedOverlayMode, setSelectedOverlayMode] =
+  //   React.useState<OverlayMode>('Everywhere');
+
   return (
-    <WegasMap
-      options={{
-        projection: 'EPSG:4326',
-        center: [6.961834028944175, 46.313121655957694],
-        zoom: 16,
-      }}
-      initialLayers={[
-        new LayerTile({
-          source: new SourceOSM(),
-        }),
-      ]}
-    >
-      <WegasLayer layer={buildingLayer} />
-      <WegasLayer layer={treeLayer} />
-      <WegasOverlay
-        OverlayComponent={overlayFactory('rgb(125,0,0,0.5)')}
-        position={[6.961834028944175, 46.313121655957694]}
-      />
-      <WegasOverlay OverlayComponent={overlayFactory('green')} />
-      <WegasSelect selectStyle={selectStyle} layers={[buildingLayer]} />
-    </WegasMap>
+    <div className={cx(flex, flexColumn, expandBoth)}>
+      {/* <div className={cx(flex, flexRow)}>
+        Allow the overlay popping :
+        <Selector
+          value={selectedOverlayMode}
+          choices={[
+            {
+              label: 'Everywhere',
+              value: 'Everywhere',
+            },
+            {
+              label: 'Not on buildings',
+              value: 'Not on buildings',
+            },
+            {
+              label: 'Not on any feature',
+              value: 'Not on any feature',
+            },
+            {
+              label: 'Only on a feature',
+              value: 'Only on a feature',
+            },
+          ]}
+          onChange={choice => {
+            if (choice != null) {
+              setSelectedOverlayMode(choice as OverlayMode);
+            }
+          }}
+        />
+      </div> */}
+      <WegasMap
+        options={{
+          projection: 'EPSG:4326',
+          center: [6.961834028944175, 46.313121655957694],
+          zoom: 16,
+        }}
+        initialLayers={[
+          new LayerTile({
+            source: new SourceOSM(),
+          }),
+        ]}
+      >
+        <WegasLayer layer={buildingLayer} />
+        <WegasLayer layer={treeLayer} />
+        {/* Useless overlay, no position and no position on click */}
+        <WegasOverlay OverlayComponent={overlayFactory('red')} />
+        {/* Static overlay overlay, no position and no position on click */}
+        <WegasOverlay
+          OverlayComponent={overlayFactory('rgb(125,0,0,0.5)')}
+          initialPosition={[6.961834028944175, 46.313121655957694]}
+        />
+        {/* Movable overlay, can be clicked everywhere */}
+        <WegasOverlay
+          OverlayComponent={overlayFactory('rgb(0,200,0,0.5)')}
+          positionOnClick
+        />
+        <WegasSelect selectStyle={selectStyle} layers={[buildingLayer]} />
+      </WegasMap>
+    </div>
   );
 }
