e77ce224f716e8484cd61ccb0ad8ce4bc51dd772
Xavier Good <71324808+xgo-work@users.noreply.github.com>
2022-05-24 17:11:43 +0200

    jodit text editor, fixed debounce issue and default layout

--- wegas-app/src/main/node/wegas-react/src/Components/HTML/JoditReactEditor.tsx
+++ wegas-app/src/main/node/wegas-react/src/Components/HTML/JoditReactEditor.tsx
@@ -1,11 +1,16 @@
-import React, {useState, useRef, useMemo} from 'react';
+import React from 'react';
 import JoditEditor, { Jodit } from "jodit";
 import 'jodit/build/jodit.min.css';
 import { wlog } from '../../Helper/wegaslog';
-import { IEventEmitterOnOptions } from 'jodit/types/types/events';
+import sanitize from '../../Helper/sanitize';
 
 
 export interface JoditEditorProps {
+
+    /**
+     * Timeout of Jodit. Defaults to 0
+     */
+    innerEditorTimeout?: number;
     /**
      * value - content to inject in the editor
      */
@@ -33,34 +38,73 @@ export interface JoditEditorProps {
     showFilePickerFunc?: (callback: (path: string) => void) => void;
 }
 
+/**
+ * @param value value to clean
+ * @returns a sanitized string stripped of \n
+ */
+function cleanValue(value: string | undefined): string {
+    const v = value || '';
+    return sanitize(v.replace(/\n/g, ''))
+}
+
+const buttonDefaultConfig = [
+    'italic', 'bold', 'underline', '|',
+     'ul', 'ol', '|',
+    'left', 'center', 'right', 'justify', '|',
+    'link', 'image', '|',
+    'source','table','|',
+    'paragraph', 'fontsize','brush'
+];
 
 export default function JoditReactEditor(props : JoditEditorProps) {
 
     const containerRef = React.useRef<HTMLTextAreaElement>(null);
     const jodit = React.useRef<JoditEditor.Jodit>();
+    const muteChanges = React.useRef<boolean>(false);
 
     function updateProps(editor :JoditEditor.Jodit, props: JoditEditorProps){
-        if(editor.value !== props.value){
-            editor.value = props.value || '';
+        const local = cleanValue(editor.value);
+        const updated = cleanValue(props.value);
+        if(local !== updated){
+            // wlog('RECEIVED', updated);
+            // wlog('CURRENT', local);
+            muteChanges.current = true;
+            editor.setEditorValue(updated);
+            muteChanges.current = false;
         }
+        editor.setReadOnly(props.readonly || false);
+        editor.setDisabled(props.disabled || false);
     }
 
-    const o :IEventEmitterOnOptions = {};
-    
     React.useEffect(()=> {
         if(jodit.current){
           updateProps(jodit.current, props);  
         }
         else if(containerRef.current){
+
             const config = Jodit.defaultOptions;
-            // config.
+            config.defaultTimeout = props.innerEditorTimeout || 0;
+            config.buttons = buttonDefaultConfig;
+            config.buttonsMD = buttonDefaultConfig;
+            config.buttonsSM = buttonDefaultConfig;
+            config.buttonsXS = buttonDefaultConfig;
+
             const j = Jodit.make(containerRef.current, config);
+
+            wlog(j.defaultTimeout);
             j.events.on('change', (value, oldValue) => {
-                wlog('youhou ' + value);
-                if(props.onChange){
-                    props.onChange(value);
+                const prev = cleanValue(oldValue);
+                const v = cleanValue(value);
+                // wlog('NEW VALUE ', JSON.stringify(prev));
+                // wlog('OLD VALUE', JSON.stringify(v));
+                if(!muteChanges.current && props.onChange && v !== prev){
+                    // wlog(muteChanges);
+                    // wlog('calling on change', v);
+                    props.onChange(v);
                 }
-            }, )
+            });
+
+            // j.events.on('input', (value, oldValue) => { }, )
             jodit.current = j;
             updateProps(j, props);
         }
