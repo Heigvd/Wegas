32a6a72c4b961f3cb890d16c43f2f4f92c67df1c
P-B Monaco <imfonky@gmail.com>
2022-06-07 08:55:02 +0200

    Using themeVar for styling

--- wegas-app/src/main/node/wegas-react/src/Components/HTML/JoditReactEditor.tsx
+++ wegas-app/src/main/node/wegas-react/src/Components/HTML/JoditReactEditor.tsx
@@ -1,44 +1,43 @@
-import React from 'react';
-import JoditEditor, { Jodit } from "jodit";
-import 'jodit/build/jodit.min.css';
-import sanitize, { toFullUrl, toInjectorStyle } from '../../Helper/sanitize';
-import { ButtonsGroups } from 'jodit/types/types';
 import { css } from '@emotion/css';
+import JoditEditor, { Jodit } from 'jodit';
+import 'jodit/build/jodit.min.css';
+import { ButtonsGroups } from 'jodit/types/types';
+import React from 'react';
+import sanitize, { toFullUrl, toInjectorStyle } from '../../Helper/sanitize';
 import { classesCTX } from '../Contexts/ClassesProvider';
-
+import { themeVar } from '../Theme/ThemeVars';
 
 export interface JoditEditorProps {
-
-    /**
-     * value - content to inject in the editor
-     */
-    value?: string;
-    /**
-     * When no content, this text is displayed in the editor
-     */
-    placeholder?: string;
-    /**
-    * the editor is disabled
-    */
-    disabled?: boolean;
-    /**
-     * read only mode
-     */
-    readonly?: boolean;
-    /**
-     * onChange - function called each time the content of the editor changes
-     */
-    onChange?: (content: string) => void;
-    /**
-     * Notifies the parent component to show the file picker
-     * @param callback function to be called with path to media
-     */
-    showFilePickerFunc?: (callback: (path: string) => void) => void;
-    /**
-     * full => with link and image insertion and html raw edition
-     * player => w/o the above
-     */
-    toolbarLayout?: "full" | "player";
+  /**
+   * value - content to inject in the editor
+   */
+  value?: string;
+  /**
+   * When no content, this text is displayed in the editor
+   */
+  placeholder?: string;
+  /**
+   * the editor is disabled
+   */
+  disabled?: boolean;
+  /**
+   * read only mode
+   */
+  readonly?: boolean;
+  /**
+   * onChange - function called each time the content of the editor changes
+   */
+  onChange?: (content: string) => void;
+  /**
+   * Notifies the parent component to show the file picker
+   * @param callback function to be called with path to media
+   */
+  showFilePickerFunc?: (callback: (path: string) => void) => void;
+  /**
+   * full => with link and image insertion and html raw edition
+   * player => w/o the above
+   */
+  toolbarLayout?: 'full' | 'player';
 }
 
 /**
@@ -46,154 +45,180 @@ export interface JoditEditorProps {
  * @returns a sanitized string stripped of \n
  */
 function cleanValue(value: string | undefined): string {
-    const v = value || '';
-    return sanitize(v.replace(/\n/g, ''))
+  const v = value || '';
+  return sanitize(v.replace(/\n/g, ''));
 }
 
-const wysiwygStyle = css({backgroundColor: 'white'});
-
+const wysiwygStyle = css({
+  backgroundColor: themeVar.colors.BackgroundColor,
+  color: themeVar.colors.DarkTextColor,
+});
 
 const IMG_PLACEHOLDER = 'IMG_PLACEHOLDER';
-function getButtonConfig(layout: "full" | "player" | undefined): ButtonsGroups {
-
-    switch(layout){
-        case 'full':
-            return [
-                'italic', 'bold', 'underline', '|',
-                 'ul', 'ol', '|',
-                'left', 'center', 'right', 'justify', '|',
-                'link', IMG_PLACEHOLDER, '|',
-                'source','table','|',
-                'paragraph', 'fontsize','brush',
-                'classSpan'//'font'
-            ];
-        case 'player':
-        default:
-            // without image, link and raw html buttons
-            return [
-                'italic', 'bold', 'underline', '|',
-                 'ul', 'ol', '|',
-                'left', 'center', 'right', 'justify', '|',
-                'table','|',
-                'paragraph', 'fontsize','brush',
-            ];
-    }
+function getButtonConfig(layout: 'full' | 'player' | undefined): ButtonsGroups {
+  switch (layout) {
+    case 'full':
+      return [
+        'italic',
+        'bold',
+        'underline',
+        '|',
+        'ul',
+        'ol',
+        '|',
+        'left',
+        'center',
+        'right',
+        'justify',
+        '|',
+        'link',
+        IMG_PLACEHOLDER,
+        '|',
+        'source',
+        'table',
+        '|',
+        'paragraph',
+        'fontsize',
+        'brush',
+        'classSpan', //'font'
+      ];
+    case 'player':
+    default:
+      // without image, link and raw html buttons
+      return [
+        'italic',
+        'bold',
+        'underline',
+        '|',
+        'ul',
+        'ol',
+        '|',
+        'left',
+        'center',
+        'right',
+        'justify',
+        '|',
+        'table',
+        '|',
+        'paragraph',
+        'fontsize',
+        'brush',
+      ];
+  }
 }
 
 const disabledPlugins = [
-    'add-new-line',
-    'drag-and-drop',
-    'drag-and-drop-element',
-    'iframe',
-    'video',
-    'print',
-    'media',
-    'powered-by-jodit'
+  'add-new-line',
+  'drag-and-drop',
+  'drag-and-drop-element',
+  'iframe',
+  'video',
+  'print',
+  'media',
+  'powered-by-jodit',
 ];
 
 export default function JoditReactEditor({
-    value,
-    onChange,
-    placeholder,
-    readonly,
-    disabled,
-    showFilePickerFunc,
-    toolbarLayout
-} : JoditEditorProps) {
+  value,
+  onChange,
+  placeholder,
+  readonly,
+  disabled,
+  showFilePickerFunc,
+  toolbarLayout,
+}: JoditEditorProps) {
+  const containerRef = React.useRef<HTMLTextAreaElement>(null);
+  const jodit = React.useRef<JoditEditor.Jodit>();
+  const muteChanges = React.useRef<boolean>(false);
+  const { classes } = React.useContext(classesCTX);
 
-    const containerRef = React.useRef<HTMLTextAreaElement>(null);
-    const jodit = React.useRef<JoditEditor.Jodit>();
-    const muteChanges = React.useRef<boolean>(false);
-    const { classes } = React.useContext(classesCTX);
+  // Main setup
+  React.useEffect(() => {
+    if (containerRef.current) {
+      const buttonsConfig: ButtonsGroups = getButtonConfig(toolbarLayout);
 
-    // Main setup
-    React.useEffect(()=> {
-        if(containerRef.current){
-
-            const buttonsConfig: ButtonsGroups = getButtonConfig(toolbarLayout);
-
-            const idx = buttonsConfig.findIndex((e) => e === IMG_PLACEHOLDER);
-            //custom button for WEGAS images
-            if(idx > -1){
-                buttonsConfig[idx] = {
-                    icon: 'image',
-                    exec: function (editor: JoditEditor.Jodit) {
-                        if(showFilePickerFunc){
-                            showFilePickerFunc((path) => 
-                            {
-                                editor.selection.insertImage(path);
-                            })
-                        }
-                    }
-                }
+      const idx = buttonsConfig.findIndex(e => e === IMG_PLACEHOLDER);
+      //custom button for WEGAS images
+      if (idx > -1) {
+        buttonsConfig[idx] = {
+          icon: 'image',
+          exec: function (editor: JoditEditor.Jodit) {
+            if (showFilePickerFunc) {
+              showFilePickerFunc(path => {
+                editor.selection.insertImage(path);
+              });
             }
+          },
+        };
+      }
 
-            const config = Jodit.defaultOptions;
-            //required to avoid triggering change events when the value is updated from outside
-            config.defaultTimeout = 0;
+      const config = Jodit.defaultOptions;
+      //required to avoid triggering change events when the value is updated from outside
+      config.defaultTimeout = 0;
 
-            config.buttons = buttonsConfig;
-            config.buttonsMD = buttonsConfig;
-            config.buttonsSM = buttonsConfig;
-            config.buttonsXS = buttonsConfig;
+      config.buttons = buttonsConfig;
+      config.buttonsMD = buttonsConfig;
+      config.buttonsSM = buttonsConfig;
+      config.buttonsXS = buttonsConfig;
 
-            config.disablePlugins = disabledPlugins;
+      config.disablePlugins = disabledPlugins;
 
-            if(Object.keys(classes).length){
-                config.controls.classSpan.list = classes;
-            }else{
-                config.removeButtons = ['classSpan'];
-            }
+      if (Object.keys(classes).length) {
+        config.controls.classSpan.list = classes;
+      } else {
+        config.removeButtons = ['classSpan'];
+      }
 
-            config.statusbar = false;
+      config.statusbar = false;
 
-            //Missing typings for placeholder...
-            (config as unknown as {placeholder: string}).placeholder = placeholder || '';
+      //Missing typings for placeholder...
+      (config as unknown as { placeholder: string }).placeholder =
+        placeholder || '';
 
-            const j = Jodit.make(containerRef.current, config);
+      const j = Jodit.make(containerRef.current, config);
 
-            // triggered on keyboard input
-            // j.events.on('input', (value, oldValue) => { }, )
-            jodit.current = j;
+      // triggered on keyboard input
+      // j.events.on('input', (value, oldValue) => { }, )
+      jodit.current = j;
 
-            return () => {
-                jodit.current?.destruct();
-                jodit.current = undefined;
-            }
-        }
-    }, [classes, placeholder, showFilePickerFunc, toolbarLayout]);
+      return () => {
+        jodit.current?.destruct();
+        jodit.current = undefined;
+      };
+    }
+  }, [classes, placeholder, showFilePickerFunc, toolbarLayout]);
 
-    React.useEffect(() => {
-        jodit.current?.events.off('change');
-        jodit.current?.events.on('change', (value, oldValue) => {
-            const prev = cleanValue(oldValue);
-            const v = cleanValue(value);
-            if(!muteChanges.current && onChange && v !== prev){
-                const injected = toInjectorStyle(v);
-                onChange(injected);
-            }
-        });
-    }, [onChange]);
+  React.useEffect(() => {
+    jodit.current?.events.off('change');
+    jodit.current?.events.on('change', (value, oldValue) => {
+      const prev = cleanValue(oldValue);
+      const v = cleanValue(value);
+      if (!muteChanges.current && onChange && v !== prev) {
+        const injected = toInjectorStyle(v);
+        onChange(injected);
+      }
+    });
+  }, [onChange]);
 
-    React.useEffect(() =>{
-        if(jodit.current){
-            const editor = jodit.current;
-            const local = cleanValue(editor.value);
-            const updated = cleanValue(toFullUrl(value));
-            if(local !== updated){
-                // hack : avoid triggering a change callback when setting the new value
-                muteChanges.current = true;
-                editor.setEditorValue(updated);
-                muteChanges.current = false;
-            }
-            editor.setReadOnly(readonly || false);
-            editor.setDisabled(disabled || false);
-        }
-    }, [disabled, readonly, value]);
+  React.useEffect(() => {
+    if (jodit.current) {
+      const editor = jodit.current;
+      const local = cleanValue(editor.value);
+      const updated = cleanValue(toFullUrl(value));
+      if (local !== updated) {
+        // hack : avoid triggering a change callback when setting the new value
+        muteChanges.current = true;
+        editor.setEditorValue(updated);
+        muteChanges.current = false;
+      }
+      editor.setReadOnly(readonly || false);
+      editor.setDisabled(disabled || false);
+    }
+  }, [disabled, readonly, value]);
 
-    return (
-        <div className={wysiwygStyle}>
-            <textarea ref={containerRef}></textarea>
-        </div>
-    );
+  return (
+    <div className={wysiwygStyle}>
+      <textarea ref={containerRef}></textarea>
+    </div>
+  );
 }
