a281606ba6531ea302f812dad8abd37fb38c1c4f
Maxence Laurent <maxence.laurent@gmail.com>
2022-07-14 09:37:35 +0200

    [app] Simplify useDeepMemo hook
    * no need to memoize the memoized value
    Signed-off-by:Maxence Laurent <maxence.laurent@gmail.com>

--- wegas-app/src/main/node/wegas-react/src/Components/Hooks/useDeepMemo.ts
+++ wegas-app/src/main/node/wegas-react/src/Components/Hooks/useDeepMemo.ts
@@ -3,12 +3,12 @@ import * as React from 'react';
 
 export function useDeepMemo<T>(value: T): T {
   const oldValue = React.useRef<T>(value);
-  return React.useMemo(() => {
-    if (!isEqual(oldValue.current, value)) {
-      oldValue.current = value;
-      return value;
-    } else {
-      return oldValue.current;
-    }
-  }, [value]);
+  // if new value is deep different from previous one
+  // update the ref
+  if (!isEqual(oldValue.current, value)) {
+    oldValue.current = value;
+  }
+
+  // always return the ref
+  return oldValue.current;
 }
